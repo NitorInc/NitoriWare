251bd54cfdf5ea9454e72599c9ebfebac1f5d240
Pigu-A <Pigu-A@users.noreply.github.com>
2016-11-19 00:19:42 +0700

    Some optimizations

--- Assets/Scripts/UI/TextOutline.cs
+++ Assets/Scripts/UI/TextOutline.cs
@@ -70,19 +70,13 @@ public class TextOutline : MonoBehaviour
 		}
 	}
 
+	private static Vector3[] offsets = {
+		new Vector3(0, 1, 0), new Vector3(1, 1, 0), new Vector3(1, 0, 0), new Vector3(1, -1, 0),
+		new Vector3(0, -1, 0), new Vector3(-1, -1, 0), new Vector3(-1, 0, 0), new Vector3(-1, 1, 0)
+	};
+
 	Vector3 GetOffset(int i)
 	{
-		switch (i % 8)
-		{
-			case 0: return new Vector3(0, 1, 0);
-			case 1: return new Vector3(1, 1, 0);
-			case 2: return new Vector3(1, 0, 0);
-			case 3: return new Vector3(1, -1, 0);
-			case 4: return new Vector3(0, -1, 0);
-			case 5: return new Vector3(-1, -1, 0);
-			case 6: return new Vector3(-1, 0, 0);
-			case 7: return new Vector3(-1, 1, 0);
-			default: return Vector3.zero;
-		}
+		return offsets[i % 8];
 	}
 }
