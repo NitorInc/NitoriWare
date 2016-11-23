5c45db70cc694271dfddda20ddb866bcf696aee4
Gman8r <brianintile@gmail.com>
2016-11-23 17:58:41 -0500

    Create victory liftoff animation

--- Assets/Resources/Microgames/Spaceship/Scripts/SpaceshipRocket.cs
+++ Assets/Resources/Microgames/Spaceship/Scripts/SpaceshipRocket.cs
@@ -5,8 +5,17 @@ public class SpaceshipRocket : MonoBehaviour
 {
 
 	public Rigidbody2D[] spaceshipBodies;
-	public ParticleSystem explosionParticles;
+	public ParticleSystem explosionParticles, liftoffParticles;
 	public SpaceshipBar bar;
+	public Animator shipAnimator;
+
+	private State state = State.Default;
+	private enum State
+	{
+		Default,
+		Victory,
+		Failure
+	}
 
 	void Update()
 	{
@@ -23,13 +32,27 @@ public class SpaceshipRocket : MonoBehaviour
 
 	void liftoff()
 	{
+		state = State.Victory;
 		MicrogameController.instance.setVictory(true, true);
 
-		//TODO victory liftoff
+		for (int i = 0; i < spaceshipBodies.Length; i++)
+		{
+			spaceshipBodies[i].GetComponent<Vibrate>().vibrateOn = true;
+			spaceshipBodies[i].GetComponent<Collider2D>().enabled = false;
+		}
+		liftoffParticles.Play();
+		shipAnimator.SetInteger("state", (int)state);
+
+
+		CameraShake.instance.xShake = .05f;
+		CameraShake.instance.yShake = .025f;
+		CameraShake.instance.shakeSpeed = 2f;
+		CameraShake.instance.shakeCoolRate = .0f;
 	}
 
 	void explode()
 	{
+		state = State.Failure;
 		MicrogameController.instance.setVictory(false, true);
 
 		for (int i = 0; i < spaceshipBodies.Length; i++)
@@ -37,9 +60,6 @@ public class SpaceshipRocket : MonoBehaviour
 			spaceshipBodies[i].isKinematic = false;
 			spaceshipBodies[i].AddForce(MathHelper.getVectorFromAngle2D(Random.Range(30f, 150f), 600f));
 			spaceshipBodies[i].AddTorque(Random.Range(-1f, 1f) * 500f);
-
-			//TODO fix Vibrate.cs and remove this
-			spaceshipBodies[i].GetComponent<Vibrate>().enabled = false;
 		}
 
 		explosionParticles.Play();
