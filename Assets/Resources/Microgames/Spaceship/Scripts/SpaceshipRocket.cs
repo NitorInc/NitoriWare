1f4a27e8de9ea9f5c57617341c643934591b72d8
Gman8r <brianintile@gmail.com>
2016-11-22 20:57:03 -0500

    Add screenshake to explosion and change smoke

---
+++ Assets/Resources/Microgames/Spaceship/Scripts/SpaceshipRocket.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipRocket : MonoBehaviour
+{
+
+	public Rigidbody2D[] spaceshipBodies;
+
+	public ParticleSystem placeholderParticles;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			explode();
+		}
+	}
+
+	void explode()
+	{
+		for (int i = 0; i < spaceshipBodies.Length; i++)
+		{
+			spaceshipBodies[i].isKinematic = false;
+			spaceshipBodies[i].AddForce(MathHelper.getVectorFromAngle2D(Random.Range(0f, 180f), 500f));
+		}
+
+		placeholderParticles.Play();
+
+		CameraShake.instance.setScreenShake(.3f);
+		CameraShake.instance.shakeSpeed = 15f;
+	}
+}
