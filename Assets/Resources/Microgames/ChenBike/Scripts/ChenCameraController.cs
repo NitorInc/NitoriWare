7ee7820ec0a2dbdfe579455fbc3a4f64cb0f5dc6
plrusek <plrusek@gmail.com>
2016-11-25 23:40:29 +0100

    Add prototype microgame ChenBike

---
+++ Assets/Resources/Microgames/ChenBike/Scripts/ChenCameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChenCameraController : MonoBehaviour {
+    public float speed = 1f;
+    private Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+        newPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        newPosition.x += Time.deltaTime * speed;
+        newPosition.y -= Time.deltaTime * speed / 4f;
+        transform.position = newPosition;
+	}
+}
