d02c7fd32fa0fbe6e9a2d0db298e1f40a8a5224a
Gman8r <brianintile@gmail.com>
2016-12-14 11:24:04 -0500

    Prototype core gameplay with spacebar input

---
+++ Assets/Resources/Microgames/RockBand/Scripts/RockBandContoller.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBandContoller : MonoBehaviour
+{
+	public static RockBandContoller instance;
+
+	public RockBandNote[] notes;
+
+	public State state;
+	public enum State
+	{
+		Default,
+		Victory,
+		Failure
+	}
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	public void victory()
+	{
+		state = State.Victory;
+		MicrogameController.instance.setVictory(true, true);
+
+		//TODO Victory animations for scene
+	}
+
+	public void failure()
+	{
+		state = State.Failure;
+		MicrogameController.instance.setVictory(false, true);
+
+		for (int i = 0; i < notes.Length; i++)
+		{
+			//TODO Failure animations for notes
+			notes[i].gameObject.SetActive(false);
+		}
+		//TODO Failure animations for scene
+	}
+	
+	void Update ()
+	{
+		if (state == State.Default && MicrogameTimer.instance.beatsLeft < 7f)
+			checkForInput();
+	}
+
+	void checkForInput()
+	{
+		//TODO Correct arrow directions
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			for (int i = 0; i < notes.Length; i++)
+			{
+				if (notes[i].state == RockBandNote.State.InRange)
+				{
+					notes[i].playNote();
+					if (i == notes.Length - 1)
+						victory();
+					return;
+				}
+			}
+			failure();
+		}
+	}
+}
---
+++ Assets/Resources/Microgames/RockBand/Scripts/RockBandNote.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBandNote : MonoBehaviour
+{
+
+	public float distancePerBeat, targetBeat;
+	public bool finalNote;
+
+	public State state;
+	public enum State
+	{
+		Idle,
+		InRange,
+		Played
+	}
+
+	void Start ()
+	{
+		updatePosition();
+	}
+	
+	void Update ()
+	{
+		updatePosition();
+	}
+
+	public void playNote()
+	{
+		state = State.Played;
+
+		//TODO Play animation for notes
+		gameObject.SetActive(false);
+	}
+
+	void updatePosition()
+	{
+		transform.position = new Vector3(distancePerBeat * (MicrogameTimer.instance.beatsLeft - targetBeat), transform.position.y, transform.position.z);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Beat Zone")
+		{
+			state = State.InRange;
+		}
+		else if (other.name == "Failure Zone")
+		{
+			RockBandContoller.instance.failure();
+		}
+	}
+}
