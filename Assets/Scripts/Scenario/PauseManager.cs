7d92497a1f69e0c0dcfde4a0fbdb0887f808cbd3
Gman8r <brianintile@gmail.com>
2017-01-05 00:59:13 -0500

    Implement rough PauseManager with audio pausing

---
+++ Assets/Scripts/Scenario/PauseManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+
+	private bool paused;
+	private float timeScale;
+
+	private AudioSource[] audioSources;
+
+	void Start ()
+	{
+		paused = false;
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+			if (!paused)
+				pause();
+			else
+				unPause();
+	}
+
+	void pause()
+	{
+		timeScale = Time.timeScale;
+		Time.timeScale = 0f;
+
+		audioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+		for (int i = 0; i < audioSources.Length; i++)
+		{
+			audioSources[i].Pause();
+		}
+
+		paused = true;
+	}
+
+	void unPause()
+	{
+		Time.timeScale = timeScale;
+
+		for (int i = 0; i < audioSources.Length; i++)
+		{
+			audioSources[i].UnPause();
+		}
+
+		paused = false;
+	}
+}
