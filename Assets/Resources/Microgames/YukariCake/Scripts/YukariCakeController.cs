4caad27cb9eb0ca745406e7b44a40c19d00c9761
Watson Tungjunyatham <starrodkirby86@mail.fresnostate.edu>
2017-01-12 17:26:27 -0800

    Create microgame prototype :ok_hand:

---
+++ Assets/Resources/Microgames/YukariCake/Scripts/YukariCakeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YukariCakeController : MonoBehaviour {
+
+    // Properties
+    public YukariCakeReimu Enemy;
+    public List<AudioSource> AudioSources;
+    public Animator YukariAnimator;
+    public AudioSource YukariSource;
+    public GameObject Food;
+
+    // Temp
+    public AudioClip Fail;
+
+	// Use this for initialization
+	void Start () {
+        foreach (var a in AudioSources)
+            a.pitch = Time.timeScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!MicrogameController.instance.getVictoryDetermined())
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                YukariAnimator.SetTrigger("stateChange");
+                
+                if(Enemy.IsAlert)
+                {
+                    PlayFailureSound();
+                    YukariAnimator.Play("YukariCakeYukariFail");
+                    MicrogameController.instance.setVictory(false, true);
+                }
+                else
+                {
+                    PlayVictorySound();
+                    YukariAnimator.Play("YukariCakeYukariVictory");
+                    Destroy(Food);
+                    MicrogameController.instance.setVictory(true, true);
+                }
+                Enemy.Stop();
+            }
+        }
+	}
+
+    public void PlayVictorySound()
+    {
+        // Better way of dealing with sounds
+        Debug.Log("Woo!");
+    }
+
+    public void PlayFailureSound()
+    {
+        // Yeah
+        YukariSource.PlayOneShot(Fail);
+        Debug.Log(":(");
+    }
+}
---
+++ Assets/Resources/Microgames/YukariCake/Scripts/YukariCakeReimu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// YukariCakeReimu.cs
+/// The logic that facilitates Reimu's AI and movement...
+/// </summary>
+
+public class YukariCakeReimu : MonoBehaviour {
+
+    public Animator Animator;
+    public AudioSource Source;
+    public bool IsAlert = true;
+    public float StateChangeAt = 0;
+    public int Level = 1;
+    public AudioClip WooshTo;
+    public AudioClip WooshBack;
+
+	// Use this for initialization
+	void Start () {
+        SetStateChangeAt();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (MicrogameTimer.instance.beatsLeft <= StateChangeAt && MicrogameTimer.instance.beatsLeft > 0)
+        {
+            Animator.SetTrigger("changeState");
+            SetStateChangeAt();
+        }
+	}
+
+    public void SetStateChangeAt()
+    {
+        float beatsLeft = MicrogameTimer.instance.beatsLeft;
+        float duration;
+        if (IsAlert)
+            duration = Random.Range(1f, 4f);
+        else
+            duration = Random.Range(2f, 4f);
+        StateChangeAt = Mathf.Clamp(beatsLeft - duration, 0.0f, 16f);
+    }
+
+    public void ToggleAlert()
+    {
+        IsAlert = !IsAlert;
+        Debug.Log(string.Format("Changed at {0}, IsAlert: {1}", MicrogameTimer.instance.beatsLeft, IsAlert));
+        SetStateChangeAt();
+    }
+
+    public void PlayWoosh(int wooshValue)
+    {
+        if (wooshValue == 0)
+            Source.PlayOneShot(WooshBack);
+        else
+            Source.PlayOneShot(WooshTo);
+    }
+
+    public void Stop()
+    {
+        StateChangeAt = -1f;
+    }
+
+}
