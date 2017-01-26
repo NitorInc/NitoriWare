ca677d958de58005e384575de294c52cd18bf728
Gman8r <brianintile@gmail.com>
2017-01-25 20:53:28 -0500

    Rearrange scene and naming conventions

---
+++ Assets/Resources/Microgames/CupShuffle/Scripts/CupShuffleController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupShuffleController : MonoBehaviour
+{
+
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
