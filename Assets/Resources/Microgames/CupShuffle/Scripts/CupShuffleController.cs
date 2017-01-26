9c8a38218631f6660be55f37958fa20a33d22dd9
Gman8r <brianintile@gmail.com>
2017-01-25 22:50:30 -0500

    Set up basic shuffle animations

--- Assets/Resources/Microgames/CupShuffle/Scripts/CupShuffleController.cs
+++ Assets/Resources/Microgames/CupShuffle/Scripts/CupShuffleController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class CupShuffleController : MonoBehaviour
 {
+	public int cupCount, shuffleCount;
+	public float shuffleTime;
+	public GameObject cupPrefab;
+
+	private CupShuffleCup[] cups;
 
 
 	void Start ()
 	{
-		
+		cups = new CupShuffleCup[cupCount];
+		for (int i = 0; i < cupCount; i++)
+		{
+			cups[i] = GameObject.Instantiate(cupPrefab).GetComponent<CupShuffleCup>();
+			cups[i].position = i;
+		}
+
+		for (int i = 0; i < shuffleCount; i++)
+		{
+			Invoke("shuffle", i * shuffleTime);
+		}
 	}
-	
+
+	void shuffle()
+	{
+		int indexA = Random.Range(0, cupCount), indexB;
+		do {indexB = Random.Range(0, cupCount);} while (indexA == indexB);
+		bool cupAUp = Random.value < .5f;
+		CupShuffleCup cupA = cups[indexA], cupB = cups[indexB];
+
+		cupA.endAnimation();
+		cupB.endAnimation();
+		cupA.startAnimation(cupB.position - cupA.position, shuffleTime, cupAUp);
+		cupB.startAnimation(cupA.position - cupB.position, shuffleTime, !cupAUp);
+	}
+
 	void Update ()
 	{
-		
+
 	}
 }
---
+++ Assets/Resources/Microgames/CupShuffle/Scripts/CupShuffleCup.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupShuffleCup : MonoBehaviour
+{
+	public int position;
+	public Vector3 leftmostPosition;
+	public float cupSeparation;
+	public bool isCorrect;
+	public AnimationCurve moveCurve;
+
+	private Animation currentAnimation;
+	private struct Animation
+	{
+		public bool active, goingUp;
+		public int cupDistance;
+		public float startTime, duration;
+	}
+
+	void Start()
+	{
+		transform.position = leftmostPosition + (Vector3.right * (float)position * cupSeparation);
+		currentAnimation.active = false;
+	}
+
+	public void startAnimation(int cupDistance, float duration, bool goingUp)
+	{
+		currentAnimation.active = true;
+		currentAnimation.startTime = Time.time;
+		currentAnimation.cupDistance = cupDistance;
+		currentAnimation.duration = duration;
+		currentAnimation.goingUp = goingUp;
+	}
+	
+	void Update ()
+	{
+		if (currentAnimation.active)
+			updateAnimation();
+	}
+
+	void updateAnimation()
+	{
+		if (Time.time >= currentAnimation.startTime + currentAnimation.duration)
+		{
+			endAnimation();
+		}
+		else
+		{
+			lerpPosition((Time.time - currentAnimation.startTime) / currentAnimation.duration);
+		}
+	}
+
+	public void endAnimation()
+	{
+		if (currentAnimation.active)
+		{
+			position += currentAnimation.cupDistance;
+			lerpPosition(0f);
+			currentAnimation.active = false;
+		}
+	}
+
+	void lerpPosition(float time)
+	{
+		transform.position = leftmostPosition + new Vector3(((float)position * cupSeparation)
+			+ Mathf.Lerp(0f, (float)cupSeparation * currentAnimation.cupDistance, time),
+			moveCurve.Evaluate(time) * (currentAnimation.goingUp ? 1f : -1f), transform.position.z);
+	}
+
+}
