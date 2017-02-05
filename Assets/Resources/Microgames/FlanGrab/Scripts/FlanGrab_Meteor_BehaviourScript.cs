9b54575f53b3491cf01b803186e18f6be27c886f
Nialevolstel <nialevolstel@gmail.com>
2017-02-05 04:00:26 -0300

    Initial commit

---
+++ Assets/Resources/Microgames/FlanGrab/Scripts/FlanGrab_Meteor_BehaviourScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlanGrab_Meteor_BehaviourScript : MonoBehaviour {
+
+    public float movementSpeed;           
+    public float leftLimit;
+    public float rightLimit;
+
+    // Only useful when waveMovementIsOn = True;
+    public bool waveMovementIsOn;           // If meteor will move following a Sine wave or not.
+    public float frequency;                 // Sine wave frequency
+    public float amplitude;                 // Sine wave amplitude
+    private float phase = 0;                // Sine wave phase
+    private float startPoint = 0;           // Sine wave center position
+
+    private Transform theTransform;
+    private bool isAlive = true;
+    private bool hasLanded = false;
+    private bool hasBeenDestroyed = false;
+    private ParticleSystem particleTrail;
+
+    // Use this for initialization
+    void Start () {
+        this.theTransform = this.transform;
+        this.startPoint = this.transform.position.x;
+        this.particleTrail = GetComponentInChildren<ParticleSystem>();
+        phase = Random.Range(0, 2 * Mathf.PI);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Move downwards if meteor has not landed or has not been destroyed
+        if (!hasLanded && !hasBeenDestroyed)
+        {
+            fallingMovement();
+            if (waveMovementIsOn) { waveMovement(); }
+        }
+
+        // Destroy gameobject when smoke trail is no longer active
+        if (hasBeenDestroyed)
+        {
+           if (!this.particleTrail.IsAlive())
+            {
+                Destroy(theTransform.gameObject);
+            }
+        }
+
+
+    }
+
+    void fallingMovement()
+    {
+        var moveVector = new Vector3(0, -1, 0);
+        theTransform.position = theTransform.position + (moveVector * this.movementSpeed * Time.deltaTime);
+    }
+
+    void waveMovement()
+    {
+        var omegaComponent = (Time.time * frequency + phase) % (2.0f * Mathf.PI);
+        var xPosition = amplitude * Mathf.Sin( omegaComponent );
+        theTransform.position = new Vector3(startPoint + xPosition, theTransform.position.y , theTransform.position.z);
+    }
+
+    public void meteorHasLanded()
+    {
+        this.hasLanded = true;
+        this.GetComponent<Collider2D>().enabled = false;
+
+
+    }
+
+    void OnMouseDown()
+    {
+        if (!this.hasLanded)
+        {
+            destroyMeteor();
+        }
+    }
+
+    void destroyMeteor()
+    {
+        this.hasBeenDestroyed = true;
+        this.GetComponent<Collider2D>().enabled = false;
+        this.GetComponentInChildren<ParticleSystem>().loop = false;
+        Destroy(this.GetComponentInChildren<SpriteRenderer>().gameObject);
+    }
+
+
+}
---
+++ Assets/Resources/Microgames/FlanGrab/Scripts/FlanGrab_Microgame_Behaviour.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlanGrab_Microgame_Behaviour : MonoBehaviour {
+
+    public GameObject meteorPrefab;
+    public float leftLimit;
+    public float rightLimit;
+
+    public int meteorQuantity;
+    public float timeBetweenCreation;
+    private float timeCounter = 0;
+    [SerializeField]                            // Delete Later
+    private int meteorCreationCounter = 0;
+    [SerializeField]                            // Delete Later
+    private int meteorDestructionCounter = 0;
+    private bool rightCreationPosition;
+
+    // Use this for initialization
+    void Start () {     
+        meteorDestructionCounter = 0;           // Delete Later
+        meteorCreationCounter = 0;              // Delete Later
+        timeCounter = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (meteorCreationCounter < meteorQuantity )
+        {
+            if (timeCounter > 0)
+            {
+                timeCounter -= Time.deltaTime;
+            }
+
+            else
+            {
+                generateMeteor(leftLimit, rightLimit);
+                meteorCreationCounter += 1;
+                timeCounter = timeBetweenCreation;
+            }
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            generateMeteor(leftLimit, rightLimit);
+        }
+
+    }
+
+    // Generate a meteor between the A and B horizontal position
+    void generateMeteor(float A, float B)
+    {
+        var meteorInstance = Instantiate(meteorPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        meteorInstance.SetActive(false);
+        var meteorScript = meteorPrefab.GetComponent<FlanGrab_Meteor_BehaviourScript>();
+        var waveMovementIsOn = meteorScript.waveMovementIsOn;
+        var waveAmplitude = meteorScript.amplitude;
+        var meteorBounds = meteorInstance.GetComponent<CircleCollider2D>().bounds;
+        var centerPoint = meteorBounds.center.x;
+
+        var xPosition = -1f;                                        // Default value for initialization
+        switch (waveMovementIsOn)
+        {
+            case true:
+                xPosition = Random.Range(A + centerPoint + waveAmplitude, B - centerPoint - waveAmplitude);
+                break;
+
+            case false:
+                xPosition = Random.Range(A + centerPoint, B - centerPoint);
+                break;
+        }
+
+        meteorInstance.transform.position = new Vector3(xPosition, 10, 0);
+        meteorInstance.SetActive(true);
+    }
+
+}
