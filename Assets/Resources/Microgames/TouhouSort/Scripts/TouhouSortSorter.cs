1d035bcdce10e77a7d2f1abadd3f4583d47030ce
Barleytree <barleywald@gmail.com>
2017-02-26 22:01:26 +0900

    Refactored zone logic and implemented new system in Sorter for managing stage categories. Cosmetic refactoring. Added wing category and placeholder sprites.

--- Assets/Resources/Microgames/TouhouSort/Scripts/TouhouSortSorter.cs
+++ Assets/Resources/Microgames/TouhouSort/Scripts/TouhouSortSorter.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class TouhouSortSorter : MonoBehaviour {
+public class TouhouSortSorter : MonoBehaviour
+{
 	// Primary class for TouhouSort game handling
 	// Handles objects and win/loss
 
@@ -13,7 +13,8 @@ public class TouhouSortSorter : MonoBehaviour {
 	public int slotCount;
 
 	public Transform stagingArea;
-	public TouhouSortTouhouBucket touhouBucket;
+    public TouhouSortZoneManager zoneManager;
+	//public TouhouSortTouhouBucket touhouBucket;
     public GameObject victoryDisplay;
 
 	TouhouSortSortable[] touhous;
@@ -21,10 +22,31 @@ public class TouhouSortSorter : MonoBehaviour {
 
 	bool sorted;
 
-	void Start() {
-		// Scoop up as many touhous as we can
-		touhous = touhouBucket.Scoop (slotCount);
-		slotCount = touhous.Length;
+    // enum of possible categories
+    public enum Style
+    {
+        Hat,
+        Bunny
+    }
+
+    [System.Serializable]
+    public struct Category
+    {
+        public string name;
+        public Sprite leftIcon, rightIcon;
+        public TouhouSortSortable[] leftPool, rightPool;
+    }
+    public Category[] categories;
+    
+    void Start() {
+        Category category = categories[Random.Range(0, categories.Length)];
+
+        zoneManager.SetZoneAttributes(0, category.leftIcon, category.name, false);
+        zoneManager.SetZoneAttributes(1, category.rightIcon, category.name, true);
+
+        // Scoop up as many touhous as we can
+        touhous = LoadTouhous (category, slotCount);
+        slotCount = touhous.Length;
 
 		sorted = false;
 
@@ -36,7 +58,48 @@ public class TouhouSortSorter : MonoBehaviour {
 		CheckSort ();
 	}
 
-	void CreateSlots() {
+    TouhouSortSortable[] LoadTouhous(Category category, int amount)
+    {
+        List<TouhouSortSortable> touhous = new List<TouhouSortSortable>();
+
+        foreach (TouhouSortSortable touhou in category.leftPool)
+        {
+            touhou.SetStyle(category.name);
+            touhous.Add(touhou);
+        }
+        foreach (TouhouSortSortable touhou in category.rightPool)
+        {
+            touhous.Add(touhou);
+        }
+        
+        // Scoop <amount> random touhous from the category
+        if (amount > touhous.Count)
+        {
+            amount = touhous.Count;
+        }
+
+        MouseGrabbableGroup grabGroup = stagingArea.GetComponent<MouseGrabbableGroup>();
+        TouhouSortSortable[] randomTouhous = new TouhouSortSortable[amount];
+
+        for (int i = 0; i < amount; i++)
+        {
+            TouhouSortSortable touhou = touhous[Random.Range(0, touhous.Count)];
+            MouseGrabbable grabbable = touhou.GetComponent<MouseGrabbable>();
+            randomTouhous[i] = touhou;
+
+            touhous.Remove(touhou);
+            touhou.transform.parent = stagingArea;
+
+            grabbable.onRelease.AddListener(CheckSort);
+
+            grabGroup.addGrabbable(grabbable, true);
+        }
+
+        return randomTouhous;
+    }
+
+    void CreateSlots()
+    {
 		// Instantiate a list of Vector3 objects
 		// which will be the starting spots of
 		// our touhous
@@ -62,35 +125,42 @@ public class TouhouSortSorter : MonoBehaviour {
 		}
 	}
 
-	void FillSlots() {
+	void FillSlots()
+    {
 		// Fill instantiated slots with sortable touhous
-		for (int i = 0; i < slotCount; i++) {
+		for (int i = 0; i < slotCount; i++)
+        {
 			touhous [i].transform.position = slots [i];
 		}
 	}
 
-	public void CheckSort() {
+	public void CheckSort()
+    {
 		// Check the current state of the sort
 		// End the game if everything is sorted
 		bool allSorted = true;
 
-		foreach (TouhouSortSortable sortable in touhous) {
+		foreach (TouhouSortSortable sortable in touhous)
+        {
 			bool thisSorted = false;
 
 			// Get the touhou's current zone, if any
 			TouhouSortDropZone currentZone = sortable.GetCurrentZone();
 			
-			if (currentZone) {
+			if (currentZone)
+            {
 				thisSorted = currentZone.Belongs (sortable);
 			}
 
-			if (thisSorted != true) {
+			if (thisSorted != true)
+            {
 				allSorted = false;
 				break;
 			}
 		}
 
-		if (allSorted) {
+		if (allSorted)
+        {
 			// Sorted
 			Debug.Log("Sorted");
 			sorted = true;
@@ -99,7 +169,8 @@ public class TouhouSortSorter : MonoBehaviour {
 
 			MicrogameController.instance.setVictory(true, true);
 		}
-		else if (sorted) {
+		else if (sorted)
+        {
 			// Unsorted (wont ever happen)
 			Debug.Log("Unsorted");
 			sorted = false;
@@ -109,4 +180,5 @@ public class TouhouSortSorter : MonoBehaviour {
             MicrogameController.instance.setVictory(false, true);
 		}
 	}
+
 }
--- Assets/Resources/Microgames/TouhouSort/Scripts/TouhouSortZoneManager.cs
+++ Assets/Resources/Microgames/TouhouSort/Scripts/TouhouSortZoneManager.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class TouhouSortZoneManager : MonoBehaviour {
-
+public class TouhouSortZoneManager : MonoBehaviour
+{
     public TouhouSortDropZone[] zones;
 
-    public Dictionary<TouhouSortSortable.Style, int> indicatorIcons;
+    public Dictionary<TouhouSortSorter.Style, int> indicatorIcons;
+
+    public void SetZoneAttributes(int index, Sprite sprite, string style, bool invert)
+    {
+        TouhouSortDropZone zone = zones[index];
+
+        zone.SetCategory(style, invert, sprite);
+    }
+
 }
