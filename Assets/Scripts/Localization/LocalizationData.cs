3271af674a8292b9327eb9cd65e8109169772e1e
Gman8r <brianintile@gmail.com>
2017-02-27 16:02:44 -0500

    Create classes to read from json files

---
+++ Assets/Scripts/Localization/LocalizationData.cs
@@ -0,0 +1,12 @@
+﻿[System.Serializable]
+public class LocalizationData
+{
+	public LocalizationItem[] items;
+}
+
+[System.Serializable]
+public class LocalizationItem
+{
+	public string key;
+	public string value;
+}
---
+++ Assets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LocalizationManager : MonoBehaviour
+{
+	public static LocalizationManager instance;
+
+	private Dictionary<string, string> localizedText;
+
+	void Awake ()
+	{
+		//Hourai Elixir
+		if (instance != null && instance != this)
+			Destroy(gameObject);
+		else
+			instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	public void loadLocalizedText(string filename)
+	{
+		localizedText = new Dictionary<string, string>();
+		string filePath = Path.Combine(Application.streamingAssetsPath, filename);
+		if (File.Exists(filePath))
+		{
+			string jsonString = File.ReadAllText(filePath);
+			LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(jsonString);
+			for (int i = 0; i < loadedData.items.Length; i++)
+			{
+				localizedText.Add(loadedData.items[i].key, loadedData.items[i].value);
+			}
+		}
+		else
+			Debug.LogError("Cannot find file " + filename + " in StreamingAssets");
+	}
+}
