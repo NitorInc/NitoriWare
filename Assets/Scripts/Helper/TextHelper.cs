78ef984783cbcb701d6d7c6d54aa887b55aa8edf
Gman8r <brianintile@gmail.com>
2017-03-01 18:59:26 -0500

    Fix incorrect variable call argument

--- Assets/Scripts/Helper/TextHelper.cs
+++ Assets/Scripts/Helper/TextHelper.cs
@@ -62,7 +62,7 @@ public class TextHelper
 			{
 				match = match.Replace("{", "").Replace("}", "");
 				if (match.StartsWith("var."))
-					text = text.Replace("{" + match + "}", getVariable("var." + match, "(TEXT NOT FOUND)"));
+					text = text.Replace("{" + match + "}", getVariable(match, "(TEXT NOT FOUND)"));
 				else
 					text = text.Replace("{" + match + "}", getLocalizedText(match, "(TEXT NOT FOUND)"));
 			}
