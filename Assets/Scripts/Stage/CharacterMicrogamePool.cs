cf5a58f08547e786584ff2396e565f05ef345906
Gman8r <brianintile@gmail.com>
2017-06-15 19:46:52 -0400

    Add Stage and CharacterMicrogamePool classes

---
+++ Assets/Scripts/Stage/CharacterMicrogamePool.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMicrogamePool : MonoBehaviour
+{
+	[SerializeField]
+	private MicrogameBatch[] microgameBatches;
+	[SerializeField]
+	private Stage.Microgame bossMicrogame;
+
+	[System.Serializable]
+	public struct MicrogameBatch
+	{
+		public int pick;
+		public Stage.Microgame[] pool;
+
+	}
+}
---
+++ Assets/Scripts/Stage/Stage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Stage : MonoBehaviour
+{
+
+	[System.Serializable]
+	public struct Interruption
+	{
+		[SerializeField]
+		private string name;	//Not actually used, but makes inspector editing easier
+		public int animatorPart;	//Must be >3 to not interfere with default stage parts
+		public float beatDuration;
+		public SpeedChange speedChange;
+		public bool applySpeedChangeOnStart;
+
+		public enum SpeedChange
+		{
+			None,
+			SpeedUp,
+			ResetSpeed,
+			Custom
+		}
+	}
+
+	[System.Serializable]
+	public struct Microgame
+	{
+		public string microgameId;
+		public int baseDifficulty;
+
+		[HideInInspector]
+		public AsyncOperation asyncOperation;
+	}
+
+	public abstract Microgame getMicrogame(int cycleIndex);
+
+	public virtual int getCustomSpeed(Interruption interruption)
+	{
+		return 1;
+	}
+
+	public abstract Interruption[] getInterruptions(int cycleIndex);
+
+}
