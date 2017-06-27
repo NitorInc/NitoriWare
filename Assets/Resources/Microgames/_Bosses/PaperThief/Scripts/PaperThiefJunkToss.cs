cd201b9de6070a32cce655118a9ec1a0a108469c
Gman8r <brianintile@gmail.com>
2017-06-27 12:16:29 -0400

    Creat throwable junk and gun

---
+++ Assets/Resources/Microgames/_Bosses/PaperThief/Scripts/PaperThiefJunkToss.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperThiefJunkToss : MonoBehaviour
+{
+
+#pragma warning disable 0649   //Serialized Fields
+    [SerializeField]
+    private GameObject junkPrefab, gunPrefab;
+    [SerializeField]
+    private List<Sprite> junkPieces;
+    [SerializeField]
+    private float timeBetweenJunk;
+#pragma warning restore 0649
+
+    private bool activated;
+    private float junkTimer;
+
+	void Start()
+	{
+        activated = false;
+	}
+
+    void Update()
+    {
+        if (activated)
+        {
+            junkTimer -= Time.deltaTime;
+            if (junkTimer <= 0f)
+                createJunk();
+        }
+	}
+
+    public void activateToss()
+    {
+        activated = true;
+        createJunk();
+    }
+
+    public void createGun()
+    {
+        GameObject.Instantiate(junkPrefab, transform.position, Quaternion.identity);
+    }
+
+    void createJunk()
+    {
+        GameObject.Instantiate(junkPrefab, transform.position, Quaternion.identity).GetComponent<SpriteRenderer>().sprite = junkPieces[0];
+        junkPieces.RemoveAt(0);
+        junkTimer += timeBetweenJunk;
+    }
+}
