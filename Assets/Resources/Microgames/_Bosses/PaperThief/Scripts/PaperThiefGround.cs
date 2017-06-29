7b4185a75aa67f8dc62e6e9f3cdea6a55e29297b
Gman8r <brianintile@gmail.com>
2017-06-28 21:01:54 -0400

    Improve ground prefab and placeholder sprites

--- Assets/Resources/Microgames/_Bosses/PaperThief/Scripts/PaperThiefGround.cs
+++ Assets/Resources/Microgames/_Bosses/PaperThief/Scripts/PaperThiefGround.cs
@@ -8,9 +8,11 @@ public class PaperThiefGround : MonoBehaviour
 
 #pragma warning disable 0649	//Serialized Fields
     [SerializeField]
+    private float leftHeight, rightHeightOffset;
+    [SerializeField]
     private BoxCollider2D _collider;
     [SerializeField]
-    private RectTransform top, topLeft, topRight, left, right, bottomLeft, bottomRight;
+    private RectTransform tile, top, topLeft, left, bottomLeft, topRight, right, bottomRight;
 #pragma warning restore 0649
 
     private RectTransform _rectTransform;
@@ -32,17 +34,37 @@ public class PaperThiefGround : MonoBehaviour
         _collider.offset = new Vector2(Mathf.Lerp(half.x, -half.x, _rectTransform.pivot.x),
             Mathf.Lerp(half.y, -half.y, _rectTransform.pivot.y));
 
-        top.sizeDelta = new Vector2(_rectTransform.sizeDelta.x - 2f, 1f);
-        top.localPosition = Vector3.zero;
+        if (tile != null)
+        {
+            tile.sizeDelta = new Vector2(size.x, size.y - 1f);
+            tile.localPosition = new Vector3(0f, -1f, 0f);
+        }
+
+        if (top != null)
+        {
+            top.sizeDelta = new Vector2(_rectTransform.sizeDelta.x - 2f, 1f);
+            top.localPosition = Vector3.zero;
+        }
         
-        topLeft.localPosition = new Vector3(-half.x, 0f, 0f);
-        topRight.localPosition = topLeft.localPosition * -1f;
+        if (topLeft != null)
+            topLeft.localPosition = new Vector3(-half.x, 0f, 0f);
+        if (topRight != null)
+            topRight.localPosition = new Vector3(half.x, 0f, 0f);
 
-        left.localPosition = new Vector3(-half.x, (-half.y) - .5f, 0f);
-        right.localPosition = new Vector3(-left.localPosition.x, left.localPosition.y, 0f);
-        left.sizeDelta = right.sizeDelta = new Vector2(1f, _rectTransform.sizeDelta.y - 1f);
-
-        bottomLeft.transform.localPosition = new Vector3(-half.x, -size.y);
-        bottomRight.transform.localPosition = new Vector3(-bottomLeft.localPosition.x, bottomLeft.localPosition.y, 0f);
+        if (left != null)
+        {
+            left.localPosition = new Vector3(-half.x, -half.y - .5f, 0f);
+            left.sizeDelta = new Vector2(1f, _rectTransform.sizeDelta.y - 1f);
+        }
+        if (right != null)
+        {
+            right.localPosition = new Vector3(half.x, -half.y - .5f, 0f);
+            right.sizeDelta = new Vector2(1f, _rectTransform.sizeDelta.y - 1f);
+        }
+        
+        if (bottomLeft != null)
+            bottomLeft.transform.localPosition = new Vector3(-half.x, -size.y);
+        if (bottomRight != null)
+            bottomRight.transform.localPosition = new Vector3(half.x, -size.y);
     }
 }
