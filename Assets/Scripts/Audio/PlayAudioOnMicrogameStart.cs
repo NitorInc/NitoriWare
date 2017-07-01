3b42cea2b4f415b05c13d6a355fe15e8578d5384
Gman8r <brianintile@gmail.com>
2017-06-30 23:53:37 -0400

    Make stage theme play at correct time from stage

---
+++ Assets/Scripts/Audio/PlayAudioOnMicrogameStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOnMicrogameStart : MonoBehaviour
+{
+
+	void Start()
+	{
+        AudioHelper.playScheduled(GetComponent<AudioSource>(), StageController.beatLength);
+	}
+	
+	void play()
+	{
+	}
+}
