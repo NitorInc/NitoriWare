4d6de529eaf203520081152c5005bf009e536380
Gman8r <brianintile@gmail.com>
2017-07-06 14:46:55 -0400

    Add Menu Controller prefab

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+
+#pragma warning disable 0649   //Serialized Fields
+    [SerializeField]
+    private bool dontEnableCursor;
+#pragma warning restore 0649
+
+	void Start()
+	{
+        if (!dontEnableCursor)
+            Cursor.visible = true;
+	}
+	
+	void Update()
+	{
+		
+	}
+}
