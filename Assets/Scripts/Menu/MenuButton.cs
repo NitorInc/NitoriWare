711f5852d946727afc85f53b53925226d8145297
Gman8r <brianintile@gmail.com>
2017-07-12 14:51:23 -0400

    Improve button press logic

---
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+
+#pragma warning disable 0649	//Serialized Fields
+    [SerializeField]
+    private Animator buttonAnimator;
+#pragma warning restore 0649
+
+	void Start()
+	{
+		
+	}
+	
+	void LateUpdate()
+	{
+        buttonAnimator.SetBool("MouseHeld", Input.GetMouseButton(0));
+        buttonAnimator.SetBool("MouseDown", Input.GetMouseButtonDown(0));
+    }
+}
