b3691b464a0d82ab0e6594ebfc7295d77e620328
Gman8r <brianintile@gmail.com>
2017-07-12 16:05:22 -0400

    Link buttons to start gameplay

---
+++ Assets/Scripts/Menu/GameMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach to every menu animator
+public class GameMenu : MonoBehaviour
+{
+    public static SubMenu subMenu = SubMenu.Splash;
+    //public static SubMenu subMenu = SubMenu.Title;  //Debug purposes
+    public static bool shifting;
+
+#pragma warning disable 0649	//Serialized Fields
+    [SerializeField]
+    private Animator animator;
+#pragma warning restore 0649
+
+    private SubMenu instanceSubMenu;
+
+    public enum SubMenu
+    {
+        Splash = 0,
+        Title = 1
+    }
+
+    void Start()
+    {
+        shifting = subMenu == SubMenu.Splash;
+        if (instanceSubMenu != subMenu)
+            updateSubMenu();
+    }
+
+    void LateUpdate()
+	{
+        if (instanceSubMenu != subMenu)
+            updateSubMenu();
+	}
+
+    public void shift(int subMenu)
+    {
+        GameMenu.subMenu = (SubMenu)subMenu;
+        updateSubMenu();
+        shifting = true;
+    }
+
+    public void endShift()
+    {
+        shifting = false;
+    }
+
+    void updateSubMenu()
+    {
+        instanceSubMenu = subMenu;
+        animator.SetInteger("SubMenu", (int)instanceSubMenu);
+    }
+}
---
+++ Assets/Scripts/Menu/GameplayMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach to all menu screens that can transition to gameplay
+public class GameplayMenu : MonoBehaviour
+{
+
+#pragma warning disable 0649   //Serialized Fields
+    [SerializeField]
+    private Animator animator;
+#pragma warning restore 0649
+    
+	void Start()
+    {
+        animator.ResetTrigger("StartGameplay");
+    }
+
+    public void startGameplay()
+    {
+        GameMenu.shifting = true;
+        animator.SetTrigger("StartGameplay");
+    }
+}
