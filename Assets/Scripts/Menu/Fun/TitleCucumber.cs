8f26068bd024c80fcf3f14d6cfc4778fb9749ba9
Gman8r <brianintile@gmail.com>
2017-07-13 02:28:57 -0400

    Make cucumbers fly away on shift

--- Assets/Scripts/Menu/Fun/TitleCucumber.cs
+++ Assets/Scripts/Menu/Fun/TitleCucumber.cs
@@ -23,6 +23,16 @@ public class TitleCucumber : MonoBehaviour
 	
 	void Update()
 	{
+        if (GameMenu.shifting)
+        {
+            _rigidBody.bodyType = RigidbodyType2D.Kinematic;
+            Vector2 escapeVelocity = MathHelper.getVector2FromAngle(
+                ((Vector2)(transform.position - Camera.main.transform.position)).getAngle(), maxSpeed);
+            transform.position += (Vector3)escapeVelocity * Time.deltaTime;
+            if (CameraHelper.isObjectOffscreen(transform, maxSpeed / 10f))
+                Destroy(gameObject);
+            return;
+        }
 
         if (_rigidBody.bodyType == RigidbodyType2D.Dynamic)
         {
