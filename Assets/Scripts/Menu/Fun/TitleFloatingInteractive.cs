3bfdfd2eea7266cade00c422d340c124ffe142f6
Gman8r <brianintile@gmail.com>
2017-07-13 16:27:23 -0400

    Create interactable spawner

--- Assets/Scripts/Menu/Fun/TitleFloatingInteractive.cs
+++ Assets/Scripts/Menu/Fun/TitleFloatingInteractive.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class TitleFloatingInteractive : MonoBehaviour
 {
+    public TitleInteractableSpawner spawner;
 
 #pragma warning disable 0649   //Serialized Fields
     [SerializeField]
-    private float escapeSpeed;
+    private float escapeSpeed, lifeTime;
     [SerializeField]
     private Rigidbody2D _rigidBody;
+    [SerializeField]
+    private Collider2D wallHitCollider;
 #pragma warning restore 0649
+
+    private bool ignoreWalls;
 
     void Start()
 	{
-
+        setIgnoreWalls(true);   
 	}
 	
 	void LateUpdate()
@@ -25,9 +30,27 @@ public class TitleFloatingInteractive : MonoBehaviour
             Vector2 escapeVelocity = MathHelper.getVector2FromAngle(
                 ((Vector2)(transform.position - Camera.main.transform.position)).getAngle(), escapeSpeed);
             transform.position += (Vector3)escapeVelocity * Time.deltaTime;
-            if (CameraHelper.isObjectOffscreen(transform, escapeSpeed / 10f))
-                Destroy(gameObject);
             return;
         }
+        else if (ignoreWalls && !CameraHelper.isObjectOffscreen(transform,
+            Mathf.Max(wallHitCollider.bounds.extents.x, wallHitCollider.bounds.extents.y)))
+        {
+
+        }
+
+        if (CameraHelper.isObjectOffscreen(transform, 10f))
+            Destroy(gameObject);
+    }
+
+    public void setIgnoreWalls(bool ignore)
+    {
+        if (wallHitCollider == null)
+            return;
+
+        foreach (BoxCollider2D wall in spawner.wallColliders)
+        {
+            Physics2D.IgnoreCollision(wallHitCollider, wall, ignore);
+        }
+        ignoreWalls = ignore;
     }
 }
---
+++ Assets/Scripts/Menu/Fun/TitleInteractableSpawner.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleInteractableSpawner : MonoBehaviour
+{
+    public BoxCollider2D[] wallColliders;
+
+#pragma warning disable 0649   //Serialized Fields
+    [SerializeField]
+    private Vector2 spawnTimeBounds, spawnExtents;
+    [SerializeField]
+    private Interactable[] interactables;
+#pragma warning restore 0649
+
+    [System.Serializable]
+    public struct Interactable
+    {
+        public GameObject prefab;
+        [Range(0f, 1f)]
+        public float spawnFrequency;
+    }
+
+
+    private float spawnIn;
+
+	void Start()
+	{
+        spawnIn = MathHelper.randomRangeFromVector(spawnTimeBounds);
+	}
+	
+	void Update()
+	{
+        if (!GameMenu.shifting && GameMenu.subMenu == GameMenu.SubMenu.Title)
+            updateSpawner();
+	}
+
+    void updateSpawner()
+    {
+        spawnIn -= Time.deltaTime;
+        if (spawnIn <= 0f)
+        {
+            spawnInteractable();
+            spawnIn = MathHelper.randomRangeFromVector(spawnTimeBounds);
+        }
+    }
+
+    void spawnInteractable()
+    {
+        float spawnWeight = 0f;
+        foreach (Interactable interactable in interactables)
+        {
+            spawnWeight += interactable.spawnFrequency;
+        }
+        float spawnValue = Random.Range(0f, spawnWeight);
+        GameObject spawn = null;
+        foreach (Interactable interactable in interactables)
+        {
+            spawnValue -= interactable.spawnFrequency;
+            if (spawnValue <= 0f)
+            {
+                spawn = interactable.prefab;
+                break;
+            }
+        }
+
+        Instantiate(spawn, transform).transform.localPosition = (Vector3)getSpawnOffset(Random.Range(0, 4));
+    }
+
+    //0-3 are cardinal directions, starting at 
+    Vector2 getSpawnOffset(int direction)
+    {
+        switch (direction)
+        {
+            case (0):
+                return new Vector2(spawnExtents.x, Random.Range(-spawnExtents.y, spawnExtents.y));
+            case (1):
+                return new Vector2(Random.Range(-spawnExtents.x, spawnExtents.x), spawnExtents.y);
+            case (2):
+                return new Vector2(-spawnExtents.x, Random.Range(-spawnExtents.y, spawnExtents.y));
+            case (3):
+                return new Vector2(Random.Range(-spawnExtents.x, spawnExtents.x), -spawnExtents.y);
+            default:
+                return Vector2.zero;
+        }
+    }
+}
