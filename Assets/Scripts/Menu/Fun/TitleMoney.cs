a9be40fe53681e903085759ec074d70dc37c8dc0
Gman8r <brianintile@gmail.com>
2017-07-13 12:08:25 -0400

    Create money interactable prefab

---
+++ Assets/Scripts/Menu/Fun/TitleMoney.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMoney : MonoBehaviour
+{
+
+#pragma warning disable 0649   //Serialized Fields
+    [SerializeField]
+    private Vector2 speedBounds, torqueBounds;
+    [SerializeField]
+    private Rigidbody2D _rigidBody;
+    [SerializeField]
+    private Collider2D clickCollider;
+    [SerializeField]
+    private Animator animator;
+#pragma warning restore 0649
+
+	void Start()
+	{
+        _rigidBody.velocity = MathHelper.getVector2FromAngle(Random.Range(0f, 360f), MathHelper.randomRangeFromVector(speedBounds));
+        _rigidBody.AddTorque(MathHelper.randomRangeFromVector(torqueBounds) * (MathHelper.randomBool() ? 1f : -1f));
+	}
+	
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0) && CameraHelper.isMouseOver(clickCollider))
+            animator.Play("Ching");
+    }
+}
