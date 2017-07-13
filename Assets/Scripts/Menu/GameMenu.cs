6d7d692c0697a92701f19febddef8594c300e251
Gman8r <brianintile@gmail.com>
2017-07-13 19:07:46 -0400

    Create gear interactive

--- Assets/Scripts/Menu/GameMenu.cs
+++ Assets/Scripts/Menu/GameMenu.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 //Attach to every menu animator
 public class GameMenu : MonoBehaviour
 {
-    public static SubMenu subMenu = SubMenu.Splash;
-    //public static SubMenu subMenu = SubMenu.Title;  //Debug purposes
+    //public static SubMenu subMenu = SubMenu.Splash;
+    public static SubMenu subMenu = SubMenu.Title;  //Debug purposes
     public static bool shifting;
 
 #pragma warning disable 0649	//Serialized Fields
