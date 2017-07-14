1258aeb591f8fb8e394efe5143dc7ddf1f8c6784
Gman8r <brianintile@gmail.com>
2017-07-14 15:49:05 -0400

    Create MenuDropdown prefab

---
+++ Assets/Scripts/Menu/MenuDropdown.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuDropdown : MonoBehaviour
+{
+
+#pragma warning disable 0649   //Serialized Fields
+    [SerializeField]
+    private Dropdown dropdown;
+#pragma warning restore 0649
+
+    public enum Type
+    {
+        Explicit,
+        Language,
+        Resolution
+    }
+
+
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+
+    public void setValue(int value)
+    {
+
+    }
+}
