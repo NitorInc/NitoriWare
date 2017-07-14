f3a273dade415603d5350cf14b62409b43360fa6
Gman8r <brianintile@gmail.com>
2017-07-14 17:37:22 -0400

    Better dropdown highlight logic

--- Assets/Scripts/Menu/MenuDropdown.cs
+++ Assets/Scripts/Menu/MenuDropdown.cs
@@ -9,28 +9,46 @@ public class MenuDropdown : MonoBehaviour
 #pragma warning disable 0649   //Serialized Fields
     [SerializeField]
     private Dropdown dropdown;
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private Collider2D clickCollider;
 #pragma warning restore 0649
 
-    public enum Type
-    {
-        Explicit,
-        Language,
-        Resolution
-    }
-
+    private int closedChildCount;
+    private bool hiding;
 
 	void Start()
 	{
-		
+        closedChildCount = transform.childCount;
 	}
 	
 	void Update()
-	{
-		
-	}
+    {
+        if (GameMenu.shifting)
+        {
+            if (!isOpen())
+            {
+                animator.SetBool("CanHighlight", false);
+                dropdown.Hide();
+                hiding = true;
+            }
+            return;
+        }
+        else
+            hiding = false;
+
+        animator.SetBool("CanHighlight", CameraHelper.isMouseOver(clickCollider) && !isOpen());
+        
+    }
 
     public void setValue(int value)
     {
 
+    }
+
+    bool isOpen()
+    {
+        return transform.childCount > closedChildCount;
     }
 }
