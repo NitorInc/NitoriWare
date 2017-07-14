4f2761db808d91cf4f9e54ee1f038aa947ed66d8
Gman8r <brianintile@gmail.com>
2017-07-13 23:59:42 -0400

    Disable buttons on shift

--- Assets/Scripts/Menu/MenuButton.cs
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -8,7 +8,11 @@ public class MenuButton : MonoBehaviour
 
 #pragma warning disable 0649	//Serialized Fields
     [SerializeField]
+    private Button button;
+    [SerializeField]
     private Animator buttonAnimator;
+    [SerializeField]
+    private Collider2D backupCollider;
 #pragma warning restore 0649
 
 	void Start()
@@ -18,7 +22,26 @@ public class MenuButton : MonoBehaviour
 	
 	void LateUpdate()
 	{
+        bool shouldEnable = shouldButtonBeEnabled();
+        if (button.enabled != shouldEnable)
+            setButtonEnabled(shouldEnable);
+
         buttonAnimator.SetBool("MouseHeld", Input.GetMouseButton(0));
         buttonAnimator.SetBool("MouseDown", Input.GetMouseButtonDown(0));
+    }
+
+    bool shouldButtonBeEnabled()
+    {
+        return !GameMenu.shifting;
+    }
+
+    void setButtonEnabled(bool enabled)
+    {
+        button.enabled = enabled;
+        if (enabled && CameraHelper.isMouseOver(backupCollider))
+        {
+            buttonAnimator.ResetTrigger("Normal");
+            buttonAnimator.SetTrigger("Highlighted");
+        }
     }
 }
