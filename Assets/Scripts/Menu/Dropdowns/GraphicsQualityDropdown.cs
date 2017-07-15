837dc5db23bc07ddb911ce0345fc74cca496549d
Gman8r <brianintile@gmail.com>
2017-07-15 00:15:51 -0400

    Add graphics quality dropdown

---
+++ Assets/Scripts/Menu/Dropdowns/GraphicsQualityDropdown.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class GraphicsQualityDropdown : MonoBehaviour
+{
+
+#pragma warning disable 0649   //Serialized Fields
+    [SerializeField]
+    private Dropdown dropdown;
+    [SerializeField]
+    private string keyPrefix;
+#pragma warning restore 0649
+
+    void Start()
+    {
+        if (!string.IsNullOrEmpty(TextHelper.getLoadedLanguage()))
+            setOptionText();
+
+        dropdown.value = QualitySettings.GetQualityLevel();
+    }
+
+    void setOptionText()
+    {
+        string[] defaultNames = QualitySettings.names;
+        for (int i = 0; i < dropdown.options.Count; i++)
+        {
+            dropdown.options[i].text = TextHelper.getLocalizedText(keyPrefix + "." + i.ToString(), defaultNames[i]);
+        }
+    }
+
+    public void select(int item)
+    {
+        if (QualitySettings.GetQualityLevel() != item)
+            QualitySettings.SetQualityLevel(item);
+    }
+}
