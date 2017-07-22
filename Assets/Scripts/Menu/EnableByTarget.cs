98afaffe794cf042e856c6ebbda3ca661a578aa3
Brian Intile <brianintile@gmail.com>
2017-07-22 16:54:42 -0400

    Disable resolution settings in WebGL build

---
+++ Assets/Scripts/Menu/EnableByTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableByTarget : MonoBehaviour
+    {
+    #pragma warning disable 0649
+    [SerializeField]
+    private RuntimePlatform[] targetPlatforms;
+    [SerializeField]
+    private bool disableOnTargetPlatforms;
+
+	void Start ()
+    {
+        RuntimePlatform platform = Application.platform;
+        for (int i = 0; i < targetPlatforms.Length; i++)
+        {
+            if (targetPlatforms[i] == platform)
+            {
+                gameObject.SetActive(!disableOnTargetPlatforms);
+                return;
+            }
+        }
+	}
+}
