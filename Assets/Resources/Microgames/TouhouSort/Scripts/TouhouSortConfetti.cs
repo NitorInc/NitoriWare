f5c64506596eb7942c968d8971d6f0801453e269
Barleytree <barleywald@gmail.com>
2017-07-27 05:23:10 +0900

    Added confetti and grab sound effect.

---
+++ Assets/Resources/Microgames/TouhouSort/Scripts/TouhouSortConfetti.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TouhouSortConfetti : MonoBehaviour
+{
+    private ParticleSystem particles;
+
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        var main = particles.main;
+        main.startColor = new HSBColor(Random.Range(0f, 1f), 1f, Random.Range(.7f, .9f)).ToColor();
+    }
+}
