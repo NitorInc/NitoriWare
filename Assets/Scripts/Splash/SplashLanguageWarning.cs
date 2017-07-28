a138e4438e202628f1a994507f3ccf3cce9df8a1
Brian Intile <brianintile@gmail.com>
2017-07-28 03:33:39 -0400

    Implement warning for incomplete languages

---
+++ Assets/Scripts/Splash/SplashLanguageWarning.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashLanguageWarning : MonoBehaviour
+{
+    private Text textComponent;
+
+	void Awake()
+    {
+        textComponent = GetComponent<Text>();
+	}
+	
+	void Update()
+    {
+        var language = TextHelper.getLoadedLanguage();
+        if (!string.IsNullOrEmpty(language.getLanguageID()) && language.incomplete)
+        {
+            textComponent.enabled = true;
+            enabled = false;
+        }
+            
+	}
+}
