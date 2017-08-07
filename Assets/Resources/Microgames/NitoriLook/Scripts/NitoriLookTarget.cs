c93831b404e3edb748bf149a3efba9d5c276a2f7
Gman8r <brianintile@gmail.com>
2017-08-07 00:15:11 -0400

    Create look target

---
+++ Assets/Resources/Microgames/NitoriLook/Scripts/NitoriLookTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NitoriLookTarget : MonoBehaviour
+{
+
+#pragma warning disable 0649	//Serialized Fields
+#pragma warning restore 0649
+
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+        transform.rotation = Camera.main.transform.rotation;
+        Vector3 cameraEulers = Camera.main.transform.rotation.eulerAngles;
+        //cameraEulers.Scale(new Vector3(.5f, 1f, 1f));
+        //cameraEulers = new Vector3(cameraEulers.x * .5f, cameraEulers.y, cameraEulers.z);
+        transform.rotation = Quaternion.Euler(cameraEulers);
+	}
+}
