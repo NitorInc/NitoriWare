3ae68e7dc83e596d847a764196978fe13ff9de1b
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2017-08-19 13:52:54 +0900

    added random knife spawning

--- Assets/Resources/Microgames/KnifeDodge/Scripts/KnifeDodgeController.cs
+++ Assets/Resources/Microgames/KnifeDodge/Scripts/KnifeDodgeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KnifeDodgeController : MonoBehaviour {
+	List<GameObject> knifeList;
+	public GameObject knifePrefab;
+	public GameObject reimuPrefab;
+
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +16,18 @@ public class KnifeDodgeController : MonoBehaviour {
 
 	// Spawns several knives above the player.
 	void SpawnKnives() {
+		knifeList = new List<GameObject> ();
+		Instantiate (knifePrefab, GetRandomLocation(), Quaternion.identity);
 
+	}
+
+	Vector3 GetRandomLocation() {
+		Vector2 dir = Random.insideUnitCircle;
+		Vector3 position = Vector3.zero;
+		//make it appear on the top/bottom
+		return new Vector3(dir.x * Camera.main.orthographicSize * Camera.main.aspect, 
+			Mathf.Sign(dir.y) * Camera.main.orthographicSize);
+		
 	}
 
 	// Deletes knives to create a safe zone.
