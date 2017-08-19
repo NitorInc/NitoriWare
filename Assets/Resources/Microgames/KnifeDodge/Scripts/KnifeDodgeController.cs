891934d52348e9b931b1ffb29359a372fba64a9e
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2017-08-19 13:32:00 +0900

    start knife creation scripts

---
+++ Assets/Resources/Microgames/KnifeDodge/Scripts/KnifeDodgeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeDodgeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SpawnKnives ();
+		CreateSafeZone ();
+	}
+
+	// Spawns several knives above the player.
+	void SpawnKnives() {
+
+	}
+
+	// Deletes knives to create a safe zone.
+	void CreateSafeZone() {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
