079f359b08cadb6fde62cd68567210d6269d1dd2
Gman8r <brianintile@gmail.com>
2017-08-19 21:12:08 -0400

    Constrain Y look angle

--- Assets/Resources/Microgames/NitoriLook/Scripts/NitoriLookMovement.cs
+++ Assets/Resources/Microgames/NitoriLook/Scripts/NitoriLookMovement.cs
@@ -25,11 +25,11 @@ public class NitoriLookMovement : MonoBehaviour
             x -= 360f;
         }
         x -= Input.GetAxis("Mouse Y") * mouseRotateVelocity.y;
-        x = Mathf.Clamp(x, -90f, 90f);
+        x = Mathf.Clamp(x, -80f, 15f);
 
         float y = eulerRotation.y;
-        y += Input.GetAxis("Mouse X") * mouseRotateVelocity.x;
-
+		y += Input.GetAxis("Mouse X") * mouseRotateVelocity.x;
+		
         transform.rotation = Quaternion.Euler(new Vector3(x, y, 0f));
     }
 }
