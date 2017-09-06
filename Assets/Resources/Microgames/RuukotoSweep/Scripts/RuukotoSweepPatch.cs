c7f372816ff688ab972e9f0d895390dbdb666396
Brian Intile <brianintile@gmail.com>
2017-09-05 23:40:18 -0400

    Create leaf disperse function

---
+++ Assets/Resources/Microgames/RuukotoSweep/Scripts/RuukotoSweepPatch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuukotoSweepPatch : MonoBehaviour
+{
+    public ParticleSystem leafParticles;
+    public int spawnCountMin, spawnCountMax;
+    
+	void Start ()
+    {
+        leafParticles.Emit(Random.Range(spawnCountMin, spawnCountMax));
+        var leafModule = leafParticles.main;
+        leafModule.simulationSpeed = 0f;
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            collide();
+	}
+
+    void collide()
+    {
+        var leafModule = leafParticles.main;
+        leafModule.simulationSpeed = 1f;
+    }
+}
