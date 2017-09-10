9451dfe3ae9c33701fbe1dece44984c4204e9942
Barleytree <barleywald@gmail.com>
2017-09-11 05:21:53 +0900

    Refactored animations. Added sparkles.

--- Assets/Resources/Microgames/DollDance/Scripts/DollDancePerformance.cs
+++ Assets/Resources/Microgames/DollDance/Scripts/DollDancePerformance.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class DollDancePerformance : MonoBehaviour
 {
 
+    const int POINT_LAYER = 1;
+    const int DOLL_LAYER = 2;
+
     DollDanceSequence sequence;
 
     Animator animator;
@@ -19,17 +22,24 @@ public class DollDancePerformance : MonoBehaviour
     
     IEnumerator RunPreview(List<DollDanceSequence.Move> moves)
     {
+        this.animator.SetBool("Preview", true);
+        float moveLength = this.animator.GetCurrentAnimatorStateInfo(POINT_LAYER).length;
+        yield return new WaitForSeconds(moveLength);// this.animator.GetCurrentAnimatorStateInfo(POINT_LAYER).length);
+
         foreach (DollDanceSequence.Move move in moves)
         {
-            this.animator.Play(move.ToString(), 1);
-            yield return new WaitForSeconds(this.animator.GetCurrentAnimatorStateInfo(1).length);
+            this.animator.Play(move.ToString());
+            yield return new WaitForSeconds(moveLength * 2);// this.animator.GetNextAnimatorStateInfo(POINT_LAYER).length);
         }
 
+        //yield return new WaitForSeconds(moveLength * 2);
         this.OnPreviewComplete();
     }
     
     void OnPreviewComplete()
     {
+        this.animator.SetBool("Preview", false);
+
         // Start listening for sequence input
         this.sequenceListener = this.gameObject.AddComponent<DollDanceSequenceListener>();
     }
@@ -41,7 +51,7 @@ public class DollDancePerformance : MonoBehaviour
 
     public void Release(DollDanceSequence.Move move)
     {
-        if (this.animator.GetCurrentAnimatorStateInfo(2).IsName(move.ToString()))
+        if (this.animator.GetCurrentAnimatorStateInfo(DOLL_LAYER).IsName(move.ToString()))
             this.animator.SetTrigger(DollDanceSequence.Move.Idle.ToString());
     }
 
