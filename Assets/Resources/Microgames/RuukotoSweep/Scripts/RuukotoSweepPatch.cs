731255314a86bdf5c71f7eaf1fa96960a787314b
Brian Intile <brianintile@gmail.com>
2017-09-26 01:13:27 -0400

    Ranxomize leaf patch spawn location

--- Assets/Resources/Microgames/RuukotoSweep/Scripts/RuukotoSweepPatch.cs
+++ Assets/Resources/Microgames/RuukotoSweep/Scripts/RuukotoSweepPatch.cs
@@ -7,13 +7,37 @@ public class RuukotoSweepPatch : MonoBehaviour
     public RuukotoSweep_Movement playerMovement;
     public ParticleSystem leafParticles;
     public int spawnCountMin, spawnCountMax;
+    public Vector2 xSpawnBounds, ySpawnBounds;
     
 	void Start ()
     {
         leafParticles.Emit(Random.Range(spawnCountMin, spawnCountMax));
         var leafModule = leafParticles.main;
         leafModule.simulationSpeed = 0f;
+
+        setStartPosition();
 	}
+
+    void setStartPosition()
+    {
+        do
+        {
+            transform.position = new Vector3(MathHelper.randomRangeFromVector(xSpawnBounds), MathHelper.randomRangeFromVector(ySpawnBounds), transform.position.z);
+        }
+        while (isTooClose());
+    }
+
+    bool isTooClose()
+    {
+        for (int i = 0; i < transform.parent.childCount; i++)
+        {
+            //Debug.Log(((Vector2)(transform.parent.GetChild(i).position - transform.position)).magnitude);
+            Transform childTransform = transform.parent.GetChild(i);
+            if (childTransform != transform && ((Vector2)(childTransform.position - transform.position)).magnitude < 1f)
+                return true;
+        }
+        return ((Vector2)(transform.position - playerMovement.transform.position)).magnitude < 2f;
+    }
 	
 	void Update ()
     {
