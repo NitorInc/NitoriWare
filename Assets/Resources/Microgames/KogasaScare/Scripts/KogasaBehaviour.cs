321b400b77882d2166e6286b8eb52d2a717d8d02
plrusek <plrusek@gmail.com>
2017-09-30 18:11:40 +0200

    Prototype for KogasaScare
    started to add early "scare" functionality, had troubles with it.

---
+++ Assets/Resources/Microgames/KogasaScare/Scripts/KogasaBehaviour.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KogasaBehaviour : MonoBehaviour {
+
+    public Animator kogasaAnimator;
+    public bool iswalking = false;
+    public static bool isscaring = false;
+    public string kogasawalkanim;
+    public string kogasawalkanimreverse;
+    public Transform kogasaTransform;
+    public Sprite kogSprite1;
+    public Sprite kogSprite2;
+    public Sprite kogSprite3;
+    public float moveSpeed;
+    public float movesped;
+    public float maxposXleft;
+    public float maxposXright;
+    private SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Start () {
+        kogasaAnimator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //maxposXleft = -5.214749; //set in inspector
+        //maxposXright = 6.21187;  //set in inspector
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown("space"))
+        {
+            isscaring = true;
+        }
+        else isscaring = false;
+
+            if (Input.GetKey("left") && !Input.GetKey("right"))
+            {
+            if (kogasaTransform.position.x >= maxposXleft)
+            {
+                kogasaAnimator.Play(kogasawalkanim);
+                //kogasaAnimator.SetFloat("Direction", 1.0f);
+                if ((kogasaTransform.position.x >= maxposXleft) && (spriteRenderer.sprite == kogSprite1) || (spriteRenderer.sprite == kogSprite2) || (spriteRenderer.sprite == kogSprite3))
+                {
+                    kogasaTransform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+                }
+            }
+            }
+            if (Input.GetKeyUp("left"))
+            {
+                kogasaAnimator.Play("idle");
+            }
+            if (Input.GetKey("right") && !Input.GetKey("left"))
+            {
+            if (kogasaTransform.position.x <= maxposXright)
+            {
+                kogasaAnimator.Play(kogasawalkanimreverse);
+                //kogasaAnimator.SetFloat("Direction", -1.0f); //for some reason, there's a weird stutter at the start of the animation. i'm going with two animations instead
+                if ((spriteRenderer.sprite == kogSprite1) || (spriteRenderer.sprite == kogSprite2) || (spriteRenderer.sprite == kogSprite3))
+                {
+                    kogasaTransform.Translate(Vector2.left * -moveSpeed * Time.deltaTime);
+                }
+            }
+            }
+            if (Input.GetKeyUp("right"))
+            {
+                kogasaAnimator.Play("idle");
+            }
+
+            if (Input.GetKey("left") && Input.GetKey("right"))
+            {
+                kogasaAnimator.Play("idle");
+            }
+        }
+}
---
+++ Assets/Resources/Microgames/KogasaScare/Scripts/VictimBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictimBehavior : MonoBehaviour {
+
+    public Transform victimTransform;
+    public float minvictimspawnx; //-4.64
+    public float maxvictimspawnx; // 4.64
+    public float xspawn;
+    Vector2 pos;
+
+    // Use this for initialization
+    void Start () {
+        victimTransform = GetComponent<Transform>();
+        xspawn = Random.Range(minvictimspawnx, maxvictimspawnx);
+        pos = new Vector2(xspawn, -0.55f);
+        victimTransform.position = pos;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        collide(other);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        collide(other);
+    }
+
+    void collide(Collider2D other)
+    {
+        if (KogasaBehaviour.isscaring == true && other.name == "KogasaObject")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
