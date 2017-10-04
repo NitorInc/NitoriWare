b64434877b6ae9cbfd37562d284d21742434d425
DracoGideon <DracoGideon@gmail.com>
2017-10-04 04:42:16 -0500

    NueAbduct added animations

---
+++ Assets/Resources/Microgames/NueAbduct/Scripts/UFOController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOController : MonoBehaviour {
+
+    public Transform[] spawnPoints;
+
+    [SerializeField]
+    Transform suckPoint;
+    public Transform SuckPoint {
+        get {
+            return suckPoint;
+        }
+    }
+
+    [SerializeField]
+    float gracePeriod = 0.2f;
+    public float GracePeriod {
+        get {
+            return gracePeriod;
+        }
+    }
+    [SerializeField]
+    float suckTime = 0.5f;
+    public float SuckTime {
+        get {
+            return suckTime;
+        }
+    }
+    [SerializeField]
+    float suckSpeed = 1.0f;
+    public float SuckSpeed {
+        get {
+            return suckSpeed;
+        }
+    }
+
+    public Animator[] anims;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.visible = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlaySuckAnimation() {
+        for (int i = 0; i < anims.Length; i++) {
+            anims[i].Play("Intensify");
+        }
+    }
+
+    public void PlayIdleAnimation() {
+        for (int i = 0; i < anims.Length; i++) {
+            anims[i].Play("Idle");
+        }
+    }
+}
---
+++ Assets/Resources/Microgames/NueAbduct/Scripts/VictimBehavior.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictimBehavior : MonoBehaviour {
+
+    
+    Vector2 targetPos;
+    Animator anim;
+    Vibrate vib;
+
+    public UFOController ufo;
+
+    public float wanderRadius = 0.3f;
+    public float wanderSpeed = 0.5f;
+    public float decisionTimeMin = 0.5f;
+    public float decisionTimeMax = 1.5f;
+
+    public enum State {
+        Idle,
+        Wander,
+        Sucking,
+        Sucked
+    }
+
+    public State currState;
+
+    Timer wanderTimer;
+    Timer graceTimer;
+    Timer suckTimer;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponentInChildren<Animator>();
+        vib = GetComponentInChildren<Vibrate>();
+        targetPos = transform.position;
+        wanderTimer = TimerManager.NewTimer(1.0f, Wander, 0, false, false);
+        graceTimer = TimerManager.NewTimer(ufo.GracePeriod, SuckFail, 0 , false, false);
+        suckTimer = TimerManager.NewTimer(ufo.SuckTime, SuckSucceed, 0, false, false);
+        SetState(State.Wander);
+	}
+
+    void Wander() {
+        targetPos = Random.insideUnitCircle * wanderRadius + new Vector2(transform.position.x, transform.position.y);
+        wanderTimer.SetTime(Random.Range(decisionTimeMin, decisionTimeMax));
+        wanderTimer.Start();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateState();
+    }
+
+    void SetState(State nextState) {
+        if (nextState != currState) {
+            ExitState();
+            EnterState(nextState);
+        }
+    }
+
+    void EnterState(State nextState) {
+        switch (nextState) {
+            case State.Idle:
+                break;
+            case State.Wander:
+                wanderTimer.Restart();
+                break;
+            case State.Sucked:
+                anim.Play("Sucked");
+                break;
+            case State.Sucking:
+                suckTimer.Restart();
+                vib.enabled = true;
+                ufo.PlaySuckAnimation();
+                break;
+        }
+        currState = nextState;
+    }
+
+    void UpdateState() {
+        switch (currState) {
+            case State.Idle:
+                break;
+            case State.Wander:
+                transform.position = Vector2.MoveTowards(transform.position, targetPos, wanderSpeed * Time.deltaTime);
+                break;
+            case State.Sucked:
+                transform.position = Vector2.MoveTowards(transform.position, ufo.SuckPoint.position, ufo.SuckSpeed * Time.deltaTime);
+                break;
+            case State.Sucking:
+                break;
+        }
+    }
+
+    void ExitState() {
+        switch (currState) {
+            case State.Idle:
+                break;
+            case State.Wander:
+                wanderTimer.Stop();
+                break;
+            case State.Sucked:
+                break;
+            case State.Sucking:
+                vib.enabled = false;
+                suckTimer.Stop();
+                graceTimer.Stop();
+                ufo.PlayIdleAnimation();
+                break;
+        }
+    }
+
+    void SuckFail() {
+        SetState(State.Wander);
+    }
+
+    void SuckSucceed () {
+        SetState(State.Sucked);
+    }
+
+    void SuckedAnimation () {
+        anim.Play("Sucked");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.GetComponentInChildren<VictimBehavior>() == null) {
+            if (currState == State.Sucked) {
+                Destroy(this.gameObject);
+            } else if (!other.name.Contains("Succ")) {
+                SetState(State.Sucking);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.GetComponentInChildren<VictimBehavior>() == null) {
+            if (currState == State.Sucking) {
+                if (!other.name.Contains("Succ")) {
+                    graceTimer.Restart();
+                }
+            }
+        }
+    }
+
+    private void OnDestroy() {
+        wanderTimer.Stop();
+        graceTimer.Stop();
+        suckTimer.Stop();
+    }
+}
