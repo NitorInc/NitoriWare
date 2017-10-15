88707f98e9ed5ec1bd7f43fe81d3a7cb8c3a6ef7
Brian Intile <brianintile@gmail.com>
2017-10-14 21:42:00 -0400

    Remove extraneous effects objects

--- Assets/Resources/Microgames/DollDance/Scripts/DollDanceController.cs
+++ Assets/Resources/Microgames/DollDance/Scripts/DollDanceController.cs
@@ -24,18 +24,18 @@ public class DollDanceController: MonoBehaviour
     
     public void Victory()
     {
-        this.victoryEffects.SetActive(true);
-        //performance.getAliceShadeComponent().setShaded(false);
-
         MicrogameController.instance.setVictory(true, true);
+
+        if (victoryEffects != null)
+            this.victoryEffects.SetActive(true);
     }
 
     public void Defeat()
     {
-        this.defeatEffects.SetActive(true);
-        //performance.getAliceShadeComponent().setShaded(false);
-
         MicrogameController.instance.setVictory(false, true);
+
+        if (defeatEffects != null)
+            this.defeatEffects.SetActive(true);
     }
 
 }
