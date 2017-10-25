075bfa0198d6be0d9798af047565cac194dee6fc
Niektóry <niektory@gmail.com>
2017-10-25 22:24:24 +0200

    Setup a script to be called when dropping mask fragments
    This will be used to snap the fragments together when dropped close enough to each other.

---
+++ Assets/Resources/Microgames/MaskPuzzle/Scripts/MaskPuzzleMaskFragment.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskPuzzleMaskFragment : MonoBehaviour {
+
+    private int snapCounter = 0;
+
+    public void SnapToOtherFragments()
+    {
+        print(snapCounter++);
+    }
+}
