3cb71dec2ab945ae1d6c954ed91e490825d704be
Niektóry <niektory@gmail.com>
2017-10-25 22:56:33 +0200

    Implement the method to snap the fragments together
    Quick and dirty implementation. When dropping a fragment within a certain distance to another fragment, the dropped fragment will snap to the other one (its position will be set to the other one's position).

--- Assets/Resources/Microgames/MaskPuzzle/Scripts/MaskPuzzleMaskFragment.cs
+++ Assets/Resources/Microgames/MaskPuzzle/Scripts/MaskPuzzleMaskFragment.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class MaskPuzzleMaskFragment : MonoBehaviour {
 
-    private int snapCounter = 0;
+    // ugly, but will do for now
+    [Header("The other mask fragments")]
+    [SerializeField]
+    private GameObject otherFragment1, otherFragment2;
 
+    [Header("How close the fragments need to be to snap together?")]
+    [SerializeField]
+    private float maxSnapDistance = 1f;
+
+    // To be called when dropping a mask
+    // Checks whether any other fragments are near the drop position
+    // If yes, snaps this fragment to the other one
     public void SnapToOtherFragments()
     {
-        print(snapCounter++);
+        // debug stuff
+        print("Positions for " + name);
+        print("own: " + (Vector2)transform.position);
+        print("other1: " + (Vector2)otherFragment1.transform.position);
+        print("other2: " + (Vector2)otherFragment2.transform.position);
+
+        // TODO: use a list or something
+        if (Vector2.Distance(transform.position, otherFragment1.transform.position) <= maxSnapDistance)
+        {
+            transform.position = (Vector2)otherFragment1.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, otherFragment2.transform.position) <= maxSnapDistance)
+        {
+            transform.position = (Vector2)otherFragment2.transform.position;
+        }
     }
 }
