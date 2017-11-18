c53be428ce392c2ec3d21d76c658e24dcde20224
Barleytree <barleywald@gmail.com>
2017-11-19 02:05:20 +0900

    Added initial control scripts

---
+++ Assets/Resources/Microgames/HecShapes/Scripts/HecShapesPlanet.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class HecShapesPlanet : MonoBehaviour
+{
+
+    [Header("How quickly the planet snaps into the holder")]
+    [SerializeField]
+    float snapSpeed;
+
+    Transform snapTarget;
+
+    void Update()
+    {
+        if (this.snapTarget != null)
+        {
+            float distance = Time.deltaTime * this.snapSpeed;
+            this.transform.position = Vector2.MoveTowards(
+                this.transform.position,
+                this.snapTarget.position,
+                distance);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponentInParent<HecShapesHolder>())
+            this.snapTarget = collision.transform;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponentInParent<HecShapesHolder>())
+            this.snapTarget = null;
+    }
+
+    public void OnGrab()
+    {
+        print("Grab");
+    }
+
+    public void OnRelease()
+    {
+        print("Release");
+    }
+
+}
