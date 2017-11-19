5722d0712a7160a03b6657fb4aa344199668c22a
Barleytree <barleywald@gmail.com>
2017-11-19 21:55:31 +0900

    Minor tweaks, added default color

--- Assets/Resources/Microgames/HecShapes/Scripts/HecShapesHolder.cs
+++ Assets/Resources/Microgames/HecShapes/Scripts/HecShapesHolder.cs
@@ -2,5 +2,19 @@
 
 public class HecShapesHolder : MonoBehaviour
 {
-    
+
+    [SerializeField]
+    SpriteRenderer hair;
+    [SerializeField]
+    SpriteRenderer eyes;
+
+    [SerializeField]
+    Color color;
+
+    void Update()
+    {
+        this.hair.color = this.color;
+        this.eyes.color = this.color;
+    }
+
 }
