9fcb5f68ea7bfed0a81508539a04a648f46f15ce
Niektóry <niektory@gmail.com>
2017-11-30 23:41:38 +0100

    Add a component to describe mask edges that should be connected

---
+++ Assets/Resources/Microgames/MaskPuzzle/Scripts/MaskPuzzleMaskEdges.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskPuzzleMaskEdges : MonoBehaviour
+{
+    [Header("Ignore the edges and always allow two mask pieces to connect")]
+    public bool overrideEdgeCheck = true;
+
+    [Header("All possible edges that can connect with one-another")]
+    public Edge[] connectableEdges;
+
+    [System.Serializable]
+    public class Edge
+    {
+        public Transform fragment1;
+        public Transform fragment2;
+    }
+}
