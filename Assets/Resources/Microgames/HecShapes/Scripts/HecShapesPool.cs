8d2343f537a2489928c9adfa6aecea948f6a8708
Barleytree <barleywald@gmail.com>
2017-12-10 07:55:42 +0900

    Made shape distribution random

--- Assets/Resources/Microgames/HecShapes/Scripts/HecShapesPool.cs
+++ Assets/Resources/Microgames/HecShapes/Scripts/HecShapesPool.cs
@@ -3,64 +3,110 @@ using UnityEngine;
 
 public class HecShapesPool : MonoBehaviour
 {
-    
+
+    [Header("Desired space between random shape locations")]
+    public float startSpacing;
+    [Header("Number of times to attempt shape positioning")]
+    public int spaceAttempts = 1;
+
+    [Header("Generic slottable prefab")]
     [SerializeField]
     HecShapesSlottable slottableTemplate;
-
+    [Header("Styled shape prefab")]
     [SerializeField]
     List<HecShapesCelestialBody> availableShapes;
-    [SerializeField]
-    List<Transform> startPositions;
-
+    
     MouseGrabbableGroup grabGroup;
-
+    Collider2D zone;
+    
     List<HecShapesCelestialBody> shapes;
     int correctIndex;
 
     void Awake()
     {
         this.grabGroup = GetComponent<MouseGrabbableGroup>();
+        this.zone = GetComponent<Collider2D>();
 
+        GenerateShapes();
+    }
+
+    void Start()
+    {
+        // Make the planets
+        List<Vector2> takenPositions = new List<Vector2>();
+        for (int i = 0; i < this.shapes.Count; i++)
+        {
+            // Calculate random start position
+            Vector2 start = FindSpace(this.zone.bounds, takenPositions);
+            takenPositions.Add(start);
+
+            InstantiateShape(this.shapes[i], start, i == this.correctIndex);
+        }
+    }
+
+    void GenerateShapes()
+    {
         this.shapes = new List<HecShapesCelestialBody>();
         while (availableShapes.Count > 0)
         {
             int index = Random.Range(0, availableShapes.Count);
             this.shapes.Add(availableShapes[index]);
             availableShapes.RemoveAt(index);
-
-            if (this.shapes.Count == this.startPositions.Count)
-                break;
         }
 
+        // Choose the correct shape at random
         this.correctIndex = Random.Range(0, this.shapes.Count);
     }
 
-    void Start()
+    Vector2 FindSpace(Bounds bounds, List<Vector2> takenPositions)
     {
-        // Make the planets
-        for (int i = 0; i < this.shapes.Count; i++)
+        // Attempt to find a position that isn't too close to occupied positions
+        Vector2 space = Vector2.zero;
+        for (int i = 0; i < this.spaceAttempts; i++)
         {
-            // This is the planet's generic object
-            HecShapesSlottable slottable = Instantiate(
-                    this.slottableTemplate,
-                    this.startPositions[i]);
+            space = new Vector2(
+                Random.Range(bounds.min.x, bounds.max.x),
+                Random.Range(bounds.min.y, bounds.max.y));
 
-            if (i == this.correctIndex)
-                slottable.correct = true;
-
-            // Instantiate the planet's sprite and collider holding object as a child
-            var shape = Instantiate(this.shapes[i], slottable.transform);
-
-            // Make the planet grabbable
-            var grabbable = slottable.gameObject.AddComponent<MouseGrabbable>();
-            grabbable.disableOnLoss = true;
-            grabbable.disableOnVictory = true;
-
-            grabbable._collider2D = shape.GetComponent<Collider2D>();
-
-            // Add to grabbable group
-            this.grabGroup.addGrabbable(grabbable, false);
+            bool good = true;
+            foreach (Vector2 taken in takenPositions)
+            {
+                if (Vector2.Distance(space, taken) < this.startSpacing)
+                {
+                    good = false;
+                    break;
+                }
+            }
+            
+            if (good)
+                break;
         }
+
+        return space;
+    }
+    
+    void InstantiateShape(HecShapesCelestialBody shapeTemplate, Vector2 start, bool correct)
+    {
+        // This is the planet's generic object
+        HecShapesSlottable slottable = Instantiate(
+                this.slottableTemplate,
+                start,
+                new Quaternion(),
+                this.transform);
+        slottable.correct = correct;
+
+        // Instantiate the planet's sprite and collider holding object as a child
+        var shape = Instantiate(shapeTemplate, slottable.transform);
+
+        // Make the planet grabbable
+        var grabbable = slottable.gameObject.AddComponent<MouseGrabbable>();
+        grabbable.disableOnLoss = true;
+        grabbable.disableOnVictory = true;
+
+        grabbable._collider2D = shape.GetComponent<Collider2D>();
+
+        // Add to grabbable group
+        this.grabGroup.addGrabbable(grabbable, false);
     }
 
     public HecShapesSlottable.Shape GetCorrectShape()
