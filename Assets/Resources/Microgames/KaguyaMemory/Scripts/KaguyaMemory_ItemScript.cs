96b77ec55d9594329acf82df92b17928ab687a53
Joriel Alves <chairgtables9@gmail.com>
2017-12-10 20:32:28 -0800

    Kaguya Memory Fixes + Stages and Started Bugswat
    Kaguya Memory fixes based on previous review
    Added stages; each stage has more items and a smaller door gap (objects move on third stage)
    Bug Swat started; you can hit a non-moving Wriggle
    Will figure out to simulate bug movement soon

--- Assets/Resources/Microgames/KaguyaMemory/Scripts/KaguyaMemory_ItemScript.cs
+++ Assets/Resources/Microgames/KaguyaMemory/Scripts/KaguyaMemory_ItemScript.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class KaguyaMemory_ItemScript : MonoBehaviour {
 
     public GameObject rngMaster;
+    public GameObject KaguyaChan;
     public GameObject correctIndicator;
     public GameObject wrongIndicator;
     public bool isMoving = false;
     public bool isCorrect = false;
+    public float movementSpeed = 0;
 
     private Vector3 startingPosition;
     private bool isSelectable = false;
+    private Rigidbody2D rb2d;
+    private Quaternion defaultRotation;
+    private bool isFinished = false;
 
     [SerializeField]
     private AudioClip correctSound;
@@ -22,8 +27,22 @@ public class KaguyaMemory_ItemScript : MonoBehaviour {
     // Use this for initialization
     void Start () {
         GetComponent<SpriteRenderer>().enabled = false;
-        startingPosition = transform.position;
+        
+        rb2d = GetComponent<Rigidbody2D>();
 
+        if (GetComponent<CapsuleCollider2D> () != null)
+        {
+            GetComponent<CapsuleCollider2D>().enabled = false;
+        }
+        if (GetComponent<CircleCollider2D>() != null)
+        {
+            GetComponent<CircleCollider2D>().enabled = false;
+        }
+
+        defaultRotation = transform.rotation;
+        GetComponent<Rigidbody2D>().gravityScale = 0;
+
+        Invoke("obtainStartingPosition", 0.5f);
         Invoke("appearSelectable", 2.3f);
     }
 
@@ -36,6 +55,7 @@ public class KaguyaMemory_ItemScript : MonoBehaviour {
             {
                 theIndicator = Instantiate(correctIndicator);
                 MicrogameController.instance.setVictory(true, true);
+                KaguyaChan.GetComponent<KaguyaMemory_KaguyaEndAnimation>().isWin = true;
                 MicrogameController.instance.playSFX(correctSound, volume: 0.5f,
                 panStero: AudioHelper.getAudioPan(0));
             }
@@ -43,22 +63,107 @@ public class KaguyaMemory_ItemScript : MonoBehaviour {
             {
                 theIndicator = Instantiate(wrongIndicator);
                 MicrogameController.instance.setVictory(false, true);
+                KaguyaChan.GetComponent<KaguyaMemory_KaguyaEndAnimation>().isLose = true;
                 MicrogameController.instance.playSFX(wrongSound, volume: 0.5f,
                 panStero: AudioHelper.getAudioPan(0));
             }
             theIndicator.transform.position = transform.position;
+            rb2d.velocity = new Vector2(0, 0);
             rngMaster.GetComponent<KaguyaMemory_RNGDeciderScript>().finished = true;
+            isFinished = true;
             isSelectable = false;
         }
         
     }
 
+    void Update()
+    {
+        transform.rotation = defaultRotation;
+        rb2d.angularVelocity = 0;
+        if (isFinished == true)
+        {
+            rb2d.velocity = new Vector2(0, 0);
+            return;
+        }
+        if(rngMaster.GetComponent<KaguyaMemory_RNGDeciderScript>().finished == true)
+        {
+            isFinished = true;
+            rb2d.velocity = new Vector2(0, 0);
+            return;
+        }
+        //prevent objects from moving out of bounds
+        if (transform.position.x > 5f)
+        {
+            rb2d.velocity = new Vector2(-movementSpeed, rb2d.velocity.y);
+        }
+        if (transform.position.x < -5.8f)
+        {
+            rb2d.velocity = new Vector2(movementSpeed, rb2d.velocity.y);
+        }
+        if (transform.position.y > 3.5f)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, -movementSpeed);
+        }
+        if (transform.position.y < -3.8f)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, movementSpeed);
+        }
+        
+        //fix speed after bounces
+        if (rb2d.velocity.x > 0 && rb2d.velocity.x < movementSpeed)
+        {
+            rb2d.velocity = new Vector2(movementSpeed, rb2d.velocity.y);
+        }
+        if (rb2d.velocity.x < 0 && rb2d.velocity.x > -movementSpeed)
+        {
+            rb2d.velocity = new Vector2(-movementSpeed, rb2d.velocity.y);
+        }
+        if (rb2d.velocity.y > 0 && rb2d.velocity.y < movementSpeed)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, movementSpeed);
+        }
+        if (rb2d.velocity.y < 0 && rb2d.velocity.y > -movementSpeed)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, -movementSpeed);
+        }
+    }
     void appearSelectable()
     {
+        if (GetComponent<CapsuleCollider2D>() != null)
+        {
+            GetComponent<CapsuleCollider2D>().enabled = true;
+        }
+        if (GetComponent<CircleCollider2D>() != null)
+        {
+            GetComponent<CircleCollider2D>().enabled = true;
+        }
+
         transform.position = startingPosition;
         GetComponent<SpriteRenderer>().enabled = true;
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+
+        float vSpeed = movementSpeed;
+        float hSpeed = movementSpeed;
+        int randomNumber = Random.Range(0, 100);
+
+        if(randomNumber < 50)
+        {
+            hSpeed *= -1;
+        }
+
+        randomNumber = Random.Range(0, 100);
+
+        if (randomNumber < 50)
+        {
+            vSpeed *= -1;
+        }
+
+        rb2d.velocity = new Vector2(hSpeed, vSpeed);
         GetComponent<Rigidbody2D>().gravityScale = 0;
         isSelectable = true;
+    }
+
+    void obtainStartingPosition()
+    {
+        startingPosition = transform.position;
     }
 }
--- Assets/Resources/Microgames/KaguyaMemory/Scripts/KaguyaMemory_RNGDeciderScript.cs
+++ Assets/Resources/Microgames/KaguyaMemory/Scripts/KaguyaMemory_RNGDeciderScript.cs
@@ -6,6 +6,7 @@ public class KaguyaMemory_RNGDeciderScript : MonoBehaviour
 {
 
     public GameObject[] items;
+    public GameObject KaguyaChan;
     public int maxItems = 3;
     public float showDelay = 1;
     public float speedMultiplier = 1;
@@ -22,6 +23,24 @@ public class KaguyaMemory_RNGDeciderScript : MonoBehaviour
         //select an item
         chosenItem = items[Random.Range(0, maxItems)];
         chosenItem.GetComponent<KaguyaMemory_ItemScript>().isCorrect = true;
+        KaguyaChan.transform.position += new Vector3(15, 0, 0);
+
+        //randomize item positions
+        int randomLoops = Random.Range(50, 100);
+        for (int i = 0; i < randomLoops; i++)
+        {
+            for (int j = 0; j < maxItems; j++)
+            {
+                int randomChance = Random.Range(0, 100);
+                if (randomChance < 50)
+                {
+                    int randomItem = Random.Range(0, maxItems);
+                    Vector3 tempPosition = items[j].transform.position;
+                    items[j].transform.position = items[randomItem].transform.position;
+                    items[randomItem].transform.position = tempPosition;
+                }
+            }
+        }
 
         Invoke("ShowItem", showDelay);
     }
@@ -30,6 +49,7 @@ public class KaguyaMemory_RNGDeciderScript : MonoBehaviour
     {
         chosenItem.transform.position = new Vector3(-4, -1, 0);
         chosenItem.GetComponent<SpriteRenderer>().enabled = true;
+        KaguyaChan.transform.position -= new Vector3(15, 0, 0);
         chosenItem.GetComponent<Rigidbody2D>().velocity = new Vector2(10 * speedMultiplier, 8);
         chosenItem.GetComponent<Rigidbody2D>().gravityScale = 2 * speedMultiplier;
 
