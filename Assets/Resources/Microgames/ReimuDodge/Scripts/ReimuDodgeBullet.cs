55804699558f3dbd52ac6e042c02df7ff3023fb5
Saphust <ia.sotogunckel@gmail.com>
2017-12-25 21:21:21 -0300

    Third commit to ReimuDodge Added bullet scripts

---
+++ Assets/Resources/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuDodgeBullet : MonoBehaviour {
+
+	[Header("How fast the bullet goes")]
+	[SerializeField]
+	private float speed = 1f;
+
+	[Header("Firing delay in sceonds")]
+	[SerializeField]
+	private float delay = 1f;
+
+	private Vector2  trajectory;
+	// Use this for initialization
+	void Start ()
+	{
+		Invoke ("SetTrajectory", delay);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (trajectory != null)
+		{
+			Vector2 newPosition = (Vector2)transform.position + (trajectory * speed * Time.deltaTime);
+			transform.position = newPosition;
+		}
+	}
+
+	void SetTrajectory()
+	{
+		GameObject player = GameObject.Find ("Player");
+		trajectory = (player.transform.position - transform.position).normalized;
+	}
+}
