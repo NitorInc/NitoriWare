e4ea3672f6a62e602809d53c4473e4dc3f657fe2
Brian Intile <brianintile@gmail.com>
2017-12-29 22:10:19 -0500

    Add RNG Controller

---
+++ Assets/Resources/Microgames/RockPaperSatori/Scripts/RockPaperSatoriRNGController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockPaperSatoriRNGController : MonoBehaviour
+{
+    public Move opponentMove;
+    public enum Move
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+    
+	void Start ()
+    {
+        opponentMove = (Move)Random.Range((int)Move.Rock, (int)Move.Scissors + 1);
+	}
+}
