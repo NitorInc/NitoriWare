a76b3076579120b8759b33c2b30bcbd2b29e0992
Niektóry <niektory@gmail.com>
2018-01-03 04:50:49 +0100

    Instantiate characters at start

--- Assets/Resources/Microgames/MamiPoser/Scripts/MamiPoserController.cs
+++ Assets/Resources/Microgames/MamiPoser/Scripts/MamiPoserController.cs
@@ -11,4 +11,27 @@ public class MamiPoserController : MonoBehaviour {
 
     [Header("Other characters prefabs")]
     public MamiPoserCharacter[] characterPrefabs;
+
+    [Header("Number of character copies to be spawned")]
+    public int characterSpawnNumber;
+
+    private MamiPoserCharacter chosenCharacterPrefab;
+    private int mamizouIndex;
+    private List<MamiPoserCharacter> createdCharacters;
+
+    void Start()
+    {
+        // Determine which character to use and which of the copies is Mamizou
+        chosenCharacterPrefab = characterPrefabs[Random.Range(0, characterPrefabs.Length)];
+        print("Chosen character prefab: " + chosenCharacterPrefab);
+        mamizouIndex = Random.Range(0, characterSpawnNumber);
+        print("Mamizou index: " + mamizouIndex);
+
+        // Spawn the characters
+        createdCharacters = new List<MamiPoserCharacter>();
+        for (int i = 0; i < characterSpawnNumber; i++)
+        {
+            createdCharacters.Add(Instantiate(chosenCharacterPrefab));
+        }
+    }
 }
