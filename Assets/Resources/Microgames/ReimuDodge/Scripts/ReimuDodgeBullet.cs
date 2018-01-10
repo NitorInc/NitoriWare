2a251f162ac5ed33d643b7ef5bdb1a9ec2e7895a
Tetawex <tetawex@gmail.com>
2018-01-10 18:45:37 +0300

    Bullet script

---
+++ Assets/Resources/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ReimuDodgeBullet : MonoBehaviour
+{
+    [Header("How fast the bullet goes")]
+    [SerializeField]
+    private float speed = 1f;
+
+    [Header("Firing delay in seconds")]
+    [SerializeField]
+    private float delay = 1f;
+
+    private Vector2 trajectory = new Vector2(0, 0);
+    private GameObject player;
+
+    void Start()
+    {
+        Invoke("SetTrajectory", delay);
+    }
+
+    void Update()
+    {
+        transform.position = (Vector2)transform.position + (trajectory * Time.deltaTime);
+    }
+
+    void SetTrajectory()
+    {
+        GameObject player = GameObject.Find("Player");
+        trajectory = (player.transform.position - transform.position).normalized;
+    }
+}
