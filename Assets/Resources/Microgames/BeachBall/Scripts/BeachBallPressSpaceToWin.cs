d414f5eb01942cb742d1e9de36572f7b30c8e242
PeacockRoy <gandwuser@yahoo.com>
2018-01-14 22:32:14 -0500

    Add files via upload

---
+++ Assets/Resources/Microgames/BeachBall/Scripts/BeachBallPressSpaceToWin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeachBallPressSpaceToWin : MonoBehaviour {
+
+	public GameObject BeachBallBeachBall;
+	public Animator BeachBallBeachBallAnimator;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			// Ball goes up and hopefully through the hoop
+
+			PlayBallAnimation();
+		}
+		
+	}
+
+	public void PlayBallAnimation()
+	{BeachBallBeachBallAnimator.Play("BeachBallBallGoesUp");
+	}
+}
