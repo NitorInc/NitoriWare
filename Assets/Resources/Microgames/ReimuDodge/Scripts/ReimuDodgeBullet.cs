a72b66b5aeede46634f20c4ace98c8ee4c25f3f4
Sizayoi <sizayoi64@gmail.com>
2018-01-16 09:07:06 -0200

    Added bullet script

--- Assets/Resources/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
+++ Assets/Resources/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReimuDodgeBullet : MonoBehaviour {
+public class ReimuDodgeBullet : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private float speed = 1, delay = 1;
+
+    private Vector2 trajectory;
+
+    void Start()
+    {
+        Invoke("SetTrajectory", delay);
+    }
+
+    void SetTrajectory()
+    {
+        trajectory = (GameObject.FindGameObjectWithTag("Player").transform.position - transform.position).normalized;
+    }
+
+    void Update()
+    {
+        transform.position += (Vector3)trajectory * Time.deltaTime * speed;
+    }
 }
