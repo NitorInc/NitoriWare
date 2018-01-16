c6c8c3b9991771e05343c57af7b1045961bcfc80
Sizayoi <sizayoi64@gmail.com>
2018-01-16 09:15:09 -0200

    Added player script

---
+++ Assets/Resources/Microgames/ReimuDodge/Scripts/ReimuDodgePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuDodgePlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip deathSound;
+    private bool alive = true;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (alive)
+            Kill();
+    }
+    void Kill()
+    {
+        MicrogameController.instance.setVictory(false, true);
+        alive = false;
+        GetComponentInChildren<SpriteRenderer>().gameObject.SetActive(false);
+        GetComponent<FollowCursor>().enabled = false;
+        MicrogameController.instance.playSFX(deathSound, volume: 0.5f,
+            panStereo: AudioHelper.getAudioPan(transform.position.x));
+        GetComponentInChildren<ParticleSystem>().Play();
+    }
+}
