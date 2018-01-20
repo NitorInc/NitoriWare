78f2447c9c4c825a714c1a51779796750cc2a55f
AdurnaNagra <33379708+AdurnaNagra@users.noreply.github.com>
2018-01-20 16:59:28 -0600

    More reading

---
+++ Assets/Resources/Microgames/DatingSim/Scripts/DatingSimGirl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class DatingSimGirl : MonoBehaviour {
+
+    [MenuItem("Tools/Read file")]
+    static void ReadString()
+    {
+        string path = "Assets/Resources/Microgames/DatingSim/Scripts/DatingSimDialogue.txt";
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path); //change this so it doesn't print out
+        Debug.Log(reader.ReadToEnd());
+        reader.Close();
+    }//from here put into a method (or modify into the struct) of temp
+
+
+    /// <summary>
+    /// string text = System.IO.File.ReadAllText("myfile.txt");
+    /// use that as well
+    /// 
+    /// other reading:
+    ///       using (theReader)
+    ///  {
+    ///      line = theReader.ReadLine();
+    ///      if(line != null){
+    ///              // While there's lines left in the text file, do this:
+    ///              do
+    ///              {
+    ///                     // Do whatever you need to do with the text line, it's a string now
+    ///                     // In this example, I split it into arguments based on comma
+    ///                     // deliniators, then send that array to DoStuff()
+    ///                     string[] entries = line.Split(',');
+    ///                     if (entries.Length > 0)
+    ///                     DoStuff(entries);
+    ///line = theReader.ReadLine();
+    ///              }
+    ///              while (line != null);
+    ///      } 
+    ///      // Done reading, close the reader and return true to broadcast success    
+    ///      theReader.Close();
+    ///      return true;
+    ///      }
+    ///}
+    ///
+    /// other reading:
+    /// import System.IO;
+    ///var fileName = "foo.txt";
+    ///
+    ///function Start()
+    ///{
+    ///    var sr = new StreamReader(Application.dataPath + "/" + fileName);
+    ///    var fileContents = sr.ReadToEnd();
+    ///    sr.Close();
+    ///
+    ///    var lines = fileContents.Split("\n"[0]);
+    ///    for (line in lines)
+    ///    {
+    ///        print(line);
+    ///    }
+    ///}
+    /// probably use the last one; it looks like what we need
+    /// We need to be able to read in the lines, (probably on a line by line basis) modify the input to remove the "*" and ":", separate each set into entries,
+    /// then put those entry sets into structs for each girl, these structs would be put into a dyn array to rng select from and choose
+    /// </summary>
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
