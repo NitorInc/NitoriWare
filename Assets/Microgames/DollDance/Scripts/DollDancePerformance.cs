533805db2f96d8ac76426db2777cd8ca9acd61df
Brian Intile <brianintile@gmail.com>
2018-01-23 01:42:04 -0500

    Fix bug calling nonexistent animation

--- Assets/Microgames/DollDance/Scripts/DollDancePerformance.cs
+++ Assets/Microgames/DollDance/Scripts/DollDancePerformance.cs
@@ -104,7 +104,6 @@ public class DollDancePerformance : MonoBehaviour
         yield return new WaitForSeconds(cueSettleDelay);
 
         this.animator.Play("Settle");
-        this.animator.Play("LightDoll");
         //this.animator.Play("Forward", EYE_LAYER);
         playEyeAnimation("LookDoll");
 
