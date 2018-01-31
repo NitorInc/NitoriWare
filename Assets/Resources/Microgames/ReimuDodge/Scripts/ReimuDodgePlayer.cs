6ffc6b43e8728bf9882979957962d7e8fe4c0daf
Copper Chihuahua <copperlightsource@gmail.com>
2018-01-31 18:17:45 -0500

    Part 4 - Add player script and death consequences

---
+++ Assets/Resources/Microgames/ReimuDodge/Scripts/ReimuDodgePlayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class ReimuDodgePlayer : MonoBehaviour 
+{
+		
+	[SerializeField]
+	private AudioClip deathSound;
+
+	private bool alive = true;
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if (alive)
+		{
+			Kill();
+
+			MicrogameController.instance.setVictory(victory: false, final: true);
+		}
+	}
+
+	void Kill()
+	{
+		alive = false;
+
+		SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+		spriteRenderer.gameObject.SetActive (false);
+
+		FollowCursor followCursor = GetComponent<FollowCursor> ();
+		followCursor.enabled = false;
+
+		MicrogameController.instance.playSFX(deathSound, volume: 0.5f, panStereo: AudioHelper.getAudioPan(transform.position.x));
+		ParticleSystem particleSystem = GetComponentInChildren<ParticleSystem>();
+		particleSystem.Play();
+
+	}
+
+}
