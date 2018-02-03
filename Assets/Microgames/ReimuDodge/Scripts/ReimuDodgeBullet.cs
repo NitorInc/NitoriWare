661f1195ff98d0f7851c34148b180107a99b3248
stellarLuminant <stellarLuminant@gmail.com>
2018-02-03 14:44:43 -0800

    Part 4 of ReimuDodge tutorial
    Collision done, as well as a bit of time spent on the ParticleSystem. Also added a bit of failureVoiceDelay so that the death sound doesn't play on top of voice clip.

--- Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReimuDodgeBullet : MonoBehaviour {
-
+public class ReimuDodgeBullet : MonoBehaviour 
+{
     [Header("How fast the bullet goes")]
     [SerializeField]
     private float speed = 1f;
@@ -16,21 +16,25 @@ public class ReimuDodgeBullet : MonoBehaviour {
     private Vector2 trajectory;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         Invoke("SetTrajectory", delay);
     }
 	
     // Update is called once per frame
-    void Update() {
+    void Update() 
+    {
         // Only start moving after the trajectory has been set
-        if (trajectory != null) {
+        if (trajectory != null) 
+        {
             // Move the bullet a certain distance based on trajectory speed and time
             Vector2 newPosition = (Vector2)transform.position + (trajectory * speed * Time.deltaTime);
             transform.position = newPosition;
         }
     }
 
-    void SetTrajectory() {
+    void SetTrajectory()
+    {
         // Find the player object in the scene and calculate a trajectory towards them
         GameObject player = GameObject.Find("Player");
         trajectory = (player.transform.position - transform.position).normalized; 
