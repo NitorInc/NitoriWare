4ae1cb9a2d9203a510fc28a3331975f98c9a0c46
hanss314 <24215006+hanss314@users.noreply.github.com>
2018-02-10 12:57:32 -0800

    Added teleportation on click

--- Assets/Microgames/DoorKnock/Scripts/DoorKnockDoor.cs
+++ Assets/Microgames/DoorKnock/Scripts/DoorKnockDoor.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class DoorKnockDoor : MonoBehaviour {
     
     [SerializeField]
-    private Collider2D collider;
-
-	// Use this for initialization
+    private Collider2D clickCollider;
+	
+    private float screenWidth;
+    private float screenHeight;
+    
+    // Use this for initialization
 	void Start() {
-       		
-	}
+        // Get the screen dimensions
+        screenHeight = Camera.main.orthographicSize;    
+        screenWidth = screenHeight * Screen.width / Screen.height;
+ 	}
 	
 	// Update is called once per frame
 	void Update() {
-        if (Input.GetMouseButtonDown(0) && CameraHelper.isMouseOver(collider))
+        if (Input.GetMouseButtonDown(0) && CameraHelper.isMouseOver(clickCollider))
         {
            OnClick(); 
         } 
@@ -23,11 +27,15 @@ public class DoorKnockDoor : MonoBehaviour {
     
     // When the object is clicked
     void OnClick() {
+        print("Clicked.");
         Teleport();
     }
     
     // Move to a random location
     void Teleport() {
-    
+        print(screenWidth);
+        float newx = Random.Range(-screenWidth, screenWidth) / 2;
+        float newy = Random.Range(-screenHeight, screenHeight) / 2;
+        transform.position = new Vector2(newx, newy);
     }
 }
