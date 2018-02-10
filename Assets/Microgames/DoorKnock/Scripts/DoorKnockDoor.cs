bb4d724f5ba5254957a53226429f04198e80791b
hanss314 <24215006+hanss314@users.noreply.github.com>
2018-02-10 12:27:28 -0800

    Basic click detection

---
+++ Assets/Microgames/DoorKnock/Scripts/DoorKnockDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DoorKnockDoor : MonoBehaviour {
+    
+    [SerializeField]
+    private Collider2D collider;
+
+	// Use this for initialization
+	void Start() {
+       		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        if (Input.GetMouseButtonDown(0) && CameraHelper.isMouseOver(collider))
+        {
+           OnClick(); 
+        } 
+	}
+    
+    // When the object is clicked
+    void OnClick() {
+        Teleport();
+    }
+    
+    // Move to a random location
+    void Teleport() {
+    
+    }
+}
