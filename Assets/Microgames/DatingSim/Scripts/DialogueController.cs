529056df45c09e098c9e13c0813c5927bef3186c
DracoGideon <DracoGideon@gmail.com>
2018-02-13 08:56:07 -0600

    Added DatingSim prototype, including gameplay, win/lose, basic layout and animation, but no audios.

---
+++ Assets/Microgames/DatingSim/Scripts/DialogueController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueController : MonoBehaviour {
+
+    private TMP_Text textComp;
+    private AdvancingText textPlayer;
+    DialoguePreset preset;
+
+    void Start() {
+        textComp = GetComponent<TMP_Text>();
+        textPlayer = GetComponent<AdvancingText>();
+        preset = FindObjectOfType<DialoguePreset>();
+        DialoguePreset.OnCharacterSelection += SetDialogue;
+    }
+
+    void SetDialogue(int index) {
+        textComp.text = preset.GetStartingDialogue(index);
+        textComp.maxVisibleCharacters = 0;
+        textPlayer.resetAdvance();
+    }
+
+    public void SetDialogue(string str) {
+        textComp.text = str;
+        textComp.maxVisibleCharacters = 0;
+        textPlayer.resetAdvance();
+    }
+}
