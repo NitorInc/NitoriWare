9dff3cc62b9cf18ffbac7b5ae1e56271191626d5
Brian Intile <brianintile@gmail.com>
2018-02-13 21:46:22 -0500

    Add victory and loss sounds

---
+++ Assets/Microgames/DatingSim/Scripts/DatingSimSoundPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatingSimSoundPlayer : MonoBehaviour
+{
+    public AudioClip winClip, lossClip;
+    
+	void Start ()
+    {
+        OptionController.OnWinning += PlayWinSound;
+        OptionController.OnLosing += PlayLossSound;
+    }
+	
+    void PlayWinSound()
+    {
+        MicrogameController.instance.playSFX(winClip);
+    }
+
+    void PlayLossSound()
+    {
+        MicrogameController.instance.playSFX(lossClip);
+    }
+}
