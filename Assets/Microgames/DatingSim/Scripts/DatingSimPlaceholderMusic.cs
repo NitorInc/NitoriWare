8b56207b87d198060a416df22a5752d77d7ea6f4
Brian Intile <brianintile@gmail.com>
2018-02-13 21:16:27 -0500

    Add placeholder music

---
+++ Assets/Microgames/DatingSim/Scripts/DatingSimPlaceholderMusic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatingSimPlaceholderMusic : MonoBehaviour
+{
+    public float startDelayInBeats = 0f;
+    public List<AudioClip> characterClips;
+
+    private AudioSource audioSource;
+    
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+        DialoguePreset.OnCharacterSelection += SelectTrack;
+	}
+
+    void SelectTrack(int index)
+    {
+        audioSource.clip = characterClips[index];
+        audioSource.pitch = Time.timeScale;
+        Invoke("PlayTrack", startDelayInBeats * (60f / 130f));
+    }
+
+    void PlayTrack()
+    {
+        audioSource.Play();
+    }
+}
