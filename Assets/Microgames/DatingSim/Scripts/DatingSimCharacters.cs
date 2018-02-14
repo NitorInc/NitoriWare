50b6fccf43440eb95f916942983c8729ebfb8aa1
Brian Intile <brianintile@gmail.com>
2018-02-14 18:54:57 -0500

    Add character class and roster prefab

---
+++ Assets/Microgames/DatingSim/Scripts/DatingSimCharacters.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatingSimCharacters : MonoBehaviour
+{
+    [SerializeField]
+    public List<Character> characters;
+
+    [System.Serializable]
+    public class Character
+    {
+        public string idName;
+        public string fullName;
+        [Multiline]
+        public string introDialogue;
+        public List<CharacterOption> rightOptions, wrongOptions;
+        public AudioClip musicClip;
+        public Sprite defaultPortait;
+        public Sprite winPortrait;
+        public Sprite lossPortrait;
+        public Sprite backgroundImage;
+        public Material optionBGMaterial;
+    }
+
+    [System.Serializable]
+    public class CharacterOption
+    {
+        [Multiline]
+        public string optionDialogue;
+        [Multiline]
+        public string responseDialogue;
+        
+        private bool correct;
+        public void setCorrect(bool correct)
+        {
+            this.correct = correct;
+        }
+        public bool isCorrect()
+        {
+            return correct;
+        }
+    }
+}
