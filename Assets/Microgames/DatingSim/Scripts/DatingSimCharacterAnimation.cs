4540b11b92dd3207b0639a0a5bfcbeabc1b6b890
Brian Intile <brianintile@gmail.com>
2018-02-13 19:31:27 -0500

    Tie loss animation to loss

---
+++ Assets/Microgames/DatingSim/Scripts/DatingSimCharacterAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatingSimCharacterAnimation : MonoBehaviour
+{
+    private Animator rigAnimator;
+
+    void Start()
+    {
+        rigAnimator = GetComponent<Animator>();
+        OptionController.OnWinning += WinAnimation;
+        OptionController.OnLosing += LossAnimation;
+    }
+
+    void WinAnimation()
+    {
+        rigAnimator.SetInteger("State", 1);
+    }
+
+    void LossAnimation()
+    {
+        rigAnimator.SetInteger("State", 2);
+    }
+
+}
