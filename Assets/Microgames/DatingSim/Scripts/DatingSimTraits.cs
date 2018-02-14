cd1eae7f557a530b9eaedd16d3c1de491c7ffedd
Brian Intile <brianintile@gmail.com>
2018-02-14 18:55:23 -0500

    Create override Traits

---
+++ Assets/Microgames/DatingSim/Scripts/DatingSimTraits.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatingSimTraits : MicrogameTraits
+{
+    public DatingSimCharacters characterRoster;
+
+    private DatingSimCharacters.Character selectedCharacter;
+
+    public override AudioClip musicClip
+    {
+        get
+        {
+            return selectedCharacter.musicClip;
+        }
+    }
+
+    public override void onAccessInStage(string microgameId)
+    {
+        selectedCharacter = characterRoster.characters[Random.Range(0, characterRoster.characters.Count)];
+        
+        base.onAccessInStage(microgameId);
+    }
+    
+    public DatingSimCharacters.Character getSelectedCharacter()
+    {
+        return selectedCharacter;
+    }
+}
