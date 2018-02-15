dfc0225dbe08f0ced8a9bc8056cd7d502b83fb42
Brian Intile <brianintile@gmail.com>
2018-02-15 00:03:25 -0500

    Add override character option to Traits

--- Assets/Microgames/DatingSim/Scripts/DatingSimTraits.cs
+++ Assets/Microgames/DatingSim/Scripts/DatingSimTraits.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DatingSimTraits : MicrogameTraits
 {
     public DatingSimCharacters characterRoster;
+    public int overrideCharacter = -1;
 
     private DatingSimCharacters.Character selectedCharacter;
 
@@ -18,7 +19,10 @@ public class DatingSimTraits : MicrogameTraits
 
     public override void onAccessInStage(string microgameId)
     {
-        selectedCharacter = characterRoster.characters[Random.Range(0, characterRoster.characters.Count)];
+        if (overrideCharacter > -1)
+            selectedCharacter = characterRoster.characters[overrideCharacter];
+        else
+            selectedCharacter = characterRoster.characters[Random.Range(0, characterRoster.characters.Count)];
         
         base.onAccessInStage(microgameId);
     }
