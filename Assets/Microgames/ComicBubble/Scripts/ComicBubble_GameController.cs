8cdcff3c06593a5ff14766ccb6736c08e881d1b1
Nialevolstel <nialevolstel@gmail.com>
2018-02-19 05:07:46 -0300

    Added new placeholders and update the scene

---
+++ Assets/Microgames/ComicBubble/Scripts/ComicBubble_GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComicBubble_GameController : MonoBehaviour {
+
+    [System.Serializable]
+    class ComicBubbleDataCollection
+    {
+        public GameObject speechBubble;
+        public GameObject speechTarget;
+        public Vector2 finalPosition;
+
+    }
+
+    [SerializeField]
+    List<ComicBubbleDataCollection> data;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
