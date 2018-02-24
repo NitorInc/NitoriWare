88fedaa27117f54e20deac3dca490d1cd8194843
Tetawex <tetawex@gmail.com>
2018-02-24 18:00:06 +0300

    addressed some issues mentioned and added difficulties

--- Assets/Microgames/BeachBall/Scripts/BeachBallHoopParamsRandomizer.cs
+++ Assets/Microgames/BeachBall/Scripts/BeachBallHoopParamsRandomizer.cs
@@ -5,15 +5,21 @@
 /// </summary>
 public class BeachBallHoopParamsRandomizer : MonoBehaviour
 {
+    [Header("Insert animation name here")]
+    public string AnimationName;
     void Start()
     {
-        var animation = GetComponent<Animation>()["hoop_move_lr"];
+        var animation = GetComponent<Animation>()[AnimationName];
         if (Random.Range(0f, 1f) < 0.5f)
-            animation.time = Random.Range(0f, animation.length / 4f);
+        {
+            animation.time = animation.length + Random.Range(0.2f, 0.2f);
+            animation.speed = 1;
+        }
         else
-            animation.time = Random.Range(animation.length *3/ 4f, animation.length);
-        if (Random.Range(0f, 1f) < 0.5f)
+        {
             animation.speed = -1;
+            animation.time = animation.length / 2 + Random.Range(-0.2f, 0.2f);
+        }
     }
 
     void Update()
