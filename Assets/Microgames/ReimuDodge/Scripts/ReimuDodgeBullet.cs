1e64e3202e027578b25f68eae72b1e8314220801
Myon <mendes.california@gmail.com>
2018-02-27 00:24:30 +0000

    Part 3 - Implemented bulled script

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ReimuDodgeBullet : MonoBehaviour
+{
+
+    [Header("How fast the bullet goes")]
+    [SerializeField]
+    private float speed = 1f;
+
+    [Header("Firing delay in seconds")]
+    [SerializeField]
+    private float delay = 1f;
+
+    // Stores the direction of travel for the bullet
+    private Vector2 trajectory;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Invoke the setTrajectory method after the delay
+        Invoke("SetTrajectory", delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Only start moving after the trajectory has been set
+        if (trajectory != null)
+        {
+            // Move the bullet a certain distance based on trajectory speed and time
+            Vector2 newPosition = (Vector2)transform.position + (trajectory * speed * Time.deltaTime);
+            transform.position = newPosition;
+        }
+    }
+
+    void SetTrajectory()
+    {
+        // Find the player object in the scene and calculate a trajectory towards them
+        GameObject player = GameObject.Find("Player");
+        trajectory = (player.transform.position - transform.position).normalized;
+    }
+}
