e0840b0057f4df78acd0d4914dd4aea57c3688c3
DracoGideon <DracoGideon@gmail.com>
2018-03-01 02:24:46 -0600

    Added scene setup

---
+++ Assets/Microgames/ClownTorch/Scripts/ClownTorchPlayerTorchController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClownTorchPlayerTorchController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        point.z = 0.0f;
+        transform.position = point;
+	}
+}
