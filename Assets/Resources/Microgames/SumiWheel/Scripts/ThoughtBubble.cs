db73241d1b17018ff296e32433e1631b3850d2d7
FamilyTeamYT <ytfamilyteam@gmail.com>
2018-03-03 10:14:01 -0300

    Part 2 - Re-trying animations

---
+++ Assets/Resources/Microgames/SumiWheel/Scripts/ThoughtBubble.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThoughtBubble : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        //choose cards
+        int sumithought1 = Random.Range(3, 6);
+
+        print(sumithought1);
+
+        int sumithought2 = sumithought1 - 2;
+        print(sumithought2);
+
+        //draw cards
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+}
---
+++ Assets/Resources/Microgames/SumiWheel/Scripts/WheelSpin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelSpin : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Determine difficulty, if easy, half speed, if normal, regular speed, if hard, double speed
+        //Rotation
+        transform.Rotate(Vector3.forward / 2);
+    }
+}
