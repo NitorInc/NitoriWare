9b394272cc82f4ed0dddf12e4e0440998483a6ce
ChairGTables <chairgtables9@gmail.com>
2018-03-03 23:15:56 -0800

    It is easier now to change timings on the editor with one object

--- Assets/Microgames/KaguyaMemory/Scripts/KaguyaMemory_ChangeColor.cs
+++ Assets/Microgames/KaguyaMemory/Scripts/KaguyaMemory_ChangeColor.cs
@@ -6,16 +6,8 @@ public class KaguyaMemory_ChangeColor : MonoBehaviour {
 
     public Vector3 RGB;
     public float alpha = 1;
-    public float changeDelay = 0;
-    //public GameObject RNGMaster;
 
-	// Use this for initialization
-	void Start () {
-        //changeDelay = RNGMaster.GetComponent<KaguyaMemory_RNGDeciderScript>().showDelay;
-        Invoke("InitiateChange", changeDelay);
-	}
-
-    void InitiateChange()
+    public void InitiateChange()
     {
         GetComponent<SpriteRenderer>().color = new Color(RGB.x, RGB.y, RGB.z, alpha);
         if (alpha == 0)
---
+++ Assets/Microgames/KaguyaMemory/Scripts/KaguyaMemory_Timing.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KaguyaMemory_Timing : MonoBehaviour {
+
+    public float initialDoorOpen = 0.0f;
+    public float doorCloseAfterInitialOpen = 0.0f;
+    public float doorOpenAfterClose = 0.0f;
+    public float throwItemAfterInitialOpen = 0.0f;
+}
