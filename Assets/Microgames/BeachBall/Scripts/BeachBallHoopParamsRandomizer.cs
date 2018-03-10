0d601adfd7636b599e5dc2023b86946fa2509395
Tetawex <tetawex@gmail.com>
2018-03-10 10:15:45 +0300

    Make ball throw speed adjustable through a single variable. Make hoop animation at stage 1 shorter and more linear

--- Assets/Microgames/BeachBall/Scripts/BeachBallHoopParamsRandomizer.cs
+++ Assets/Microgames/BeachBall/Scripts/BeachBallHoopParamsRandomizer.cs
@@ -5,19 +5,21 @@
 /// </summary>
 public class BeachBallHoopParamsRandomizer : MonoBehaviour
 {
-    [Header("Insert animation name here")]
+    [Header("Animation name")]
     public string AnimationName;
+    [Header("Animation scale")]
+    public float AnimationScale;
     void Start()
     {
         var animation = GetComponent<Animation>()[AnimationName];
         if (Random.Range(0f, 1f) < 0.5f)
         {
             animation.time = animation.length + Random.Range(0.2f, 0.2f);
-            animation.speed = 1;
+            animation.speed = 1 * AnimationScale;
         }
         else
         {
-            animation.speed = -1;
+            animation.speed = -1 * AnimationScale;
             animation.time = animation.length / 2 + Random.Range(-0.2f, 0.2f);
         }
     }
