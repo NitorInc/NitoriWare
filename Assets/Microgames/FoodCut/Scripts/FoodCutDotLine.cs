7bb458d784a1eddc68e7a8755d9e4aee937f81d8
Boombott <Briancbotting1@gmail.com>
2018-03-10 19:42:10 -0800

    Added dotted line; gave dotted line and cleaver colliders; added functionality of collider triggering when player presses space with cleaver over it

---
+++ Assets/Microgames/FoodCut/Scripts/FoodCutDotLine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodCutDotLine : MonoBehaviour {
+    Collider2D dotLineCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        dotLineCollider = GetComponent<Collider2D>();
+        dotLineCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Temporarily turns trigger on when Space is Pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dotLineCollider.isTrigger = true;
+            Debug.Log("Trigger On: " + dotLineCollider.isTrigger);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Trigger works!");
+    }
+}
