87952a4e0677ce0da679f6b01ce204ff4b952ba0
Brian Intile <brianintile@gmail.com>
2018-03-10 19:34:24 -0500

    Overhaul scripts and game logic

---
+++ Assets/Resources/Microgames/SumiWheel/Scripts/SumiWheelCardClick.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumiWheelCardClick : MonoBehaviour
+{
+    private SumiWheelCardController cardController;
+
+    private bool clickable = true;
+    private SumiWheelThoughtCard correctCard = null;
+
+    public void setController(SumiWheelCardController cardController)
+    {
+        this.cardController = cardController;
+    }
+
+    void OnMouseDown()
+    {
+        if (!clickable || MicrogameController.instance.getVictoryDetermined())
+            return;
+
+        cardController.clickCard(this);
+        clickable = false;
+
+        var spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        foreach (var spriteRenderer in spriteRenderers)
+        {
+            var color = spriteRenderer.color;
+            color.a = .5f;
+            spriteRenderer.color = color;
+        }
+
+
+    }
+
+    public void setAsCorrect(SumiWheelCardController cardController)
+    {
+    }
+
+    public bool isClickable()
+    {
+        return clickable;
+    }
+}
