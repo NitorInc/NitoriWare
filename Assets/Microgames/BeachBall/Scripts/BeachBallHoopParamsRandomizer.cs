ed025a8007f5a006a1a55a6e32b856876a04aaf4
Brian Intile <brianintile@gmail.com>
2018-03-13 14:56:43 -0400

    More adjustments to BeachBall

--- Assets/Microgames/BeachBall/Scripts/BeachBallHoopParamsRandomizer.cs
+++ Assets/Microgames/BeachBall/Scripts/BeachBallHoopParamsRandomizer.cs
@@ -12,16 +12,12 @@ public class BeachBallHoopParamsRandomizer : MonoBehaviour
     void Start()
     {
         var animation = GetComponent<Animation>()[AnimationName];
+        animation.time = animation.length + Random.Range(0.2f, 0.2f);
+        animation.speed = 1 * AnimationScale;
+
         if (Random.Range(0f, 1f) < 0.5f)
-        {
-            animation.time = animation.length + Random.Range(0.2f, 0.2f);
-            animation.speed = 1 * AnimationScale;
-        }
-        else
-        {
-            animation.speed = -1 * AnimationScale;
-            animation.time = animation.length / 2 + Random.Range(-0.2f, 0.2f);
-        }
+            transform.parent.localScale = new Vector3(
+                -transform.parent.localScale.x, transform.parent.localScale.y, transform.parent.localScale.z);
     }
 
     void Update()
