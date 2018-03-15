05e604d9b8ab1a254daa4820b94f83d5efcbcccd
Tetawex <tetawex@gmail.com>
2018-03-16 02:13:10 +0300

    Add stage 1 animations

---
+++ Assets/Microgames/BeachBall/Scripts/BeachBallWinLossAnim.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeachBallWinLossAnim : MonoBehaviour
+{
+    public AnimationClip WinAnimation;
+    public AnimationClip LossAnimation;
+
+    private Animation anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+
+        var eventWrapper = GameObject.Find(BeachBallWinLossEventWrapper.DefaultGameObjectName)
+            .GetComponent<BeachBallWinLossEventWrapper>();
+
+        //Add event listeners
+        eventWrapper.OnWin += (sender, args) =>
+        {
+            anim.clip = WinAnimation;
+            anim.Play();
+        };
+        eventWrapper.OnLoss += (sender, args) =>
+        {
+            anim.clip = LossAnimation;
+            anim.Play();
+        };
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Microgames/BeachBall/Scripts/BeachBallWinLossEventWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class BeachBallWinLossEventWrapper : MonoBehaviour
+{
+
+    public event EventHandler OnWin;
+    public event EventHandler OnLoss;
+
+    public const String DefaultGameObjectName = "WinLossEventWrapper";
+
+    //It does not work -_-
+    /*public static BeachBallWinLossEventWrapper Instance
+    {
+        get
+        {
+            return GameObject.Find(DefaultGameObjectName).GetComponent<BeachBallWinLossEventWrapper>();
+        }
+    }*/
+
+    private bool fired;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (!fired && MicrogameController.instance.getVictoryDetermined())
+        {
+            fired = true;
+            if (MicrogameController.instance.getVictory())
+                OnWin(this, new EventArgs());
+            else
+                OnLoss(this, new EventArgs());
+        }
+    }
+}
