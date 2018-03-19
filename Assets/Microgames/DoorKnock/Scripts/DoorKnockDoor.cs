2714a6040064d4a9c457f1e194f46452ebfa553e
hanss314 <24215006+hanss314@users.noreply.github.com>
2018-03-18 19:32:24 -0700

    Add animations to script

--- Assets/Microgames/DoorKnock/Scripts/DoorKnockDoor.cs
+++ Assets/Microgames/DoorKnock/Scripts/DoorKnockDoor.cs
@@ -60,6 +60,7 @@ public class DoorKnockDoor : MonoBehaviour {
             // We win
             win = true;
             MicrogameController.instance.setVictory(victory: true, final: true);
+            WinAnimation();
         }
         // Don't teleport if we've won
         else if (teleportOnClick && !win){
@@ -75,10 +76,30 @@ public class DoorKnockDoor : MonoBehaviour {
         transform.position = new Vector2(newx, newy);
         StartCoroutine(Appear());
     }
-
+    
+    // Set a different direction
     void NewDirection() {
         float angle = Random.Range(0.0f, 2*Mathf.PI);
         direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * speed;
+    }
+
+    // Winning animation
+    void WinAnimation(){
+        StartCoroutine(OpenDoors());
+    }
+
+    // Door opening animation
+    IEnumerator OpenDoors(){
+        int speed = 10;
+        Transform rigTransform = transform.Find("Rig").transform;
+        Transform doorL = rigTransform.Find("DoorPanelL").transform;
+        Transform doorR = rigTransform.Find("DoorPanelR").transform;
+        for (int i = 0; i < 180/speed; i++){
+            doorL.Rotate(new Vector3(0, speed, 0));
+            doorR.Rotate(new Vector3(0, -speed, 0));
+            yield return new WaitForFixedUpdate();
+        }
+        yield return null;
     }
 
     // popping back animation
