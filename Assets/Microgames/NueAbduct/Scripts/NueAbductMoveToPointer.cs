dd87bd0bf37e2d0136f0ba64bf86f09d94b595db
Brian Intile <brianintile@gmail.com>
2018-03-19 20:00:57 -0400

    Snap UFO to cursor in NueAbduct

--- Assets/Microgames/NueAbduct/Scripts/NueAbductMoveToPointer.cs
+++ Assets/Microgames/NueAbduct/Scripts/NueAbductMoveToPointer.cs
@@ -10,7 +10,7 @@ namespace NitorInc.NueAbduct {
         public float speed = 2.0f;
 
         void Update() {
-            var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var targetPos = CameraHelper.getCursorPosition();
             float step = speed * Time.deltaTime;
             target.transform.position = Vector2.MoveTowards(target.transform.position, targetPos, step);
         }
