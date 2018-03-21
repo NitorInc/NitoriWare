affc2561663ef6e4736391878ebac561c1537ee4
DracoGideon <DracoGideon@gmail.com>
2018-03-21 02:42:36 -0500

    Pound logics are now independent to its animation

--- Assets/Microgames/MochiPound/Scripts/MochiPoundController.cs
+++ Assets/Microgames/MochiPound/Scripts/MochiPoundController.cs
@@ -4,113 +4,44 @@ using UnityEngine;
 
 namespace NitorInc.MochiPound {
 
-    public enum Hit {
-        Left = 0,
-        Right
-    }
-
     public class MochiPoundController : MonoBehaviour {
-
         public int difficulty = 0;
-        public int[] requiredHits = {25, 50, 75}; 
-        public int RequiredHits {
+        public int[] requiredHits = {
+            4,6,8
+        };
+        public int RequiredHit {
             get {
                 return requiredHits[difficulty];
             }
         }
+
+        int hitCounter;
+
+        public void CountHit() {
+            hitCounter++;
+        }
+
         public bool IsLastHit {
             get {
-                return (RequiredHits - hitCounter) == 1;
+                return hitCounter == RequiredHit;
             }
         }
-        int hitCounter = 0;
 
+        public float finishPoundAnimTime = 0.2f;
+        public float finishSequenceAnimTime = 0.2f;
         public Animator[] finishAnims;
-        // how long in real time the last pound would take
-        public float lastPoundElapsedTime = 0.25f;
-        // how long in real time the finish pound (pound from both sides) pound would take
-        public float finishWaitTime = 0.55f;
-
-        bool hasWon = false;
-        Hit lastHit = Hit.Left;
-
-        public MochiPoundRabbitController[] rabbits;
-
-        void Start() {
-            hitCounter = 0;
-            rabbits[(int)Hit.Left].Disable();
+        public GameObject[] objectsToDisableOnFinish;
+        public void PrepareToStartFinalSequence() {
+            Invoke("PlayFinishSequence", finishSequenceAnimTime);
         }
-
-        void Update() {
-            if (!hasWon) {
-                if (BothAnimationFinished()) {
-                    RefreshRabbit(OpposedTo(lastHit));
-                    switch (lastHit) {
-                        case Hit.Left:
-                            if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-                                rabbits[(int)Hit.Right].Pound();
-                                ++hitCounter;
-                                if (hitCounter < RequiredHits)
-                                    rabbits[(int)Hit.Left].Windup();
-                                lastHit = Hit.Right;
-                            }
-                            break;
-                        case Hit.Right:
-                            if (Input.GetKeyDown(KeyCode.RightArrow)) {
-                                rabbits[(int)Hit.Left].Pound();
-                                ++hitCounter;
-                                if (hitCounter < RequiredHits)
-                                    rabbits[(int)Hit.Right].Windup();
-                                lastHit = Hit.Left;
-                            }
-                            break;
-                    }
-                }
-            }
-        }
-
-        public bool BothAnimationFinished() {
-            return rabbits[0].IsAnimationFinished && rabbits[1].IsAnimationFinished;
-        }
-
-        public void RefreshRabbit(Hit side) {
-            rabbits[(int)side].ResetStatus();
-            rabbits[(int)side].ShowButton(true);
-        }
-
-        public Hit OpposedTo(Hit side) {
-            if (side == Hit.Left) {
-                return Hit.Right;
-            }
-            else {
-                return Hit.Left;
-            }
-        }
-
-        public void OnHit() {
-            if (hitCounter >= RequiredHits) {
-                MicrogameController.instance.setVictory(true, true);
-                hasWon = true;
-                for (int i = 0; i < rabbits.Length; i++) {
-                    rabbits[i].Disable();
-                }
-                Invoke("PlayFinishPound", lastPoundElapsedTime);
-            }
-        }
-
-        void PlayFinishPound() {
-            for (int i = 0; i < rabbits.Length; i++) {
-                rabbits[i].OnVictory();
-            }
-            Invoke("PlayFinishSequence", finishWaitTime);
-        }
-
         void PlayFinishSequence() {
             for (int i = 0; i < finishAnims.Length; i++) {
                 finishAnims[i].enabled = true;
                 finishAnims[i].Play("Finish", 0, 0.0f);
             }
+            for (int i = 0; i < objectsToDisableOnFinish.Length; i++) {
+                objectsToDisableOnFinish[i].SetActive(false);
+            }
         }
-
     }
 }
--- Assets/Microgames/MochiPound/Scripts/MochiPoundRabbitController.cs
+++ Assets/Microgames/MochiPound/Scripts/MochiPoundRabbitController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NitorInc.Utility;
 
 namespace NitorInc.MochiPound {
 
@@ -23,7 +24,15 @@ namespace NitorInc.MochiPound {
         public float shakeSpeed = 1.0f;
         MochiPoundPlanet[] planets;
         MochiPoundController ctrler;
-        public Hit opposingSide;
+
+        public float poundAnimTime = 0.25f;
+        public float poundHitTime = 0.15f;
+        public float windupAnimTime = 0.25f;
+        Timer poundTimer;
+        Timer windupTimer;
+
+        public KeyCode inputKey = KeyCode.LeftArrow;
+        public MochiPoundRabbitController opposingRabbit;
 
         public MochiPoundArrowKey button;
 
@@ -34,126 +43,99 @@ namespace NitorInc.MochiPound {
         public string idleAnimName = "Idle";
         int idleAnimNameHash;
 
-        public float startAnimationSpeed = 2.5f;
-        public float finishAnimationSpeed = 0.7f;
+        bool readyToPound = true;
+
+        MochiPoundController ctrl;
 
         // Use this for initialization
         void Start() {
             anim = GetComponent<Animator>();
             shake = FindObjectOfType<CameraShake>();
             planets = FindObjectsOfType<MochiPoundPlanet>();
-            ctrler = FindObjectOfType<MochiPoundController>();
-            poundAnimNameHash = Animator.StringToHash(poundAnimName);
-            windupAnimNameHash = Animator.StringToHash(windupAnimName);
-            idleAnimNameHash = Animator.StringToHash(idleAnimName);
-            anim.speed = startAnimationSpeed;
+            poundTimer = TimerManager.NewTimer(poundAnimTime, OnPoundFinish, 0, false, false);
+            windupTimer = TimerManager.NewTimer(windupAnimTime, OnWindupFinish, 0, false, false);
+            ctrl = FindObjectOfType<MochiPoundController>();
+            
+            if (inputKey == KeyCode.RightArrow) {
+                readyToPound = false;
+            }
+        }
+
+        public void PrepareToFinish() {
+            poundTimer.Stop();
+            windupTimer.Stop();
+            readyToPound = false;
+            Invoke("FinishPound", ctrl.finishPoundAnimTime);
+        }
+
+        void FinishPound() {
+            anim.Play(poundAnimName, 0, 0.0f);
+            ctrl.PrepareToStartFinalSequence();
+        }
+
+        public void SetReady(bool ready = true) {
+            readyToPound = true;
+        }
+
+        public bool IsPounding() {
+            return poundTimer.IsRunning();
+        }
+
+        public bool IsWindingUp() {
+            return windupTimer.IsRunning();
+        }
+
+        void OnPoundFinish() {
+        }
+
+        void OnWindupFinish() {
+            readyToPound = true;
         }
 
         // Update is called once per frame
         void Update() {
-            if (!hasWon) {
-                UpdateHit();
-            }
-        }
-
-        void UpdateHit() {
-            if (!hasHit) {
-                var animState = anim.GetCurrentAnimatorStateInfo(0);
-                if (animState.shortNameHash == poundAnimNameHash) {
-                    if (animState.normalizedTime >= poundNormalizedTime && animState.normalizedTime < 1.0f) {
-                        OnMochiHit();
-                        hasHit = true;
+            if (!ctrl.IsLastHit) {
+                ShowButton(readyToPound);
+                if (Input.GetKeyDown(inputKey)) {
+                    if (readyToPound) {
+                        Pound();
                     }
                 }
             }
-        }
-
-        public void ResetStatus() {
-            hasHit = false;
+            else {
+                ShowButton(false);
+            }
         }
 
         public void ShowButton(bool active) {
             button.SetActive(active);
         }
 
-        public bool IsAnimationFinished {
-            get {
-                var animState = anim.GetCurrentAnimatorStateInfo(0);
-                if (animState.shortNameHash == idleAnimNameHash) {
-                    return true;
-                }
-                if (animState.shortNameHash == windupAnimNameHash) {
-                    if (animState.normalizedTime >= windupNormalizedTime) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-                if (animState.shortNameHash == poundAnimNameHash) {
-                    if (animState.normalizedTime >= poundNormalizedTime) {
-                        return true;
-                    }
-                    else {
-                        return false;
-                    }
-                }
-                return false;
-            }
-        }
-
-        public bool IsWindingUp() {
-            var animState = anim.GetCurrentAnimatorStateInfo(0);
-            if (animState.shortNameHash == windupAnimNameHash) {
-                if (animState.normalizedTime < windupNormalizedTime) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            return false;
-        }
-
         public void Pound() {
-            //PlayPoundImmediate();
-            PlayPoundAnim();
-            ShowButton(false);
+            readyToPound = false;
+            anim.Play(poundAnimName);
+            TimerManager.NewTimer(poundHitTime, OnMochiHit, 0);
+            poundTimer.Restart(poundAnimTime);
         }
-        public void Disable() {
-            hasHit = true;
-            ShowButton(false);
-        }
-
 
         public void Windup() {
-            PlayWindup();
-        }
-
-        public void OnVictory() {
-            anim.speed = finishAnimationSpeed;
-            hasWon = true;
-            PlayPoundAnim();
-            ShowButton(false);
+            anim.Play(windupAnimName);
+            windupTimer.Restart(windupAnimTime);
         }
 
         void OnMochiHit() {
+            ctrl.CountHit();
+            if (!ctrl.IsLastHit) {
+                opposingRabbit.Windup();
+            } else {
+                PrepareToFinish();
+                opposingRabbit.PrepareToFinish();
+            }
             mochiAnim.Play("Bump", 0, 0.0f);
             shake.xShake = xShake;
             for (int i = 0; i < planets.Length; i++) {
                 planets[i].Shake();
             }
-            ctrler.OnHit();
-        }
-
-        void PlayWindup() {
-            anim.Play(windupAnimNameHash, 0, 0.0f);
-        }
-
-        void PlayPoundAnim() {
-            anim.Play(poundAnimNameHash, 0, 0.0f);
-        }
-
-        void PlayPoundImmediate() {
-            anim.Play(poundAnimNameHash, 0, poundNormalizedTime);
         }
 
     }
