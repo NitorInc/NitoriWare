baa97e93b514542ab9c7a92e5a24ecb0609fa590
HatanoMyon <mendes.california@gmail.com>
2018-03-22 00:35:59 +0000

    Create script folder and start idealizing needed scripts for moving people and player control

---
+++ Assets/Microgames/MoneyTrap/Scripts/MoneyTrapController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyTrapController : MonoBehaviour {
+
+    /*
+    * code from wrench to get reference for key input capturing
+    if (Input.GetKeyDown(KeyCode.DownArrow))
+           {
+               if (!moving)
+               {
+                   _audioSource.PlayOneShot(fastenSound);
+                   setRotation(maxRotation);
+               }
+               moving = true;
+           }
+    */
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Microgames/MoneyTrap/Scripts/PeopleMove1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleMove1 : MonoBehaviour {
+
+    //Controls movement for people objects
+
+    private float speed = 1f;
+
+    private float direction = 1f;
+
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
