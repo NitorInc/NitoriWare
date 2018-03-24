a451f3487bbd2bc9876b7ef4e881a424178aa4e4
DracoGideon <DracoGideon@gmail.com>
2018-03-24 00:30:40 -0500

    Added animation speed control. Added sound effects.

--- Assets/Microgames/MochiPound/Scripts/MochiPoundController.cs
+++ Assets/Microgames/MochiPound/Scripts/MochiPoundController.cs
@@ -27,15 +27,24 @@ namespace NitorInc.MochiPound {
             }
         }
 
-        public float finishPoundAnimTime = 0.2f;
-        public float finishSequenceAnimTime = 0.2f;
         public Animator[] finishAnims;
         public GameObject[] objectsToDisableOnFinish;
-        public void PrepareToStartFinalSequence() {
-            Invoke("PlayFinishSequence", finishSequenceAnimTime);
+        public AudioClip finishPoundSound;
+        public float finishPoundVolume = 2.5f;
+        bool hasCalled = false;
+        public float finishAnimSpeed = 1.0f;
+
+        public void PrepareToStartFinalSequence(float poundAnimTime) {
+            if (!hasCalled) {
+                hasCalled = true;
+                Invoke("PlayFinishSequence", poundAnimTime);
+            }
         }
+
         void PlayFinishSequence() {
+            MicrogameController.instance.playSFX(finishPoundSound, MicrogameController.instance.getSFXSource().panStereo, 1.0f, finishPoundVolume);
             for (int i = 0; i < finishAnims.Length; i++) {
+                finishAnims[i].speed *= finishAnimSpeed;
                 finishAnims[i].enabled = true;
                 finishAnims[i].Play("Finish", 0, 0.0f);
             }
