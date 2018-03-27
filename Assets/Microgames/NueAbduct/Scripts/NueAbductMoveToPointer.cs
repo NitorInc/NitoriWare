109fd52557378fd5c735d0bfe8675835a7aed1ec
Brian Intile <brianintile@gmail.com>
2018-03-27 02:53:53 -0400

    NueAbduct: Add victory animation

--- Assets/Microgames/NueAbduct/Scripts/NueAbductMoveToPointer.cs
+++ Assets/Microgames/NueAbduct/Scripts/NueAbductMoveToPointer.cs
@@ -10,6 +10,9 @@ namespace NitorInc.NueAbduct {
         public float speed = 2.0f;
 
         void Update() {
+            if (MicrogameController.instance.getVictoryDetermined())
+                return;
+
             var targetPos = CameraHelper.getCursorPosition();
             float step = speed * Time.deltaTime;
             target.transform.position = Vector2.MoveTowards(target.transform.position, targetPos, step);
