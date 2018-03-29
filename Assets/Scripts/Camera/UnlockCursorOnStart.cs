f8440b069a884423714705c0bbc05985df59a809
Brian Intile <brianintile@gmail.com>
2018-03-28 22:30:27 -0400

    Hotfix to unlock cursor in menu

---
+++ Assets/Scripts/Camera/UnlockCursorOnStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockCursorOnStart : MonoBehaviour
+{
+    
+	void Start ()
+    {
+        Invoke("unlockCursor", .1f);
+	}
+
+    void unlock()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
