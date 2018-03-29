4c5cf71911e39912c0712cfa4d87d85313396b23
Brian Intile <brianintile@gmail.com>
2018-03-29 01:12:10 -0400

    NueAbduct: Add hotfix to tie UFO noise to volume settings

---
+++ Assets/Microgames/NueAbduct/Scripts/NueAbductAnimationVolumeHotfix.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NueAbductAnimationVolumeHotfix : MonoBehaviour
+{
+    public float volumeMult;
+    private AudioSource audioSource;
+
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	void LateUpdate ()
+    {
+        audioSource.volume = volumeMult * PrefsHelper.getVolume(PrefsHelper.VolumeType.SFX);
+	}
+}
