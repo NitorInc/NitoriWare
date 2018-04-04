5bb154aeb232a760192ac1babd282282c87753ea
dC <lordrandomness@gmail.com>
2018-04-04 19:46:48 +1000

    Answers appear and can be selected for the addition problem by clicking with the mouse, causing win/loss

--- Assets/Microgames/KeineMath/Scripts/KeineMath_Chalkboard.cs
+++ Assets/Microgames/KeineMath/Scripts/KeineMath_Chalkboard.cs
@@ -16,6 +16,10 @@ public class KeineMath_Chalkboard : MonoBehaviour {
     [SerializeField]
     private int maxTerm = 5;
 
+    [Header("Number of answers to generate")]
+    [SerializeField]
+    private int answerCount = 3;
+
     [Header("Operation to use")]
     [SerializeField]
     private string operation = "+";
@@ -26,7 +30,10 @@ public class KeineMath_Chalkboard : MonoBehaviour {
     private GameObject term2;
     private GameObject term3;
     private List<GameObject> terms = new List<GameObject>();
+    private GameObject answer;
     private GameObject operationSymbol;
+    private int correctAnswer;
+    private bool answered = false;
 
     // Use this for initialization
     void Start () {
@@ -37,7 +44,9 @@ public class KeineMath_Chalkboard : MonoBehaviour {
         term3 = GameObject.Find("Term3");
         terms.Add(term3);
         operationSymbol = GameObject.Find("Operation");
+        answer = GameObject.Find("Answer");
         generateProblem();
+        generateAnswers();
 	}
 	
 	// Update is called once per frame
@@ -53,11 +62,13 @@ public class KeineMath_Chalkboard : MonoBehaviour {
         }
         if (operation.Equals("+"))
         {
+            correctAnswer = 0;
             for(int i = 0; i < terms.Count; i++)
             {
                 if (i < termCount)
                 {
                     terms[i].GetComponent<KeineMath_Term>().setValue(termList[i]);
+                    correctAnswer += termList[i];
                 } else
                 {
                     terms[i].SetActive(false);
@@ -73,5 +84,44 @@ public class KeineMath_Chalkboard : MonoBehaviour {
         float symbolx = operationSymbol.transform.position.x - (1f * Mathf.Max(termList.ToArray())) + 1;
         float symboly = operationSymbol.transform.position.y;
         operationSymbol.transform.position = new Vector3(symbolx, symboly, 0);
+    }
+
+    void generateAnswers()
+    {
+        int answerValue;
+        int answerOffset;
+        List<int> answerOffsets = new List<int>();
+        for(int i = 0; i < answerCount; i++)
+        {
+            int sign = (Random.Range(1, 3) * 2) - 3; //Generates 1 or -1
+            answerOffsets.Add(i * sign);
+        }
+        for(int i = 1; i <= answerCount; i++)
+        {
+            answerOffset = answerOffsets[Random.Range(0, answerOffsets.Count)];
+            answerOffsets.Remove(answerOffset);
+            answerValue = correctAnswer + answerOffset;
+            float newx = answer.transform.position.x + (3.25f * i);
+            float newy = answer.transform.position.y;
+            Vector3 newposition = new Vector3(newx, newy, 0);
+            GameObject newanswer = Object.Instantiate(answer, newposition, Quaternion.identity);
+            newanswer.GetComponent<KeineMath_Answer>().value = answerValue;
+        }
+    }
+
+    public void processAnswer(int answer)
+    {
+        if (!answered)
+        {
+            answered = true;
+            if (answer == correctAnswer)
+            {
+                MicrogameController.instance.setVictory(victory: true, final: true);
+            }
+            else
+            {
+                MicrogameController.instance.setVictory(victory: false, final: true);
+            }
+        }
     }
 }
