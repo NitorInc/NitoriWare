7fe37d5023f1f7f490cfea5cd64e036ebf918bd3
Brian Intile <brianintile@gmail.com>
2018-04-16 22:55:47 -0400

    NitoriLook: Raise player view angle slightly

--- Assets/Microgames/_Finished/NitoriLook/Scripts/NitoriLookMovement.cs
+++ Assets/Microgames/_Finished/NitoriLook/Scripts/NitoriLookMovement.cs
@@ -14,7 +14,7 @@ public class NitoriLookMovement : MonoBehaviour
 
 	void Awake()
 	{
-        transform.rotation = Quaternion.Euler(new Vector3(0f, Random.Range(-180f, 180f), 0f));
+        transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, Random.Range(-180f, 180f), 0f));
     }
 	
 	void Update()
