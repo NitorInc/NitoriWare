749ad04990da58eb13ff9556c6bb58526b07a3aa
LadyEbony <jgarc129@ucr.edu>
2018-06-04 21:23:48 -0700

    Added scene and traits. Imported sprites and animations. Implemented Player and Bullet scripts.

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//namespace GiveANameSpace
+
+public class ReimuDodgeBullet : MonoBehaviour
+{
+    [Header("The thing to fly towards")]
+    [SerializeField]
+    private GameObject target;
+
+    [Header("How fast da bullet")]
+    [SerializeField]
+    private float speed = 1f;
+
+    [Header("Firing delay in seconds")]
+    [SerializeField]
+    private float delay = 1f;
+
+    private Vector2 trajectory;
+
+    void Start()
+    {
+        Invoke("SetTrajectory", delay);
+    }
+
+    void Update()
+    {
+        if (trajectory != null)
+        {
+            transform.position = (Vector2)transform.position + trajectory * Time.deltaTime * speed;
+        }
+    }
+
+    void SetTrajectory()
+    {
+        trajectory = (target.transform.position - transform.position).normalized;
+    }
+}
