dbd055a1cc6bd476d37aeb3762126d4ddde6b784
Revelance <41409656+Revelance@users.noreply.github.com>
2018-07-23 11:27:15 -0700

    SekibankiMicrogame

---
+++ Assets/Microgames/SekibankiNeck/Scripts/SekibankiNeckBodyBehaviors.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SekibankiNeckBodyBehaviors : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip goalSound;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private Sprite victorypose;
+    private Animator animator;
+
+
+
+        
+            
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+        if (collision.gameObject.tag == "MicrogameTag3")
+        {
+            ParticleSystem particleSystem = GetComponentInChildren<ParticleSystem>();
+            particleSystem.Play();
+
+            MicrogameController.instance.playSFX(goalSound, volume: 1f, panStereo: AudioHelper.getAudioPan(transform.position.x));
+            Animator animator = GetComponentInChildren<Animator>();
+            animator.enabled = (false);
+            SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            spriteRenderer.sprite = victorypose;
+
+
+
+        }
+
+
+    }
+}
---
+++ Assets/Microgames/SekibankiNeck/Scripts/SekibankiNeckBulletMover.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SekibankiNeckBulletMover : MonoBehaviour {
+
+    [Header("Target")]
+    [SerializeField]
+    private GameObject target;
+
+    [Header("Target2")]
+    [SerializeField]
+    private GameObject target2;
+
+    [Header("Speed")]
+    [SerializeField]
+    private float speed = 1f;
+
+    [Header("Delay")]
+    [SerializeField]
+    private float delay = 1f;
+
+    private Vector2 trajectory;
+
+	// Use this for initialization
+	void Start () {
+
+        Invoke("SetTrajectory", 0f);
+
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (trajectory != null)
+
+        {
+            Vector2 newPosition = (Vector2)transform.position + (trajectory * speed * Time.deltaTime);
+            transform.position = newPosition;
+        }
+	}
+
+
+    void SetTrajectory()
+    {
+        trajectory = (target.transform.position - transform.position).normalized;
+
+        Invoke("SetTrajectory2", delay);
+    }
+
+    void SetTrajectory2()
+    {
+        trajectory = (target2.transform.position - transform.position).normalized;
+
+        Invoke("SetTrajectory", delay);
+    }
+}
