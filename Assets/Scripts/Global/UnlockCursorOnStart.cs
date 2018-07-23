3bc49e998d4ca57809b979c26e0f9b08fa7f20a1
Brian Intile <brianintile@gmail.com>
2018-07-23 16:22:15 -0400

    Add UnlockCursorOnStart redundancy to Title

---
+++ Assets/Scripts/Global/UnlockCursorOnStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockCursorOnStart : MonoBehaviour
+{
+
+    void Start()
+    {
+        Invoke("unlockCursor", .1f);
+    }
+
+    void unlockCursor()
+    {
+        Cursor.lockState = GameController.DefaultCursorMode;
+    }
+}
