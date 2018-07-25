23ddc22fae70f810ae8de581e44e4c17d97d0743
Brian Intile <brianintile@gmail.com>
2018-07-25 17:24:51 -0400

    Update credits and implement auto-column splitting

---
+++ Assets/Scripts/Menu/CreditsSplitLines.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class CreditsSplitLines : MonoBehaviour
+{
+    [SerializeField]
+    private string lastText;
+    [SerializeField]
+    private Text[] textColumns;
+    [SerializeField]
+    [Multiline]
+    private string contents;
+	
+	void Update ()
+    {
+		if (contents != lastText && textColumns?.Length > 0)
+        {
+            var lines = contents.Split('\n');
+            int lineIndex = 0;
+            while (lineIndex < lines.Length)
+            {
+                foreach (var column in textColumns)
+                {
+                    if (lineIndex < textColumns.Length)
+                        column.text = lines[lineIndex];
+                    else
+                        column.text += "\n" + lines[lineIndex];
+                    lineIndex++;
+                    if (lineIndex >= lines.Length)
+                        break;
+                }
+            }
+            lastText = contents;
+        }
+	}
+}
