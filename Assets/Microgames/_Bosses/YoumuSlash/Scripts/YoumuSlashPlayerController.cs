fb5ac6e694f8a2f74a36b79b802725847696b89c
Brian Intile <brianintile@gmail.com>
2018-07-26 00:09:07 -0400

    Improve return animation and stop playing beat

--- Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlashPlayerController.cs
+++ Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlashPlayerController.cs
@@ -67,7 +67,6 @@ public class YoumuSlashPlayerController : MonoBehaviour
                 MicrogameController.instance.playSFX(debugSound);
                 rigAnimator.SetTrigger("Idle");
                 rigAnimator.ResetTrigger("Attack");
-                rigAnimator.SetTrigger("Beat");
             }
         }
         else if (beat > nextIdleBeat)
