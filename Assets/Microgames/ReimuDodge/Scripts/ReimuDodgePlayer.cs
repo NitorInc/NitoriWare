e4435f18699a71448eec727cbf6ec23e6f83a7b1
Neil Jewers <njjewers@edu.uwaterloo.ca>
2018-07-29 10:17:39 -0400

    Added player script, death consequences

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgePlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuDodgePlayer : MonoBehaviour {
+	[SerializeField]
+	private AudioClip deathSound;
+
+	private bool alive = true;
+
+	// This will happen when the player's hitbox collides with a bullet
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		// Only kill Reimu if she's still alive
+		if (alive)
+		{
+			Kill();
+		}
+	}
+
+	void Kill()
+	{
+		// Kill Reimu
+		alive = false;
+
+		// Get a reference to the player's sprite
+		SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+		// Make the sprite disappear by disabling the the GameObject it's attached to
+		spriteRenderer.gameObject.SetActive(false);
+
+		// Finally, disable the FollowCursor script to stop the object from moving
+		FollowCursor followCursor = GetComponent<FollowCursor>();
+		followCursor.enabled = false;
+
+		// Play the death sound effect
+		// At a custom volume
+		// And panned to the player's X Posision
+		MicrogameController.instance.playSFX(deathSound, volume: 0.5f,
+			panStereo: AudioHelper.getAudioPan(transform.position.x));
+
+		// Now get a reference to the death exposion and start it
+		ParticleSystem particleSystem = GetComponentInChildren<ParticleSystem>();
+		particleSystem.Play();
+	}
+}
