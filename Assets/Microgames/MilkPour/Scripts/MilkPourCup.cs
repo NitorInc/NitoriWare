af1310d792e08a2e394c8c742488d072cae2b469
stellarLuminant <stellarLuminant@gmail.com>
2018-07-31 10:07:36 -0700

    MilkPour: Graybox prototype
    The other difficulties are not done yet. Next time I'll get to the cup switching functionality which will be used in difficulties 2 and 3.

---
+++ Assets/Microgames/MilkPour/Scripts/MilkPourCup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkPourCup : MonoBehaviour {
+
+	public const float MAX_FILL = 100;
+
+	public float Fill;
+	public float RequiredFill;
+
+	[SerializeField]
+	Transform _maskTransform;
+
+	[SerializeField]
+	float _maskMinY;
+
+	[SerializeField]
+	float _maskMaxY;
+
+	[SerializeField]
+	Transform _fillLineTransform;
+
+	void Start () {
+		Fill = 0;
+		_fillLineTransform.localPosition = new Vector2 (_fillLineTransform.localPosition.x, RequiredFill / MAX_FILL);
+	}
+
+	void Update () {
+		_maskTransform.localPosition = new Vector2 (_maskTransform.localPosition.x,
+			Mathf.Lerp (_maskMinY, _maskMaxY, Fill / MAX_FILL));
+	}
+}
---
+++ Assets/Microgames/MilkPour/Scripts/MilkPourState.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkPourState : MonoBehaviour {
+
+	private const float FILL_RATE = 75;
+
+	[SerializeField]
+	private MilkPourCup _currentCup;
+
+	[SerializeField]
+	private mpState _currentState;
+
+	private enum mpState {
+		Idle,
+		Filling
+	}
+
+	void Start () { }
+
+	void Update () {
+
+		if (Input.GetKey (KeyCode.Space))
+			_currentState = mpState.Filling;
+		else
+			_currentState = mpState.Idle;
+
+		switch(_currentState) {
+			case mpState.Idle: {
+				if (_currentCup.Fill > _currentCup.RequiredFill)
+					MicrogameController.instance.setVictory(true, true);
+			}
+			break;
+			case mpState.Filling:
+			{
+				_currentCup.Fill += FILL_RATE * Time.deltaTime;
+				if (_currentCup.Fill > MilkPourCup.MAX_FILL)
+					MicrogameController.instance.setVictory(false, true);
+			}
+			break;
+		}
+	}
+}
