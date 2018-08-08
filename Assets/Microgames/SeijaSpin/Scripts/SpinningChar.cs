56cc521c6547639de5aa3223420fa2f3e0cdfc5c
Shadow <Shadowth117@gmail.com>
2018-08-08 00:07:52 -0400

    First Commit
    Set some basic scene properties up and started on the spinning object script.

---
+++ Assets/Microgames/SeijaSpin/Scripts/SpinningChar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningChar : MonoBehaviour {
+
+    public int spinCount;
+    private Vector2 lastVec2Angle;
+    private Vector2 lastMouseAngle;
+    private Vector2 currentAngle;
+    private float lastAngle;
+    private float currentAngleChange;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Check if we can spin and whether the mouse is in position to
+		if (spinCount > 0 && CameraHelper.isMouseOver(gameObject.GetComponent<CircleCollider2D>()))
+        {
+            // Check for mouse input and interact if so
+            if (Input.GetMouseButtonDown(0))
+            {
+                lastVec2Angle = (Vector2)(CameraHelper.getCursorPosition() - transform.position);
+                lastAngle = lastVec2Angle.getAngle();
+            } else if (Input.GetMouseButton(0))
+            {
+                currentAngle = (Vector2)(CameraHelper.getCursorPosition() - transform.position);
+                currentAngleChange = currentAngle.getAngle();
+                lastMouseAngle = currentAngle;
+            }
+        }
+	}
+}
