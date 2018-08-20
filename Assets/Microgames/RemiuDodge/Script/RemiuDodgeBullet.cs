be7a4356eb5661dcef94883ceba4cb48fc11318f
Qapples <armand.scotty@yahoo.com>
2018-08-20 10:51:39 -0400

    added bullets

---
+++ Assets/Microgames/RemiuDodge/Script/RemiuDodgeBullet.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class RemiuDodgeBullet : MonoBehaviour {
+
+    // Use this for initialization
+    // A Unity in-editor variable
+    [Header("The thing to fly towards")]
+    [SerializeField]
+    private GameObject target;
+
+    // Stores the direction of travel for the bullet
+    private Vector2 trajectory;
+
+    [Header("How fast the bullet goes")]
+    [SerializeField]
+
+    private float speed = 1f;
+
+    [Header("Firing delay in seconds")]
+    [SerializeField]
+    private float delay = 1f;
+    // Use this for initialization
+    void Start()
+    {
+        // Calculate a trajectory towards the target
+        Invoke("SetTrajectory", delay);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (trajectory != null)
+        {
+            // Move bullet based on trajectory speed and time
+            Vector2 newPos = (Vector2)transform.position + (trajectory * speed * Time.deltaTime);
+
+            transform.position = newPos;
+        }
+     
+	}
+    
+    //this method gets the difference between the player and the bullet.
+    void SetTrajectory()
+    {
+        trajectory = (target.transform.position - transform.position).normalized;
+    }
+}
