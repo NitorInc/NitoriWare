6c91f2c9c113b2eaf440b722f0d66e5e4b9fa9b8
StardustReveries <42533755+StardustReveries@users.noreply.github.com>
2018-08-21 17:24:23 +0700

    Tutorial - Bullet Script

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuDodgeBullet : MonoBehaviour {
+
+    // In-editor variables
+    [Header("Bullet target")]
+    [SerializeField]
+    private GameObject target;
+
+    [Header("Bullet speed")]
+    [SerializeField]
+    private float speed = 1f;
+
+    [Header("Bullet delay")]
+    [SerializeField]
+    private float delay = 1f;
+
+    // Bullet direction
+    private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+        // Invoke SetDirection after delay
+        Invoke("SetDirection", delay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        // Assign direction if set
+        if (direction != null) {
+            Vector2 newPosition = (Vector2)transform.position + (direction * speed * Time.deltaTime);
+            transform.position = newPosition;
+        }
+
+	}
+
+    void SetDirection () {
+        // Set direction
+        direction = (target.transform.position - transform.position).normalized;
+    }
+}
