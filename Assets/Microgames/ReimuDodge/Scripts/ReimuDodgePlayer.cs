bb2d2119a5533e309a8956f7a064049d5392a895
bosmobosmo <36439009+bosmobosmo@users.noreply.github.com>
2018-08-26 19:13:17 +0700

    death scene

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgePlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuDodgePlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip deathsound;
+
+    private bool alive = true;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (alive)
+        {
+            Kill();
+        }
+    }
+
+    void Kill()
+    {
+        alive = false;
+
+        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        spriteRenderer.gameObject.SetActive(false);
+
+        FollowCursor followcursor = GetComponent<FollowCursor>();
+        followcursor.enabled = false;
+
+        MicrogameController.instance.playSFX(deathsound, volume: 0.5f, panStereo: AudioHelper.getAudioPan(transform.position.x));
+
+        ParticleSystem particleSystem = GetComponentInChildren<ParticleSystem>();
+        particleSystem.Play();
+    }
+}
