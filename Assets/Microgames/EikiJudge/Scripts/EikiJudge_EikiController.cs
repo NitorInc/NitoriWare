4c03f05ca2a8a2ab5104bc7abec54311f5be267b
Ktululz <contact@ktululz.fr>
2018-08-28 02:40:29 +0200

    Initial commit
    Bigger commit than expected, basically one-shot the entire microgame

---
+++ Assets/Microgames/EikiJudge/Scripts/EikiJudge_EikiController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EikiJudge_EikiController : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer eikiArmSprite;
+    [SerializeField]
+    private Sprite eikiArmLeft, eikiArmRight;
+
+    public EikiJudge_Controller controller;
+
+    private EikiJudge_Controller.Direction judgementDirection;
+
+    void Start()
+    {
+        eikiArmSprite.sprite = null;
+        controller.SpawnSouls();
+    }
+
+    void Update()
+    {
+        // If all souls are instantiated AND game isn't lost
+        if (controller.allSoulsReady && controller.wasted == false)
+        {
+            EikiJudgement();
+
+            // If direction != none 
+            if (judgementDirection != EikiJudge_Controller.Direction.none)
+            {
+                //soulsList[0].soulController.SendTheSoul(judgementDirection);
+                controller.SendJudgement(judgementDirection);
+            }
+        }
+    }
+
+    // Key input / move Eiki's arm / set a direction for the soul
+    // TODO: refactor to implement animation
+    void EikiJudgement()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            eikiArmSprite.sprite = eikiArmLeft;
+            judgementDirection = EikiJudge_Controller.Direction.left;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            eikiArmSprite.sprite = eikiArmRight;
+            judgementDirection = EikiJudge_Controller.Direction.right;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            eikiArmSprite.sprite = null;
+            judgementDirection = EikiJudge_Controller.Direction.none;
+        }
+
+        else
+        {
+            judgementDirection = EikiJudge_Controller.Direction.none;
+        }
+    }
+}
---
+++ Assets/Microgames/EikiJudge/Scripts/EikiJudge_SoulController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EikiJudge_SoulController : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer soulRenderer;
+    [SerializeField]
+    private Sprite badSoulSprite, goodSoulSprite;
+
+    public int soulListPosition;
+    private EikiJudge_Controller.SoulType soulType;
+
+    private bool moveToCourt = false;
+    private float delay = 0.25f;
+
+    private Vector3 soulTarget;
+    private float speed = 10f;
+
+    private Vector3 targetPortal = Vector3.zero;
+    private Vector3 soulTrajectory;
+
+    public EikiJudge_Controller.Direction RightPortalDirection { get; set; }
+
+
+    private void Start()
+    {
+        // Randomize bad/good soul
+        if (Random.value > 0.5f)
+        {
+            soulRenderer.sprite = goodSoulSprite;
+            soulType = EikiJudge_Controller.SoulType.good;
+            RightPortalDirection = EikiJudge_PortalsController.portalsController.heavenDirection;
+        }
+        else
+        {
+            soulRenderer.sprite = badSoulSprite;
+            soulType = EikiJudge_Controller.SoulType.bad;
+            RightPortalDirection = EikiJudge_PortalsController.portalsController.hellDirection;
+        }
+
+        // Put some delay before moving in front of Eiki
+        Invoke("MoveToCourtDelay", delay * soulListPosition);
+    }
+
+
+    void Update()
+    {
+        // If he's still on the list / waiting line
+        soulListPosition = EikiJudge_Controller.controller.soulsList.IndexOf(this);
+
+        if (soulListPosition >= 0 && moveToCourt)
+        {
+            // Move the soul in the correct spot
+            soulTarget = new Vector3(0f, -1.5f * soulListPosition, soulListPosition * -1f);
+            transform.position = Vector3.Lerp(transform.position, soulTarget, Time.deltaTime * speed);
+        }
+        else
+        {
+            // Move the soul towards the chosen portal
+            Vector3 newPosition = transform.position + (soulTrajectory * speed * Time.deltaTime);
+            transform.position = newPosition;
+        }
+
+    }
+
+    // Delayed move
+    private void MoveToCourtDelay()
+    {
+        moveToCourt = true;
+    }
+
+    // Send the soul to a portal
+    public void SendTheSoul(EikiJudge_Controller.Direction judgementDirection)
+    {
+        if (judgementDirection == EikiJudge_Controller.Direction.left)
+        {
+            targetPortal = new Vector3(-4.35f, 2.5f);
+        }
+        else
+        {
+            targetPortal = new Vector3(4.35f, 2.5f);
+        }
+
+        soulTrajectory = (targetPortal - transform.position).normalized;
+
+        // If the soul is not sent to the right portal -> loose
+        if (judgementDirection != RightPortalDirection)
+        {
+            EikiJudge_Controller.controller.LoseGame();
+        }
+    }
+}
