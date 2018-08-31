73f3578c50713766c8b41d76e9c33ec89cb2f9d5
MrMinimoose <42853941+MrMinimoose@users.noreply.github.com>
2018-08-31 14:16:55 -0500

    Bullet Script WIP

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuDodgeBullet : MonoBehaviour {
+    //A Unity in-editor variable
+    [Header("the thing to fly towards")]
+    [SerializeField]
+    private GameObject target;
+
+    //Stores the direction of travel for the bullet
+    private Vector2 trajectory;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Calculate a trajectory towards the target
+        trajectory = (target.transform.position - transform.position).normalized;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
