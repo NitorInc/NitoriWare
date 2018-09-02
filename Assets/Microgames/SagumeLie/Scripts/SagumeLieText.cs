4e3cf09f5d228488b2345165febdc3ec8e10e7c0
fuzzys <30754267+fuzzys@users.noreply.github.com>
2018-09-02 09:48:12 -0400

    Canvas change & answers randomized from list
    Canvas is now a canvas like it should be; list of Truths is picked from a list with no duplicates. This introduces a potential bug which I kept in for demonstration; will describe in Discord

--- Assets/Microgames/SagumeLie/Scripts/SagumeLieText.cs
+++ Assets/Microgames/SagumeLie/Scripts/SagumeLieText.cs
@@ -27,31 +27,45 @@ public class SagumeLieText : MonoBehaviour
 
     void Start()
     {
+        // get traits
         var traits = (SagumeLieTraits)MicrogameController.instance.getTraits();
 
+        // questionIndex is a random question from the pool of all questions. QuestionText is set to this, so it'll be in the bubble.
         questionIndex = Random.Range(0, traits.QuestionPool.Length);
-
         QuestionText.SetText(traits.getLocalizedQuestionText(questionIndex));
 
-        // set number of maximum answers
-        // this is prep for higher difficulty; 4 for testing purposes
-
+        // set the number of maximum answers. should be changed based on difficulty. right now, it's 4 for testing.
         int maxAnswer = 4;
 
-        // find the answers
-        // note: truth can have duplicate answers right now. find fix??
-
+        // pick which position is the Lie. Might be better to have more than one lie option.
         liePosition = Random.Range(0, maxAnswer);
 
+        // make a list of the Truths for the given question.
+        List<string> unpickedTruths = new List<string>();
+        for (int i = 0; i < traits.QuestionPool[questionIndex].TruthResponses.Length; i++)
+        {
+            unpickedTruths.Add(traits.getLocalizedResponseText(questionIndex, false, i));
+        };
+
+        // !!! DEBUG !!! if there's room for 3 truths, but we only have 2 on the list, it'll error.
+        // this'll be fixed by either reducing answers, or making sure every question has 3 possible truths. 
+        if (traits.QuestionPool[questionIndex].TruthResponses.Length < 3)
+        { unpickedTruths.Add("Debug truth! You shouldn't be seeing this!");
+        };
+
+        // go through the number of Answers. if it's the Lie position, make it a Lie. if it's the Truth position, make it a Truth then remove it from the list.
         for (int i = 0; i < maxAnswer; i++)
         {
             if (i == liePosition)
                 Answer[i] = traits.getLocalizedResponseText(questionIndex, true, Random.Range(0, traits.QuestionPool[questionIndex].LieResponses.Length));
 
             else
-                Answer[i] = traits.getLocalizedResponseText(questionIndex, false, Random.Range(0, traits.QuestionPool[questionIndex].TruthResponses.Length));
+                Answer[i] = unpickedTruths[Random.Range(0, unpickedTruths.Count)];
+                unpickedTruths.Remove(Answer[i]);
         }
 
+
+        // set answers to what was picked
         Answer0.SetText(Answer[0]);
         Answer1.SetText(Answer[1]);
         Answer2.SetText(Answer[2]);
@@ -100,5 +114,4 @@ public class SagumeLieText : MonoBehaviour
         else
             SagumeFailure();
     }
-
 }
