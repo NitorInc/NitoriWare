19e03cda649ea481dbe447f2b3182818b492586f
Naomi B <noterday@outlook.com>
2018-09-03 16:41:09 -0400

    Add player movement script
    Rename microgame files to begin with microgame title

---
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerPlayerControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonSoccerPlayerControls : MonoBehaviour {
+    
+    // A Unity in-editor variable
+    [Header("Movement Speed")]
+    [SerializeField]
+    private float moveSpeed = 1f;
+    
+    
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        updateMovement();
+	}
+    
+    void updateMovement ()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime);
+        }
+    }
+}
