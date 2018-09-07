3eed44e61a63ebf2fe601a5dacc577a3150e7f24
MrMinimoose <42853941+MrMinimoose@users.noreply.github.com>
2018-09-07 16:40:45 -0500

    All done, I think! Sorry about the procrastinating~...

--- Assets/Microgames/ReimuDodge/Scripts/ReimuDodgePlayer.cs
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgePlayer.cs
@@ -17,6 +17,10 @@ public class ReimuDodgePlayer : MonoBehaviour
         if (alive)
         {
             Kill();
+
+            // Now tell the MicrogameController in the scene that the game is over
+            // and we've lost forever
+            MicrogameController.instance.setVictory(victory: false, final: true);
         }
     }
     
@@ -36,7 +40,7 @@ public class ReimuDodgePlayer : MonoBehaviour
         // Play the death sound effect
         // At a custom volume
         // And panned to the player's X Position
-        MicrogameController.instance.playSFX(deathSound, volume: 0.5f,
+        MicrogameController.instance.playSFX(deathSound, volume: 0.25f,
             panStereo: AudioHelper.getAudioPan(transform.position.x));
         // Now get a reference to the death explosion and start it
         ParticleSystem particleSystem = GetComponentInChildren<ParticleSystem>();
