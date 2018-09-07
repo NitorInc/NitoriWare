437a46446963784cd77cc1157f51f664a1f7afe8
MrMinimoose <42853941+MrMinimoose@users.noreply.github.com>
2018-09-07 15:30:01 -0500

    Finished script?

--- Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReimuDodgeBullet : MonoBehaviour {
+public class ReimuDodgeBullet : MonoBehaviour
+{
     //A Unity in-editor variable
     [Header("the thing to fly towards")]
     [SerializeField]
     private GameObject target;
+
+    [Header("How fast the bullet goes")]
+    [SerializeField]
+    private float delay = 1f;
+
+    [Header("Firing delay in seconds")]
+    [SerializeField]
+    private float delay = 1f;
 
     //Stores the direction of travel for the bullet
     private Vector2 trajectory;
@@ -14,12 +23,24 @@ public class ReimuDodgeBullet : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        //Calculate a trajectory towards the target
-        trajectory = (target.transform.position - transform.position).normalized;
+        // Invoke the setTrajectory method after the delay
+        Invoke("SetTrajectory", delay);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		// Only start moving after the trajectory has been set
+        if (trajectory != null)
+        {
+            // Move the bullet a certain distance based on trajectory speed and time
+            Vector2 newPosition = (Vector2)transform.position + (trajectory * Time.deltaTime);
+            transform.position = newPosition;
+        }    
 	}
+    void SetTrajectory()
+    {
+        // Calculate a trajectory towards the target
+        trajectory = (target.transform.position - transform.position).normalized;
+    }
 }
