84f99be25154dea8db87b23cdddd12a24e1360f6
MrMinimoose <42853941+MrMinimoose@users.noreply.github.com>
2018-09-07 16:21:13 -0500

    It didn't look like Kill worked but hERE GOES

--- Assets/Microgames/ReimuDodge/Scripts/ReimuDodgePlayer.cs
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgePlayer.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class ReimuDodgePlayer : MonoBehaviour
 {
-    
+    [SerializeField]
+    private AudioClip deathSound;
+
+    private bool alive = true;
+
     // This will happen when the player's hitbox collides with a bullet
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        // Test that this works
-        print("Player was hit!");
+        // Only kill Reimu if she's still alive
+        if (alive)
+        {
+            Kill();
+        }
     }
     
-    
+    void Kill()
+    {
+        // Murder
+        alive = false;
+        // Get a reference to player's sprite
+        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        // Make the sprite dissapear by disabling the GameObject it's at
+        spriteRenderer.gameObject.SetActive(false);
+
+        // Finally disable the FollowCursor script to stop the object from moving
+        FollowCursor followCursor = GetComponent<FollowCursor>();
+        followCursor.enabled = false;
+
+        // Play the death sound effect
+        // At a custom volume
+        // And panned to the player's X Position
+        MicrogameController.instance.playSFX(deathSound, volume: 0.5f,
+            panStereo: AudioHelper.getAudioPan(transform.position.x));
+        // Now get a reference to the death explosion and start it
+        ParticleSystem particleSystem = GetComponentInChildren<ParticleSystem>();
+        particleSystem.Play();
+    }
 }
