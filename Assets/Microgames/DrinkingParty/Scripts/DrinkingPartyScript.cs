25849a53cce9f6fc07bf7a544ef7227dc7d6514f
WilliamDouris <William.Douris@github.com>
2018-09-10 22:42:49 +0200

    Add Some temporary texture + Script detect Space

---
+++ Assets/Microgames/DrinkingParty/Scripts/DrinkingPartyScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkingPartyScript : MonoBehaviour
+{
+    private float ScaleLose = 0.1f;
+    public GameObject ObjectLiquide1;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("space key was pressed");
+            ObjectLiquide1 = GameObject.Find("/Liquide1");
+            SpriteRenderer spriteRenderer = ObjectLiquide1.GetComponent<SpriteRenderer>();
+            spriteRenderer.gameObject.SetActive(false);
+        }
+    }
+}
