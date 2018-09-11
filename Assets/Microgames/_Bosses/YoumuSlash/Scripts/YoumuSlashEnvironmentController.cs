bf761b77a06fb5c7b911587617ed177da7c80cc3
Brian Intile <brianintile@gmail.com>
2018-09-10 23:40:06 -0400

    Create tutorial indicator animation

---
+++ Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlashEnvironmentController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YoumuSlashEnvironmentController : MonoBehaviour
+{
+    [SerializeField]
+    private YoumuSlashTimingController timingData;
+
+    private Animator animator;
+    
+	void Start ()
+    {
+        YoumuSlashTargetSpawner.OnTargetLaunch += onTargetLaunched;
+        animator = GetComponent<Animator>();
+	}
+
+    void onTargetLaunched(YoumuSlashBeatMap.TargetBeat target)
+    {
+        animator.Play("TutorialBeat" + (target.HitDirection == YoumuSlashBeatMap.TargetBeat.Direction.Right ? "Right" : "Left"));
+    }
+	
+	void Update ()
+    {
+		
+	}
+}
