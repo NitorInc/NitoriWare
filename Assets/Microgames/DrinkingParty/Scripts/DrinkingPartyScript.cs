3a260dc6168786806fd178ffc4623ce5f5723d2c
WilliamDouris <William.Douris@github.com>
2018-09-12 19:20:28 +0200

    Add Space input + Change size with space

--- Assets/Microgames/DrinkingParty/Scripts/DrinkingPartyScript.cs
+++ Assets/Microgames/DrinkingParty/Scripts/DrinkingPartyScript.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class DrinkingPartyScript : MonoBehaviour
 {
-    private float ScaleLose = 0.1f;
     public GameObject ObjectLiquide1;
+    public float scale = 2.0f;
+
     // Use this for initialization
     void Start()
     {
-
+        ObjectLiquide1 = GameObject.Find("/Liquide1");
+        //ObjectLiquide1 = GameObject.Find("/Cara_1");
     }
 
     // Update is called once per frame
@@ -18,9 +20,11 @@ public class DrinkingPartyScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             print("space key was pressed");
-            ObjectLiquide1 = GameObject.Find("/Liquide1");
-            SpriteRenderer spriteRenderer = ObjectLiquide1.GetComponent<SpriteRenderer>();
-            spriteRenderer.gameObject.SetActive(false);
+            ObjectLiquide1.transform.localScale = new Vector3(0.09395978f, 0.20f,1f);
+            //SpriteRenderer spriteRenderer = ObjectLiquide1.GetComponent<SpriteRenderer>();
+            //spriteRenderer.gameObject.SetActive(false);
         }
     }
 }
+
+//ObjectLiquide1.transform.Translate(1, 1, 1); Use for translate the sprite
