63a267616e9b9d3c304a252f72afedee632327a8
Brian Intile <brianintile@gmail.com>
2018-09-12 20:26:49 -0400

    Improve number logic further for bursts

--- Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlashBeatMap.cs
+++ Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlashBeatMap.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu(menuName = "Microgame Assets/YoumuSlash/Beat Map")]
 public class YoumuSlashBeatMap : ScriptableObject
@@ -134,6 +135,11 @@ public class YoumuSlashBeatMap : ScriptableObject
                 return null;
         }
         return null;
+    }
+
+    public TargetBeat getNextLaunchingTarget(float beat)
+    {
+        return TargetBeats.FirstOrDefault(a => a.LaunchBeat >= beat);
     }
 
 }
--- Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlashTutorialNumber.cs
+++ Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlashTutorialNumber.cs
@@ -7,24 +7,31 @@ public class YoumuSlashTutorialNumber : MonoBehaviour
     private const string ParamName = "Pulse";
 
     [SerializeField]
+    YoumuSlashTimingData timingData;
+    [SerializeField]
     private YoumuSlashBeatMap.TargetBeat.Direction direction;
     [SerializeField]
     private Sprite[] stageSprites;
+    [SerializeField]
+    private YoumuSlashTutorialNumber otherSideNumber;
 
     private Animator animator;
     private SpriteRenderer spriteRenderer;
-    private bool skipBeat;
+    private bool skipNextBeat;
+    private int MaxStage => stageSprites.Length - 1;
+    private const int maxNaturalStage = 2;
 
     int stage = 0;
-    private void setStage(int value)
+    public int getStage() => stage;
+    public void setStage(int value)
     {
-        stage = value;
-        if (value != 0 && value < stageSprites.Length)
-        {
-            animator.ResetTrigger(ParamName);
-            animator.SetTrigger(ParamName);
-            spriteRenderer.sprite = stageSprites[value];
-        }
+        stage = Mathf.Min(value, MaxStage);
+        animator.ResetTrigger(ParamName);
+        animator.SetTrigger(ParamName);
+        spriteRenderer.sprite = stageSprites[stage];
+
+        CancelInvoke("offBeatIncrement");
+        skipNextBeat = false;
     }
     
     void Start ()
@@ -41,42 +48,54 @@ public class YoumuSlashTutorialNumber : MonoBehaviour
             return;
         if (target.HitDirection != direction)
             return;
-        
-        switch(stage)
+
+        if (otherSideNumber.getStage() > 0)  //If other side is not on stage 0, disable that and set this to next stage
         {
-            case (0):   //Start 1 under normal launch
-                setStage(1);
-                break;
-            case (1):   //Force to 2 if we're already in stage 1 (usually means target is half-beat)
-                setStage(2);
-                break;
-            default:
-                break;
+            setStage(otherSideNumber.getStage() + 1);
+            otherSideNumber.setStage(0);
         }
-        if (target.LaunchBeat % 1f != 0f)
-            skipBeat = true;
+        else   //Otherwise we increment it ourselves
+        {
+            setStage(stage + 1);   //Trigger next stage by default, max is handled naturally
+        }
+
+        if (target.LaunchBeat % 1f > 0f)
+            handleOffbeat();    //Set up off-beat increment loop if not on whole number beat
+    }
+
+    //Called after assigning stage when not on beat
+    void handleOffbeat()
+    {
+        skipNextBeat = true;
+        if (stage > 0)
+            Invoke("offBeatIncrement", timingData.BeatDuration);
+    }
+
+    void offBeatIncrement()
+    {
+        if (stage < maxNaturalStage)
+            setStage(stage + 1);
+        else
+            setStage(0);
+        handleOffbeat();
     }
 
     void onBeat(int beat)
     {
         if (!gameObject.activeInHierarchy)
             return;
-        if (skipBeat)   //Beat skipped if half-beat was activated after last beat
+        if (skipNextBeat)   //Beat skipped if off-beat increment occured after last beat
         {
-            skipBeat = false;
+            skipNextBeat = false;
             return;
         }
-        
-        switch (stage)
-        {
-            case (1):   //Move on to 2 if we're at 1
-                setStage(2);
-                break;
-            case (2):   //Reset if we're at 2
-                setStage(0);
-                break;
-            default:
-                break;
-        }
+        if (timingData.BeatMap.getNextLaunchingTarget((float)beat).LaunchBeat <= (float)beat)   //Don't increment if a new target is set to launch this beat
+            return;
+
+        //Increment or reset stage normally
+        if (stage >= maxNaturalStage)
+            setStage(0);
+        else if (stage > 0)
+            setStage(stage + 1);
     }
 }
