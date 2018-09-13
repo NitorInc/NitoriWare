fb3e7e52c2756db8c7ef747a72b097a81c6a07b0
WilliamDouris <William.Douris@github.com>
2018-09-13 17:42:00 +0200

    Input Percent_bottle + SetBottleBar (Scale + Pos)

--- Assets/Microgames/DrinkingParty/Scripts/DrinkingPartyScript.cs
+++ Assets/Microgames/DrinkingParty/Scripts/DrinkingPartyScript.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class DrinkingPartyScript : MonoBehaviour
 {
     public GameObject ObjectLiquide1;
-    public float scale = 2.0f;
+    public GameObject ObjectLiquide2;
+    public float percent_bottle1 = 100;
+    public float percent_bottle2 = 100;
+
+    private Vector3 Liquide1Scale;
+    private Vector3 Liquide2Scale;
+    private Vector3 Liquide1Pos;
+    private Vector3 Liquide2Pos;
 
     // Use this for initialization
     void Start()
     {
-        ObjectLiquide1 = GameObject.Find("/Liquide1");
-        //ObjectLiquide1 = GameObject.Find("/Cara_1");
+        
+    }
+
+    void SetBottleBar()
+    {
+        Liquide1Scale.Set(0.1f, 0.45f * (percent_bottle1 / 100), 1f);
+        Liquide2Scale.Set(0.1f, 0.45f * (percent_bottle2 / 100), 1f);
+        Liquide1Pos.Set(-0.36f, 1.03f - (0.01345f * (100 - percent_bottle1)), 0f);//0.01345 = diff(up and down)/100
+        Liquide2Pos.Set(0.67f, 1.03f - (0.01345f * (100 - percent_bottle2)), 0f);
+
+        ObjectLiquide1.transform.localScale = Liquide1Scale;
+        ObjectLiquide2.transform.localScale = Liquide2Scale;
+
+        ObjectLiquide1.transform.position = Liquide1Pos; 
+        ObjectLiquide2.transform.position = Liquide2Pos; 
     }
 
     // Update is called once per frame
@@ -19,12 +39,9 @@ public class DrinkingPartyScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            print("space key was pressed");
-            ObjectLiquide1.transform.localScale = new Vector3(0.09395978f, 0.20f,1f);
-            //SpriteRenderer spriteRenderer = ObjectLiquide1.GetComponent<SpriteRenderer>();
-            //spriteRenderer.gameObject.SetActive(false);
+            //print("space key was pressed");
+            percent_bottle1 = percent_bottle1 - 5;
+            SetBottleBar();
         }
     }
 }
-
-//ObjectLiquide1.transform.Translate(1, 1, 1); Use for translate the sprite
