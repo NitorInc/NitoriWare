a06163544734ea7d7d243f91ecbc9ac6db2d6012
hanss314 <hanss314@gmail.com>
2018-09-15 16:43:12 -0400

    Add win detection

--- Assets/Microgames/KagerouCut/Scripts/FurBallController.cs
+++ Assets/Microgames/KagerouCut/Scripts/FurBallController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FurBall : MonoBehaviour {
+public class FurBallController : MonoBehaviour {
+    [SerializeField]
+    GameObject sprite;
+    
+    private bool shouldShrink = false;
+    private Transform t;
+    private float speed = 0.01f;
+    private bool exists = true;
+    void Start(){
+        t = sprite.transform;
+    }
+    void Update(){
+        if (shouldShrink){
+            if (t.localScale.x <= 0){
+                Destroy(this.gameObject); 
+            }
+            t.localScale -= new Vector3(speed, speed, speed);
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnTriggerEnter2D (Collider2D collider) { shouldShrink = true; }
+    void OnTriggerExit2D (Collider2D collider) { shouldShrink = false; }
 }
---
+++ Assets/Microgames/KagerouCut/Scripts/KagerouCutMain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KagerouCutMain : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject[] furballs;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool ballsLeft = false;
+        foreach (GameObject ball in furballs){
+            if (ball != null){
+                ballsLeft = true;
+                break;
+            }
+        }
+        if (!ballsLeft) {
+            print("Winner!");
+            MicrogameController.instance.setVictory(victory: true, final: true);
+        }
+	}
+}
