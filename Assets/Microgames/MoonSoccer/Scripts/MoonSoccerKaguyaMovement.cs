df6f1b33e9931c2ebab0de357831ccee5360b192
Naomi B <noterday@outlook.com>
2018-09-15 13:40:32 -0400

    Add horizontal movement to the kaguya movement script

--- Assets/Microgames/MoonSoccer/Scripts/MoonSoccerKaguyaMovement.cs
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerKaguyaMovement.cs
@@ -16,23 +16,34 @@ public class MoonSoccerKaguyaMovement : MonoBehaviour {
     [SerializeField]
     private float maxHeight = 1f;
     
+    [Header("X Movement Distance")]
+    [SerializeField]
+    private float xMovement = 1f;   
+    
     private bool downward = true;
-
-	// Update is called once per frame
+    
+    private float moveDistance = 0f;
+    
+	// Update Kaguya's position by moving her vertically according to moveSpeed, then adjusting her x value based on her height for diagonal movement.
 	void Update () {
+        float x = transform.position.x;
+        float y = transform.position.y;
 		if (downward == true)
         {
             if (transform.position.y >= minHeight)
-                transform.position = new Vector2(transform.position.x, transform.position.y - moveSpeed * Time.deltaTime);
+                y = transform.position.y - moveSpeed * Time.deltaTime;
             else
                 downward = false;
         }
         else
         {
             if (transform.position.y <= maxHeight)
-                transform.position = new Vector2(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime);
+                y = transform.position.y + moveSpeed * Time.deltaTime;
             else
                 downward = true;
         }
+        moveDistance = (minHeight * -1) + maxHeight;
+        x = ((y - minHeight) / moveDistance) * xMovement;
+        transform.position = new Vector2(-5.2f + x, y);
 	}
 }
