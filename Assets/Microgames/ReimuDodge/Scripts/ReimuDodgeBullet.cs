bf9131046261b40c179c28e423c206fc2c49d9e7
dexskore <40778842+dexskore@users.noreply.github.com>
2018-09-15 23:05:36 -0400

    Imported sprites and created bullet and Reimu animation

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuDodgeBullet : MonoBehaviour {
+    // A Unity in-editor variable
+    [Header("The thing to fly towards")]
+    [SerializeField]
+    private GameObject target;
+
+    // Stores the direction of travel for the bullet
+    private Vector2 trajectory;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Calculate a trajectory towards the target
+        trajectory = (target.transform.position - transform.position).normalized;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Only start moving after the trajectory has been set
+        if (trajectory != null)
+        {
+            // Move the bullet a certain distance based on trajectory speed and time
+            Vector2 newPosition = (Vector2)transform.position + (trajectory * Time.deltaTime);
+            transform.position = newPosition;
+        }
+    }
+}
