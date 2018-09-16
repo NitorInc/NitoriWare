998d10e8593906f9b9c59eb20c03b31d47592be6
waterbuddy16 <watercalciumandchlorides@gmail.com>
2018-09-16 19:01:14 -0400

    Initial Scene Setup/ Prototype

---
+++ Assets/Microgames/ZombieParts/Scripts/ZombiePartsLimbLocation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombiePartsLimbLocation : MonoBehaviour {
+
+	[SerializeField]
+	private string correctLimbTag = "";
+	public bool completed = false;
+	
+	private void OnCollisionStay2D(Collision2D other) {
+		if (completed && other.gameObject.tag == correctLimbTag) {
+			MouseGrabbable otherGrabbable = other.gameObject.GetComponent<MouseGrabbable>();
+			if (otherGrabbable != null && !otherGrabbable.grabbed) {
+				other.gameObject.transform.position = transform.position;
+				otherGrabbable.enabled = false;
+				completed = true;
+				// MicrogameController.instance.setVictory(true, true);
+			}
+		}
+	}
+}
---
+++ Assets/Microgames/ZombieParts/Scripts/ZombiePartsLimbTilt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombiePartsLimbTilt : MonoBehaviour {
+
+	[SerializeField]
+	private AnimationCurve speedToTilt;
+	[SerializeField]
+	private float smoothness;
+	private Vector3 lastPosition = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		lastPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 velocity = transform.position - lastPosition;
+		transform.eulerAngles = Vector3.forward * Mathf.LerpAngle(
+			transform.eulerAngles.z,
+			speedToTilt.Evaluate(velocity.x / Time.unscaledDeltaTime),
+			Time.deltaTime * smoothness
+		);
+		lastPosition = transform.position;
+	}
+}
