62f26e94c6829174a04f5c3d486ea21f4ef6dffb
WilliamDouris <William.Douris@github.com>
2018-09-20 22:32:10 +0200

    Add Control Animation + Victory end

--- Assets/Microgames/DrinkingParty/Scripts/DrinkingPartyScript.cs
+++ Assets/Microgames/DrinkingParty/Scripts/DrinkingPartyScript.cs
@@ -6,9 +6,17 @@ public class DrinkingPartyScript : MonoBehaviour
 {
     public GameObject ObjectLiquide1;
     public GameObject ObjectLiquide2;
+    public GameObject ObjectCara1;
+    public GameObject ObjectCara2;
+    public Sprite Cara1_Anim1;
+    public Sprite Cara1_Anim2;
+    public Sprite Cara1_Anim3;
+    public Sprite Cara2_Anim1;
+    public Sprite Cara2_Anim2;
+    public Sprite Cara2_Anim3;
     public float percent_bottle1 = 100;
     public float percent_bottle2 = 100;
-
+    
     private Vector3 Liquide1Scale;
     private Vector3 Liquide2Scale;
     private Vector3 Liquide1Pos;
@@ -17,7 +25,8 @@ public class DrinkingPartyScript : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        
+        ObjectCara1.GetComponent<SpriteRenderer>().sprite = Cara1_Anim1;
+        ObjectCara2.GetComponent<SpriteRenderer>().sprite = Cara2_Anim1;
     }
 
     void SetBottleBar()
@@ -31,16 +40,49 @@ public class DrinkingPartyScript : MonoBehaviour
         ObjectLiquide2.transform.localScale = Liquide2Scale;
 
         ObjectLiquide1.transform.position = Liquide1Pos; 
-        ObjectLiquide2.transform.position = Liquide2Pos; 
+        ObjectLiquide2.transform.position = Liquide2Pos;
+
+        UpdateAnimation();
+        IsFinish();
     }
 
-    // Update is called once per frame
+    void IsFinish ()
+    {
+        if(percent_bottle1 <= 0)
+        {
+            MicrogameController.instance.setVictory(victory: true, final: true);
+            //Do final animation ?
+        }
+    }
+
+    void UpdateAnimation()
+    {
+        if(percent_bottle1<35)
+        {
+            ObjectCara1.GetComponent<SpriteRenderer>().sprite = Cara1_Anim3;
+        }
+        else if (percent_bottle1<75)
+        {
+            ObjectCara1.GetComponent<SpriteRenderer>().sprite = Cara1_Anim2;
+        }
+
+        if (percent_bottle2 < 35)
+        {
+            ObjectCara2.GetComponent<SpriteRenderer>().sprite = Cara2_Anim3;
+        }
+        else if (percent_bottle2 < 75)
+        {
+            ObjectCara2.GetComponent<SpriteRenderer>().sprite = Cara2_Anim2;
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //print("space key was pressed");
             percent_bottle1 = percent_bottle1 - 5;
+            percent_bottle2 = percent_bottle2 - 5;
             SetBottleBar();
         }
     }
