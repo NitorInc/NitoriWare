23d55bfad89fa9a2fe3576bd3a23558eb8587ad8
Naomi B <noterday@outlook.com>
2018-10-03 21:41:43 -0400

    Add a script to the bunny that makes it flip it's sprite at an interval of time

---
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerBunnyLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonSoccerBunnyLook : MonoBehaviour {
+    
+    private float timer = 0f;
+    private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+    
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > 1) {
+			spriteRenderer.flipX = !spriteRenderer.flipX;
+			timer = 0f;
+		}
+		
+	}
+}
