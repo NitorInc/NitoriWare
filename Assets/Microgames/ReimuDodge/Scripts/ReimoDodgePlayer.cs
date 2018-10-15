74bb7521a0d33443ce0ecaa3ad96c9ef71b573ad
Skullyhead <irskullyhead@gmail.com>
2018-10-15 10:32:12 +0200

    Forgot to add scripts to commit

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimoDodgePlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimoDodgePlayer : MonoBehaviour {
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Player was hit!");
+    }
+}
---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuDodgeBullet : MonoBehaviour {
+
+    [Header("The thing to fly towards")]
+    [SerializeField]
+    private GameObject target;
+
+    private Vector2 trajectory;
+
+    private float speedMod;
+
+	// Use this for initialization
+	void Start () {
+        speedMod = 1;
+        this.trajectory = GetTrajectoryTowardsTarget();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.trajectory != null){
+            this.transform.position = GetNewPosition();
+        }
+        this.trajectory = GetTrajectoryTowardsTarget();
+    }
+
+    Vector2 GetTrajectoryTowardsTarget() {
+        return (target.transform.position - this.transform.position).normalized;
+    }
+
+    Vector2 GetNewPosition() {
+        if (speedMod<20) {
+            speedMod = speedMod + 0.1F;
+        }
+        return (Vector2)transform.position + (trajectory * Time.deltaTime * speedMod);
+    }
+}
