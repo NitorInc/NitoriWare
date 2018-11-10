f5d139bf0cc7bc23f9fe63aa767ca5c45840ea3a
Naomi B <noterday@outlook.com>
2018-11-10 17:01:09 -0500

    Add a list of selectable ranges for the defender movement

--- Assets/Microgames/MoonSoccer/Scripts/MoonSoccerDefenderMovement.cs
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerDefenderMovement.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class MoonSoccerDefenderMovement : MonoBehaviour {
     
+    
+    private enum VerticalMovementRange
+    {
+        None,
+        TopHalf,
+        BottomHalf,
+        FullScreen
+    }
+    
+    [Header("Vertical Movement")]
     // The speed of the vertical movement
-    [Header("Vertical Movement Speed")]
     [SerializeField]
     private float moveSpeed = 1f;
 
     // Minimum height the object can reach before changing direction
-    [Header("Vertical Movement Range")]
     [SerializeField]
-    private float minHeight = 1f;
-    
-    // Maximum height the object can reach before changing direction
-    [SerializeField]
-    private float maxHeight = 1f;
+    private VerticalMovementRange movementRange;
     
     // The lenght between the leftmost and rightmost point the sprite can reach horizontally
     [Header("Horizontal Movement Length")]
@@ -24,8 +28,6 @@ public class MoonSoccerDefenderMovement : MonoBehaviour {
     private float xMovement = 1f;
     
     // The direction the object will start moving in
-    [Header("Initial Direction")]
-    [SerializeField]
     private bool downward = true;
     
     // The total distance between the top and bottom boundaries of the vertical movement
@@ -34,8 +36,40 @@ public class MoonSoccerDefenderMovement : MonoBehaviour {
     // The starting x value of the object transform
     private float startX = 0f;
     
+    
+    private float minHeight = 0f;
+    private float maxHeight = 0f;
+    
     // Initialization
     void Start () {
+        // Determine the min and max heights based on the selected movement range
+        switch  (movementRange)
+        {
+            case VerticalMovementRange.None:
+            {
+                minHeight = 0f;
+                maxHeight = 0f;
+                break;
+            }
+            case VerticalMovementRange.TopHalf:
+            {
+                minHeight = -0.5f;
+                maxHeight = 1.5f;
+                break;
+            }
+            case VerticalMovementRange.BottomHalf:
+            {
+                minHeight = -2.9f;
+                maxHeight = -0.5f;
+                break;
+            }
+            case VerticalMovementRange.FullScreen:
+            {
+                minHeight = -2.9f;
+                maxHeight = 1.5f;
+                break;
+            }
+        }
         moveDistance = (minHeight * -1) + maxHeight;
         startX = transform.position.x;
     }
@@ -62,7 +96,6 @@ public class MoonSoccerDefenderMovement : MonoBehaviour {
             }
             x = -((y - minHeight) / moveDistance) * xMovement;
             transform.position = new Vector3(startX + x, y, transform.position.z);
-        print(transform.position);
         }
 	}
 }
