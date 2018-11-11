e5d680e95400258d9e431c94fb480167d2edfe2d
Naomi B <noterday@outlook.com>
2018-11-10 20:22:16 -0500

    Add flashing arrows around the player at the start as a guide

---
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerGuideArrowUpdate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonSoccerGuideArrowUpdate : MonoBehaviour {
+
+    [Header("Blinking Speed")]
+    [SerializeField]
+    private float blinkSpeed = 0.2f;
+
+    private float blinkTimer = 0f;
+
+	// Use this for initialization
+	void Start () {
+		Invoke("DestroySelf", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		blinkTimer += Time.deltaTime;
+        if (blinkTimer >= blinkSpeed)
+        {
+            blinkTimer = 0f;
+            this.gameObject.GetComponent<SpriteRenderer> ().enabled = ! this.gameObject.GetComponent<SpriteRenderer> ().enabled;
+        }
+	}
+    
+    void DestroySelf () {
+        Destroy(gameObject);
+    }
+    
+}
