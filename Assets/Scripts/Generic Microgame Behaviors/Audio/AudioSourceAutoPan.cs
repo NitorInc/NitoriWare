82115a214ecce94e3b205c3f9ab14de73890cd25
Brian Intile <brianintile@gmail.com>
2018-12-11 00:23:48 -0500

    Add AudIoSourceAutoPan script

---
+++ Assets/Scripts/Generic Microgame Behaviors/Audio/AudioSourceAutoPan.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceAutoPan : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private float xPosBoundMult = 1f;
+    
+	void Start ()
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+	}
+	
+	void Update ()
+    {
+        if (audioSource.isPlaying)
+            audioSource.panStereo = AudioHelper.getAudioPan(transform.position.x, xPosBoundMult);
+	}
+}
