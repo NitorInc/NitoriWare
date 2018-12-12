04a0c9f6b9c0353da3f8f70ccf3c5bcbe8e4628d
Brian Intile <brianintile@gmail.com>
2018-12-12 11:52:49 -0500

    YoumuSlash: Add music distortion on fail

---
+++ Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlashMusicFail.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YoumuSlashMusicFail : MonoBehaviour
+{
+    Animator animator;
+    
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+        YoumuSlashPlayerController.onFail += onFail;
+	}
+	
+	void onFail()
+    {
+        animator.enabled = true;
+	}
+}
