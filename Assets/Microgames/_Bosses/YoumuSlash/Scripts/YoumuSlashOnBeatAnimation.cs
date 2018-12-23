f6745e61218e519239a76e9e617e9eb5940afd45
Brian Intile <brianintile@gmail.com>
2018-12-23 01:30:04 -0500

    Add beat bob animation to myons

---
+++ Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlashOnBeatAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YoumuSlashOnBeatAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private string triggerName = "Beat";
+
+    Animator beatAnimator;
+
+	void Start()
+    {
+        YoumuSlashTimingController.onBeat += onBeat;
+        beatAnimator = GetComponent<Animator>();
+	}
+
+    void onBeat(int beat)
+    {
+        beatAnimator.SetTrigger(triggerName);
+    }
+}
