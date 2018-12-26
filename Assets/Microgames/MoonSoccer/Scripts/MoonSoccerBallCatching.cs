9f99daa1bd86e664267f94379b09634cb11720cc
Naomi B <noterday@outlook.com>
2018-12-26 18:24:59 -0500

    Add different ball catching behavior for each character

--- Assets/Microgames/MoonSoccer/Scripts/MoonSoccerBallCatching.cs
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerBallCatching.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class MoonSoccerBallCatching : MonoBehaviour {
     
     // Check for collision with the ball object and causes failure if it does
-    void OnTriggerEnter2D(Collider2D col) {
+    void OnTriggerEnter2D(Collider2D col) 
+    {
         if (col.gameObject.name == "Ball")
         {
-            Destroy(col.gameObject);
-            MicrogameController.instance.setVictory(victory: false, final: true);
+            if (gameObject.name == "Kaguya")
+            // When the ball hits Kaguya
+            { 
+                Destroy(col.gameObject);
+                MicrogameController.instance.setVictory(victory: false, final: true);
+            // When the ball hits tewi
+            } else if (gameObject.name == "Rabbit") { 
+                // TODO: actual little animation there
+                MicrogameController.instance.setVictory(victory: false, final: true);
+            // When the ball hits the two defender characters
+            } else {
+                col.gameObject.GetComponent<MoonSoccerBall>().moveSpeed = -16;
+                col.gameObject.GetComponent<MoonSoccerBall>().vMoveSpeed = 16;
+                MicrogameController.instance.setVictory(victory: false, final: true);
+            }
         }
     }
 }
