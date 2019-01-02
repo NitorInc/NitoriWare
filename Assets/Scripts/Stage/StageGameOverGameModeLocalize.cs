9d6b5b777eb640773b968af04400d9662d679e2f
Brian Intile <brianintile@gmail.com>
2019-01-01 22:21:20 -0500

    Add game mode text to pause and game over menus

---
+++ Assets/Scripts/Stage/StageGameOverGameModeLocalize.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageGameOverGameModeLocalize : MonoBehaviour
+{
+	void Start ()
+    {
+        var stageName = gameObject.scene.name;
+        var localizedName = TextHelper.getLocalizedText($"menu.gamemode.{stageName}".ToLower(), stageName);
+        GetComponent<Text>().text = "Mode: " + localizedName;
+    }
+}
