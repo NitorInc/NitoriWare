00eed697e7e984d9b21f3c98009568deb8605b43
Brian Intile <brianintile@gmail.com>
2019-01-01 23:48:19 -0500

    Base menu changes

---
+++ Assets/Scripts/Menu/MagFestSettingsAccess.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MagFestSettingsAccess : MonoBehaviour
+{
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Alpha2) && Input.GetKey(KeyCode.Alpha8) && Input.GetKeyDown(KeyCode.Alpha4))
+            GetComponent<Button>().onClick.Invoke();
+		
+	}
+}
