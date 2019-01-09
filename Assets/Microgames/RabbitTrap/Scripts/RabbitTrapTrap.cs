cdfb28bb8d2f22b386418f39cb5bde3094aeadd7
Skullyhead <irskullyhead@gmail.com>
2019-01-09 13:50:08 +0100

    Victim walks from right to left in an acceptable pase

---
+++ Assets/Microgames/RabbitTrap/Scripts/RabbitTrapTrap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitTrapTrap : MonoBehaviour {
+
+    //Startposition:
+    //Position: X = 1, Y=-1.8
+    //Scale:    X = 2, Y = 2
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Microgames/RabbitTrap/Scripts/RabbitTrapVictim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitTrapVictim : MonoBehaviour {
+
+    [Header("Travel speed")]
+    [SerializeField]
+    private float speed;
+
+    private Vector2 trajectory;
+
+    // Use this for initialization
+    void Start () {
+        trajectory = new Vector2(speed,0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = GetNewPosition();
+    }
+
+    Vector2 GetNewPosition()
+    {
+        return (Vector2)transform.position + (trajectory * Time.deltaTime * -speed);
+    }
+}
