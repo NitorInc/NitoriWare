2763fd0966ac9b3941a94881106c8aa42217ac71
Naomi B <noterday@outlook.com>
2019-01-27 21:19:07 -0500

    Comment animation code and adjust ball speed for them

--- Assets/Microgames/MoonSoccer/Scripts/MoonSoccerBallCatching.cs
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerBallCatching.cs
@@ -6,27 +6,27 @@ public class MoonSoccerBallCatching : MonoBehaviour {
 		
 	private MoonSoccerBall ballScript;
     
-    // Check for collision with the ball object and causes failure if it does
+    // Check for collision againt the character which causes the minigame to fail and a ball animation to play out
     void OnTriggerEnter2D(Collider2D col) 
     {
 		
         if (col.gameObject.name == "Ball" && !MicrogameController.instance.getVictoryDetermined())
         {
 			ballScript = col.gameObject.GetComponent<MoonSoccerBall>();
+            // When the ball hits Kaguya it is destroyed
             if (gameObject.name == "Kaguya")
-            // When the ball hits Kaguya
             { 
                 Destroy(col.gameObject);
                 MicrogameController.instance.setVictory(victory: false, final: true);
-            // When the ball hits tewi
+            // When the ball hits Tewi it plays the ball bounce animation
             } else if (gameObject.name == "Rabbit") { 
                 ballScript.moveSpeed = -3;
                 col.gameObject.GetComponentInChildren<Animator>().Play("MoonSoccerBallBounce");
                 MicrogameController.instance.setVictory(victory: false, final: true);
-            // When the ball hits the two defender characters
+            // When the ball hits the two defender characters it flies off into space
             } else {
-                ballScript.moveSpeed = -16;
-                ballScript.vMoveSpeed = 16;
+                ballScript.moveSpeed = -18;
+                ballScript.vMoveSpeed = 14;
                 MicrogameController.instance.setVictory(victory: false, final: true);
             }
         }
--- Assets/Microgames/MoonSoccer/Scripts/MoonSoccerZOrdering.cs
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerZOrdering.cs
@@ -6,6 +6,13 @@ public class MoonSoccerZOrdering : MonoBehaviour {
     
 	// Update is called once per frame
 	void Update () {
-		GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(transform.position.y - (GetComponent<SpriteRenderer>().bounds.size.y/2) * -100);
+		if (MicrogameController.instance.getVictoryDetermined())
+		{
+			// Every character is set to be under the ball once the minigame ends, to make sure some ball catching animations dont look weird
+			GetComponent<SpriteRenderer>().sortingOrder = -1;
+		} else {
+			// The sorting order is determined based on the position of the sprite's lowest point (the character's feet)
+			GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(transform.position.y - (GetComponent<SpriteRenderer>().bounds.size.y/2) * -100);
+		}
 	}
 }
