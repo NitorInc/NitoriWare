e412593eda724b9eadf178d701f26fbb6a21d745
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-28 14:44:33 +0200

    Add death particle effect [ReimuDodge]
    Might've forgot to save the scene for the last few commits

--- Assets/Microgames/ReimuDodge/Scripts/ReimuDodgePlayer.cs
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgePlayer.cs
@@ -9,6 +9,9 @@ public class ReimuDodgePlayer : MonoBehaviour
     [Header("Death Sound")]
     [SerializeField]
     private AudioClip death_sound;
+    [Header("Effect played when reimu dies")]
+    [SerializeField]
+    private ParticleSystem deathEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         print("I'm hit!");
@@ -18,6 +21,7 @@ public class ReimuDodgePlayer : MonoBehaviour
     void Kill()
     {
         if (!alive) return; //Can't die if you're not alive!
+        alive = false;
         //Make the sprite dissapear
         SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         spriteRenderer.gameObject.SetActive(false);
@@ -31,5 +35,6 @@ public class ReimuDodgePlayer : MonoBehaviour
             volume: 0.5f,
             panStereo: AudioHelper.getAudioPan(transform.position.x)
          );
+         deathEffect.Play();
     }
 }
