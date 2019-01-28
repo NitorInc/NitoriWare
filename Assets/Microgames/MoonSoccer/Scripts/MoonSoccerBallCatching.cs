4c6ec1a42343dded23628dcfbe3b97f763626326
Naomi B <noterday@outlook.com>
2019-01-27 21:06:06 -0500

    Add animation changes to the ball prefab

--- Assets/Microgames/MoonSoccer/Scripts/MoonSoccerBallCatching.cs
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerBallCatching.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoonSoccerBallCatching : MonoBehaviour {
+		
+	private MoonSoccerBall ballScript;
     
     // Check for collision with the ball object and causes failure if it does
     void OnTriggerEnter2D(Collider2D col) 
     {
+		
         if (col.gameObject.name == "Ball" && !MicrogameController.instance.getVictoryDetermined())
         {
+			ballScript = col.gameObject.GetComponent<MoonSoccerBall>();
             if (gameObject.name == "Kaguya")
             // When the ball hits Kaguya
             { 
@@ -16,13 +20,13 @@ public class MoonSoccerBallCatching : MonoBehaviour {
                 MicrogameController.instance.setVictory(victory: false, final: true);
             // When the ball hits tewi
             } else if (gameObject.name == "Rabbit") { 
-                col.gameObject.GetComponent<MoonSoccerBall>().moveSpeed = -3;
+                ballScript.moveSpeed = -3;
                 col.gameObject.GetComponentInChildren<Animator>().Play("MoonSoccerBallBounce");
                 MicrogameController.instance.setVictory(victory: false, final: true);
             // When the ball hits the two defender characters
             } else {
-                col.gameObject.GetComponent<MoonSoccerBall>().moveSpeed = -16;
-                col.gameObject.GetComponent<MoonSoccerBall>().vMoveSpeed = 16;
+                ballScript.moveSpeed = -16;
+                ballScript.vMoveSpeed = 16;
                 MicrogameController.instance.setVictory(victory: false, final: true);
             }
         }
