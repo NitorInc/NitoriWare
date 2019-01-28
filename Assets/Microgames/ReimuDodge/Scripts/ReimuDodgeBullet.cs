0159c3ad4ab23bcaa8b97fcd083a10d4431be076
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-28 14:12:10 +0200

    Add bullet script

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuDodgeBullet : MonoBehaviour
+{
+
+    [Header("The object the bullet flies towards")]
+    [SerializeField]
+    private GameObject target;
+    [Header("The speed the bullet moves in")]
+    [SerializeField]
+    private float speed = 1.0f;
+
+    private Vector2 trajectory;
+    // Use this for initialization
+    void Start()
+    {
+        trajectory = (target.transform.position - transform.position).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trajectory != null)
+        {
+            transform.position = (Vector3)Vector2.MoveTowards((Vector2)transform.position, (Vector2)target.transform.position, Time.deltaTime * speed);
+        }
+    }
+}
