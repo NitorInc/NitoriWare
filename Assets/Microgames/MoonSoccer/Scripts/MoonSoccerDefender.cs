b4e4040d4872e476d7ef271ba12228d417b8fdf8
Naomi B <noterday@outlook.com>
2019-01-29 21:01:42 -0500

    Refactor movement and start position code to be in different scripts
    With the new changes to the gameplay, not every opponent that has
    start positions will also be moving, so the start position code is
    moved inside of the collision handling script, which is renamed to
    MoonSoccerDefender

--- Assets/Microgames/MoonSoccer/Scripts/MoonSoccerDefender.cs
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerDefender.cs
@@ -2,10 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoonSoccerBallCatching : MonoBehaviour {
+public class MoonSoccerDefender : MonoBehaviour {
+
+    // Each object has 3 layout structs, and one will be picked at random at the start
+    [Header("Movement Layouts")]
+    [SerializeField]
+    private float position1;
+    [SerializeField]
+    private float position2;
+    [SerializeField]
+    private float position3;
 		
 	private MoonSoccerBall ballScript;
     
+	
+	
+    void Start () {
+        // Get what the chosen layout is
+        int layout = GameObject.Find("LayoutPicker").GetComponent<MoonSoccerLayoutPick>().layout;
+        switch (layout)
+        {
+            case 0:
+				transform.position = new Vector3(transform.position.x, position1, transform.position.z);
+                break;
+            case 1:
+				transform.position = new Vector3(transform.position.x, position2, transform.position.z);
+                break;
+            case 2:
+				transform.position = new Vector3(transform.position.x, position3, transform.position.z);
+                break;
+        }
+	
+	}
+	
+	
+	
+	
     // Check for collision againt the character which causes the minigame to fail and a ball animation to play out
     void OnTriggerEnter2D(Collider2D col) 
     {
---
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerMovement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonSoccerMovement : MonoBehaviour {
+	
+	
+    [Header("Movement Speed")]
+    [SerializeField]
+	private float moveSpeed = 2f;
+	
+    // The lenght between the leftmost and rightmost point the sprite can reach horizontally
+    [Header("Horizontal Movement Length")]
+    [SerializeField]
+    private float xMovement = 1f;
+    
+    // The change in scale that happens when going all the way up the screen, in percentage
+    [Header("Scale Change From Perspective")]
+    [SerializeField]
+    private float scaleChange = 5f;
+    
+    
+    // The upper and lower bound of the vertical movement 
+    [Header("Vertical Movement Range")]
+    [SerializeField]
+    private float TopY = 1.5f;
+    [SerializeField]
+    private float BottomY = -2.9f;
+    
+    // The starting x value of the object transform
+    private float startX = 0f;
+    
+    // The upper and lower bounds of the object's vertical movement
+    private float minHeight = 0f;
+    private float maxHeight = 0f;
+    
+    private bool downward;
+    
+    // The total distance between the top and bottom boundaries of the vertical movement
+    private float moveDistance = 0f;
+    
+    // The scale of the sprite at the very start
+    private Vector3 startScale;
+    
+    // Initialization
+    void Start () {
+        moveDistance = (BottomY * -1) + TopY;
+        startX = transform.position.x;
+        startScale = transform.localScale;
+    }
+
+    
+	// Update the object's position by moving it vertically according to moveSpeed. X value is updated based on the y position
+	void Update () {
+        if (MicrogameController.instance.getVictoryDetermined() != true) {
+            float x = transform.position.x;
+            float y = transform.position.y;
+            if (downward == true)
+            {
+                if (transform.position.y >= BottomY)
+                    y = transform.position.y - moveSpeed * Time.deltaTime;
+                else
+                    downward = false;
+            }
+            else
+            {
+                if (transform.position.y <= TopY)
+                    y = transform.position.y + moveSpeed * Time.deltaTime;
+                else
+                    downward = true;
+            }
+            // Horizontal Movement based on how high they are on screen
+            x = -((y - BottomY) / moveDistance) * xMovement;
+            transform.position = new Vector3(startX + x, y, transform.position.z);
+            // Scale the character's size based on how high they are on screen
+            float vDistance = 1 - ((y - BottomY) / moveDistance);
+            transform.localScale = new Vector3((startScale.x / 100f) * (100-scaleChange + vDistance*scaleChange), 
+                                               (startScale.y / 100f) * (100-scaleChange + vDistance*scaleChange), 
+                                               startScale.z);
+        }
+	}
+}
