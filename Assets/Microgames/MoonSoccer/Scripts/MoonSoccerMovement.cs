39c24434c8a179759e43f0e39a02775ff3e6d992
Naomi B <noterday@outlook.com>
2019-01-29 21:14:00 -0500

    Add a choice of starting direction to the movement script
    All current changes are also applied to prefabs,
    and some adjustment are made to variable names

--- Assets/Microgames/MoonSoccer/Scripts/MoonSoccerMovement.cs
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerMovement.cs
@@ -9,24 +9,25 @@ public class MoonSoccerMovement : MonoBehaviour {
     [SerializeField]
 	private float moveSpeed = 2f;
 	
-    // The lenght between the leftmost and rightmost point the sprite can reach horizontally
-    [Header("Horizontal Movement Length")]
     [SerializeField]
-    private float xMovement = 1f;
-    
-    // The change in scale that happens when going all the way up the screen, in percentage
-    [Header("Scale Change From Perspective")]
-    [SerializeField]
-    private float scaleChange = 5f;
-    
+	private bool startMovingDown = false;
     
     // The upper and lower bound of the vertical movement 
-    [Header("Vertical Movement Range")]
+    [Header("Vertical Ranges")]
     [SerializeField]
     private float TopY = 1.5f;
     [SerializeField]
     private float BottomY = -2.9f;
+	
+    // The lenght between the leftmost and rightmost point the sprite can reach horizontally
+    [SerializeField]
+    private float hMovementWidth = 1f;
     
+    // The change in scale that happens when going all the way up the screen, in percentage
+    [Header("Scale Change for Perspective")]
+    [SerializeField]
+    private float scalePercentage = 5f;
+	
     // The starting x value of the object transform
     private float startX = 0f;
     
@@ -47,6 +48,7 @@ public class MoonSoccerMovement : MonoBehaviour {
         moveDistance = (BottomY * -1) + TopY;
         startX = transform.position.x;
         startScale = transform.localScale;
+		downward = startMovingDown;
     }
 
     
@@ -70,12 +72,12 @@ public class MoonSoccerMovement : MonoBehaviour {
                     downward = true;
             }
             // Horizontal Movement based on how high they are on screen
-            x = -((y - BottomY) / moveDistance) * xMovement;
+            x = -((y - BottomY) / moveDistance) * hMovementWidth;
             transform.position = new Vector3(startX + x, y, transform.position.z);
             // Scale the character's size based on how high they are on screen
             float vDistance = 1 - ((y - BottomY) / moveDistance);
-            transform.localScale = new Vector3((startScale.x / 100f) * (100-scaleChange + vDistance*scaleChange), 
-                                               (startScale.y / 100f) * (100-scaleChange + vDistance*scaleChange), 
+            transform.localScale = new Vector3((startScale.x / 100f) * (100-scalePercentage + vDistance*scalePercentage), 
+                                               (startScale.y / 100f) * (100-scalePercentage + vDistance*scalePercentage), 
                                                startScale.z);
         }
 	}
