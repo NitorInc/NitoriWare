4ffd73621f79a7b1a1095110c3f648c772435c86
Unknown <enalyen@gmail.com>
2019-03-26 18:37:59 +0100

    Started controller logic

---
+++ Assets/Microgames/CheeseFind/Scripts/CheeseFindController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseFindController : MonoBehaviour {
+    enum GameState {
+        InitState
+    };
+
+    [Header("List of all drawers")]
+    [SerializeField]
+    private GameObject[] drawerObjects;
+
+    [Header("List of all items")]
+    [SerializeField]
+    private GameObject[] itemObjects;
+
+
+    private List<CheeseFindDrawer> _drawerScripts;
+
+    private GameState _currentState = GameState.InitState;
+
+    /*
+        mouseObject.SetActive(false);
+        cheeseObject.transform.position += new Vector3(0f, 2f, 0f);
+     */
+
+	void Start () {
+        _drawerScripts = new List<CheeseFindDrawer>();
+		foreach(GameObject drawerObject in drawerObjects) {
+            CheeseFindDrawer drawer = drawerObject.GetComponent<CheeseFindDrawer>();
+            _drawerScripts.Add(drawer);
+
+            drawer.SetController(this);
+        }
+        //TODO: Items animation.
+        //TODO: Call this only after the items animation is completed. Then, after the drawers are closed, let the player interact with them.
+        CloseAllDrawers();
+	}
+
+    void CloseAllDrawers() {
+		foreach(CheeseFindDrawer drawer in _drawerScripts) {
+            drawer.CloseDrawer();
+        }
+        //Invoke("StartGame", 1f);
+    }
+
+    void StartGame() {
+        foreach(CheeseFindDrawer drawer in _drawerScripts) {
+        //    drawer.UnlockDrawer();
+        }
+    }
+	
+	void Update () {
+		switch(_currentState) {
+        case GameState.InitState:
+            break;
+        default:
+            break;
+        }
+	}
+
+    //MicrogameController.instance.setVictory(victory: true, final: true);
+}
--- Assets/Microgames/CheeseFind/Scripts/CheeseFindDrawer.cs
+++ Assets/Microgames/CheeseFind/Scripts/CheeseFindDrawer.cs
@@ -3,19 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CheeseFindDrawer : MonoBehaviour {
-    private bool _isLocked = false;
+    private bool _isLocked = true;
     private bool _isPulling = false;
     private bool _isOpen = false;
 
-    private float _pullFactor = 0f;
+    private float _pullFactor = 1f;
+    private float _pullLimit = .5f;
+    //private float _objectFactor = 0f;
 
-	void Start () {
-		
+    private CheeseFindController _controller;
+
+	void Start() {
 	}
+
+    public void SetController(CheeseFindController controller) {
+        _controller = controller;
+    }
+
+    public void CloseDrawer() {
+        _isLocked = false;
+        //TODO: Should be unlocked only after the closing animation is completed, _isLocked must be set by the CheeseFindController.
+    }
 	
-	void Update () {
+	void Update() {
+        /*print(_objectFactor);
+        if(_currentState == DrawerState.HiddingState) {
+            _objectFactor += Time.deltaTime;
+            if(_objectFactor > 1f)
+                _objectFactor = 1f;
+
+            Vector3 objectPos = new Vector3(
+                transform.position.x,
+                transform.position.y + Mathf.Lerp(2f, 0f, EaseInOutSine(_pullFactor)),
+                transform.position.z);
+
+            cheeseObject.transform.position = objectPos;
+            mouseObject.transform.position = objectPos;
+
+            if(_objectFactor >= 1f) {
+                _isLocked = false;
+                _currentState = DrawerState.ClosedState;
+            }
+        }*/
+
+        if(!_isLocked && !_isPulling && _pullFactor > 0f) {
+            _pullFactor -=  Time.deltaTime;
+            if(_pullFactor < 0f)
+                _pullFactor = 0f;
+
+            Vector3 drawerPos = new Vector3(
+                transform.parent.position.x,
+                transform.parent.position.y - Mathf.Lerp(0f, _pullLimit, EaseInOutSine(_pullFactor)),
+                transform.parent.position.z);
+            transform.position = drawerPos;
+        }
+
         float scaleFactor = .25f * _pullFactor + 1f;
 		transform.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
+
+        
 	}
 
     void OnMouseDown() {
@@ -40,15 +86,14 @@ public class CheeseFindDrawer : MonoBehaviour {
         if(_isLocked || !_isPulling)
             return;
 
-        float maxPull = .5f;
         float cursorY = CameraHelper.getCursorPosition().y;
         float deltaY = transform.parent.position.y - cursorY;
 
-        _pullFactor = Mathf.Clamp(deltaY / maxPull, 0f, 1f);
+        _pullFactor = Mathf.Clamp(deltaY / _pullLimit, 0f, 1f);
         
         Vector3 drawerPos = new Vector3(
             transform.parent.position.x,
-            transform.parent.position.y - Mathf.Lerp(0f, maxPull, EaseInOutQuart(_pullFactor)),
+            transform.parent.position.y - Mathf.Lerp(0f, _pullLimit, EaseInOutQuart(_pullFactor)),
             transform.parent.position.z);
         transform.position = drawerPos;
 
