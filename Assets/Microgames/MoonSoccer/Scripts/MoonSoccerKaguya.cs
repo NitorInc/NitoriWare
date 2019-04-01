5befda5eb996600b7cf5772d2e8b24c1f6304e75
Naomi B <noterday@outlook.com>
2019-03-31 21:17:19 -0400

    Cleanup and comment MoonSoccerKaguya

--- Assets/Microgames/MoonSoccer/Scripts/MoonSoccerKaguya.cs
+++ Assets/Microgames/MoonSoccer/Scripts/MoonSoccerKaguya.cs
@@ -4,33 +4,34 @@ using UnityEngine;
 
 public class MoonSoccerKaguya : MonoBehaviour {
     
-   
+	// The max speed of the character
     [Header("Movement Speed")]
     [SerializeField]
     private float maximumMoveSpeed = 1f;
     
-    // The acceleration of the character
+    // The time it takes for the character to accelerate to max speed
     [SerializeField]
     private float timeBeforeMaxSpeed = 1f;
     
+	
 	
     [Header("Perspective Related Variables")]
     // The lenght between the leftmost and rightmost point the sprite can reach horizontally
     [SerializeField]
     private float horizontalMovement = 1f;
 	
-    // The change in scale that happens when going all the way up the screen, in percentage
+    // The change in sprite size that happens when going all the way up the screen, in percentage
     [SerializeField]
     private float sizeChange = 5f;
     
-	
+	// The vertical boundaries of the character's movement
     [Header("Vertical Movement Range")]
     [SerializeField]
     private float TopY = 1.5f;
     [SerializeField]
     private float BottomY = -2.9f;
 	
-    
+    // The ammount of frames before Kaguya starts moving. Determines the size of the Queue used to track movement
     [Header("Delay Before Movement")]
     [SerializeField]
     private float delay = 0;
@@ -39,24 +40,22 @@ public class MoonSoccerKaguya : MonoBehaviour {
     // The current acceleration
     private float accelerationGained = 0f;
     
-    // The total distance between the top and bottom boundaries of the vertical movement
-    private float moveDistance = 0f;
-    
-    // The starting x value of the object transform
-    private float startX = 0f;
-    
-    // Whether the space key has been pressed yet or not
-    private bool hasKicked = false;
-    
     // Acceleration gained per update
     private float accelerationSpeed = 0f;
     
-    // The scale of the sprite at the very start
+    // The distance between the top and bottom movement boundaries. Needed for perspective
+    private float moveDistance = 0f;
+    
+    // The starting x value of the object
+    private float startX = 0f;
+    
+    // The the starting scale of the sprite
     private Vector3 startScale;
 	
 	// The player's transform used for tracking
 	private Transform playerTransform;
 	
+	// A queue that will contain the player's position over the last X frames.
 	private Queue<float> movementQueue = new Queue<float>();
 	
 	private float goalY;
@@ -69,20 +68,21 @@ public class MoonSoccerKaguya : MonoBehaviour {
 		transform.position = new Vector3(transform.position.x, Random.Range(BottomY, TopY), transform.position.z);
         startScale = transform.localScale;
 		playerTransform = GameObject.Find("Mokou").GetComponent<Transform>();
-         for (int i = 0; i < delay; i++) {
+		// Fill the tracking queue with Kaguya's starting position to add a delay before she moves
+        for (int i = 0; i < delay; i++) {
 			movementQueue.Enqueue(transform.position.y);
-		 }
+		}
     }
     
 	// Update is called once per frame
 	void FixedUpdate () 
     {
-        if (MicrogameController.instance.getVictoryDetermined() != true || hasKicked == false) {
-            goalY = movementQueue.Dequeue();
-			movementQueue.Enqueue(playerTransform.position.y);
+        if (MicrogameController.instance.getVictoryDetermined() != true) {
             float y = transform.position.y;
 			float x = transform.position.x;
-			if (!(Mathf.Abs(goalY-y) < 0.1)) {
+            goalY = movementQueue.Dequeue();
+			movementQueue.Enqueue(playerTransform.position.y);
+			if (!(Mathf.Abs(goalY-y) < 0.1)) { // Avoid moving if the characters are lined up closely enough
 				if (goalY < y && y >= BottomY)
 				{
 					if (accelerationGained >= 0)
