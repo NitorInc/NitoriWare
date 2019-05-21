4fb80a169ce5281cfa082d6bdd2faee76c6c0727
Brian Intile <brianintile@gmail.com>
2019-05-21 17:32:04 -0400

    Revert "YoumuSlash: Better camera looping algorithm"
    This reverts commit 87a4e5ecac344e0cbc225790a6d5a4fd5210a7fb.

--- Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlash3DEnvironmentController.cs
+++ Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlash3DEnvironmentController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class YoumuSlash3DEnvironmentController : MonoBehaviour
 {
     [SerializeField]
+    private Transform stairParent;
+    [SerializeField]
     private GameObject stairObject;
     [SerializeField]
     private Vector2 stairOffset;
@@ -14,22 +16,28 @@ public class YoumuSlash3DEnvironmentController : MonoBehaviour
     [SerializeField]
     private float cameraDollySpeed;
 
+    private int unitsTravelled;
+
     Vector3 StairOffset3D => new Vector3(0f, stairOffset.y, stairOffset.x);
     
-	//void Start ()
- //   {
- //       for (int i = 0; i < stairParent.childCount; i++)
- //       {
- //           stairParent.GetChild(i).localPosition = StairOffset3D * i;
- //       }
-	//}
+	void Start ()
+    {
+        for (int i = 0; i < stairParent.childCount; i++)
+        {
+            stairParent.GetChild(i).localPosition = StairOffset3D * i;
+        }
+        unitsTravelled = 1;
+	}
 	
 	void Update ()
     {
         cameraParent.position += StairOffset3D.resize(cameraDollySpeed * Time.deltaTime);
-        if (cameraParent.localPosition.y > stairOffset.y)
+        if (cameraParent.localPosition.y > stairOffset.y * unitsTravelled)
         {
-            cameraParent.localPosition -= StairOffset3D;
+            Transform lowestStair = stairParent.GetChild(0);
+            lowestStair.SetSiblingIndex(stairParent.childCount - 1);
+            lowestStair.localPosition += StairOffset3D * stairParent.childCount;
+            unitsTravelled++;
         }
 	}
 }
