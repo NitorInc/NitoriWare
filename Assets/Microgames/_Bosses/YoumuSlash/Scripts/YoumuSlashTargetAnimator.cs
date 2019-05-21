7f671f4dd7b315b25cbab0691cee865a22900563
Brian Intile <brianintile@gmail.com>
2019-05-21 01:19:41 -0400

    YoumuSlash: Ensure target position is always synced with music

---
+++ Assets/Microgames/_Bosses/YoumuSlash/Scripts/YoumuSlashTargetAnimator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YoumuSlashTargetAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private YoumuSlashTimingData timingData;
+    [SerializeField]
+    private float syncBufferTime = .075f;
+
+    private Animator rigAnimator;
+    private YoumuSlashBeatMap.TargetBeat target;
+    private float simulatedLaunchedTime;
+    
+	void Awake ()
+    {
+        rigAnimator = GetComponent<Animator>();
+	}
+
+    void onLaunch(YoumuSlashBeatMap.TargetBeat mapInstance)
+    {
+        target = mapInstance;
+        simulatedLaunchedTime = Time.time;
+    }
+
+    void onSlash(YoumuSlashTarget.SlashData slashData)
+    {
+        enabled = false;
+    }
+
+
+    void Update ()
+    {
+        var timeSinceLaunch = (timingData.PreciseBeat - target.LaunchBeat) * timingData.BeatDuration;
+        var simulatedTimeSinceLaunch = Time.time - simulatedLaunchedTime;
+        if (!MathHelper.Approximately(timeSinceLaunch, simulatedTimeSinceLaunch, syncBufferTime))
+        {
+            var normalizedTime = (timingData.PreciseBeat - target.LaunchBeat) / 4f;
+            rigAnimator.Rebind();
+            rigAnimator.Play("Launch", 0, normalizedTime);
+            simulatedLaunchedTime = Time.time - simulatedTimeSinceLaunch;
+        }
+    }
+}
