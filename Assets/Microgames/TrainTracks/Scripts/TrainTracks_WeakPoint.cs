dc117450cc9c9d386ffb470fd0a935ffa007bf78
Brian Intile <brianintile@gmail.com>
2019-05-23 00:52:54 -0400

    TrainTracks; Difficulty overhaul and touches

--- Assets/Microgames/TrainTracks/Scripts/TrainTracks_WeakPoint.cs
+++ Assets/Microgames/TrainTracks/Scripts/TrainTracks_WeakPoint.cs
@@ -23,25 +23,47 @@ public class TrainTracks_WeakPoint
     [SerializeField]
     private Sprite cutRope;
 
+    [SerializeField]
+    private float volleySpeed = 0f;
+
     int ropeDespawn = -1;
+    float tValue;   // Value between 0 (min position) and 1 (max position) where the weak point is, moves in later difficulties
+    bool goingRight;
+    bool snapped;
 
     // Use this for initialization
     void Start() {
         //Place weak points on rope
-        weakpointCount = 2;
+        weakpointCount = transform.root.childCount;
         //DATA: Values selected to ensure randomization places weak point on rope
-        float startx = Random.Range(-2f, 2f);
+        tValue = Random.Range(0f, 1f);
+        transform.localPosition = getLerpedPosition(tValue);
+        goingRight = MathHelper.randomBool();
+    }
+
+    // t is a value between 0 and 1
+    Vector3 getLerpedPosition(float t)
+    {
+        var startx = (t * 4f) - 2f; // Convert t to value between -2 and 2
         float yfactor = -0.21f;
-        if (flipped) {
+        if (flipped)
+        {
             yfactor *= -1;
         }
-        Vector3 position = new Vector3(startx, (startx - 0.5f) * yfactor, 0);
-        transform.localPosition = position;
-        this.GetComponentInParent<Animator>().enabled = false; //We don't want to animate it until the player cuts it
+        return new Vector3(startx, (startx - 0.5f) * yfactor, 0);
     }
 
     // Update is called once per frame
     void Update() {
+        if (volleySpeed > 0f && !snapped)
+        {
+            var frameDelta = volleySpeed * Time.deltaTime;
+            var goalT = goingRight ? 1f : 0f;
+            tValue = Mathf.MoveTowards(tValue, goalT, frameDelta);
+            if (tValue == goalT)
+                goingRight = !goingRight;
+            transform.localPosition = getLerpedPosition(tValue);
+        }
         if (ropeDespawn >= 0 && ropeDespawn <= 30)
         {
             ropeDespawn++;
@@ -53,6 +75,7 @@ public class TrainTracks_WeakPoint
     }
 
     void OnMouseDown() {
+        snapped = true;
         weakpointCount--;
         if (weakpointCount == 0) {
             //No weakpoints left, we win
@@ -68,7 +91,7 @@ public class TrainTracks_WeakPoint
             rope.GetComponent<SpriteRenderer>().sortingOrder = 2;
         }
         transform.localScale = new Vector3(0, 0, 0); //Make weak point disappear*/
-        this.GetComponentInParent<Animator>().enabled = true; //Play rope cutting animation
+        this.GetComponentInParent<Animator>().SetTrigger("Cut"); //Play rope cutting animation
         ParticleSystem particleSystem = GetComponentInChildren<ParticleSystem>();
         particleSystem.Play(); //Play rope cutting particles
         this.GetComponent<CircleCollider2D>().enabled = false;
