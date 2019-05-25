6848f7f5e9317074839aa35302a022fbf950607f
Ktululz <s.goliot@gmail.com>
2019-05-25 15:51:37 +0200

    Added win/lose animations
    Some scripts edits and prefabs adjustments

--- Assets/Microgames/EikiJudge/Scripts/EikiJudge_EikiController.cs
+++ Assets/Microgames/EikiJudge/Scripts/EikiJudge_EikiController.cs
@@ -22,13 +22,12 @@ public class EikiJudge_EikiController : MonoBehaviour
     {
         eikiArmSprite.sprite = null;
         //controller.SpawnSouls();
-        
     }
 
     void Update()
     {
-        // If all souls are instantiated
-        if (controller.allSoulsReady)
+        // If all souls are instantiated, and game isnt finished yet
+        if (controller.allSoulsReady && !controller.gameWon && !controller.wasted)
         {
             EikiJudgement();
             AnimateArm();
@@ -38,6 +37,16 @@ public class EikiJudge_EikiController : MonoBehaviour
             {
                 controller.SendJudgement(judgementDirection);
             }
+        }
+
+        // Play win/lose animations
+        if (controller.gameWon)
+        {
+            animator.SetBool("isWin", true);
+        }
+        if (controller.wasted)
+        {
+            animator.SetBool("isLose", true);
         }
     }
 
