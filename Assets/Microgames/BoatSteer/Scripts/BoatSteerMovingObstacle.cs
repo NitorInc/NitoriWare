70f3cf3bcc821fe3438f2b663d71538569b053f5
Brian Intile <brianintile@gmail.com>
2019-05-27 15:22:57 -0400

    BoatSteer: Adjust difficulties and add moving obstacles

---
+++ Assets/Microgames/BoatSteer/Scripts/BoatSteerMovingObstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatSteerMovingObstacle : MonoBehaviour
+{
+    [SerializeField]
+    private float moveChance = .2f;
+    [SerializeField]
+    private SineWave sineWave;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private Sprite movingSprite;
+    
+	void Start ()
+    {
+        if (Random.Range(0f,1f) <= moveChance)
+        {
+            sineWave.xOffset = Random.Range(0f, 100f);
+            sineWave.enabled = true;
+            spriteRenderer.sprite = movingSprite;
+        }
+	}
+}
