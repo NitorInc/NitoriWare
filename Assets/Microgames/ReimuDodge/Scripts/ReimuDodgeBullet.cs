9839ba00a45bfe64164a0c5940479ccabb09855f
buffsuki450 <51729065+buffsuki450@users.noreply.github.com>
2019-06-12 09:29:27 -0700

    Added Bullet Script

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuDodgeBullet : MonoBehaviour {
+
+    [Header("The thing to fly towards")]
+    [SerializeField]
+    private GameObject target;
+
+    [Header("How fast the bullet goes")]
+    [SerializeField]
+    private float speed = 1f;
+
+    [Header("Firing delay in seconds")]
+    [SerializeField]
+    private float delay = 1f;
+
+    private Vector2 trajectory;
+
+    void Start () {
+        Invoke("SetTrajectory", delay);
+    }
+	
+
+	void Update () {
+        if (trajectory != null)
+        {
+            // Move the bullet a certain distance based on trajectory speed and time
+            Vector2 newPosition = (Vector2)transform.position + (trajectory * speed * Time.deltaTime);
+            transform.position = newPosition;
+        }
+    }
+
+    void SetTrajectory()
+    {
+        // Calculate a trajectory towards the target
+        trajectory = (target.transform.position - transform.position).normalized;
+    }
+}
