dfe3eff4de5fff3b37817382bfbcb5ad9d06cd7a
DracoGideon <DracoGideon@gmail.com>
2019-06-15 00:15:26 -0500

    Scene setup

---
+++ Assets/Microgames/SpiritShooter/SpiritShooterSpawnPoint.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritShooterSpawnPoint : MonoBehaviour {
+    public GameObject startPoint;
+    public GameObject endPoint;
+}
---
+++ Assets/Microgames/SpiritShooter/SpiritShooterTargetMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritShooterTargetMover : MonoBehaviour {
+
+    GameObject startPoint;
+    GameObject endPoint;
+    public AnimationCurve curve;
+    public float fadeSpeed = 1.0f;
+    float t = 0;
+    bool bIsMoving = false;
+
+    public void Initialize(GameObject start, GameObject end) {
+        startPoint = start;
+        endPoint = end;
+        transform.parent.position = startPoint.transform.position;
+    }
+
+    public void StartMoving() {
+        bIsMoving = true;
+    }
+
+    public void Flip() {
+        GetComponent<SpiritShooterTargetUnit>().Flip();
+    }
+
+    void Update () {
+        if (bIsMoving) {
+            transform.position = Vector3.Lerp(startPoint.transform.position, endPoint.transform.position, curve.Evaluate(t));
+            t += Time.deltaTime * fadeSpeed;
+            if (t >= 1.0f) {
+                Flip();
+                bIsMoving = false;
+            }
+        }
+    }
+}
