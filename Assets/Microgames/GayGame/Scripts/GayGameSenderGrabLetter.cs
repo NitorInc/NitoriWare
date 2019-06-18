c2e51a2489c081385e2c118eb0a59d38f37a7add
Brian Intile <brianintile@gmail.com>
2019-06-18 14:29:59 -0400

    Create basic scene and gameplay

---
+++ Assets/Microgames/GayGame/Scripts/GayGameSenderGrabLetter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GayGameSenderGrabLetter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] enableOnGrab;
+    [SerializeField]
+    private GameObject[] disableOnGrab;
+    [SerializeField]
+    private MouseGrabbable letterHitBoxGrabbable;
+
+    private bool grabbed = false;
+    public bool Grabbed => grabbed;
+
+    void Start ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("MicrogameTag1"))
+        {
+            grabbed = true;
+            foreach (var obj in enableOnGrab)
+            {
+                obj.SetActive(true);
+            }
+            foreach (var obj in disableOnGrab)
+            {
+                obj.SetActive(false);
+            }
+            letterHitBoxGrabbable.enabled = true;
+        }
+    }
+}
