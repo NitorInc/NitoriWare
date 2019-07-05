b3219ecf49a71d22429f3b045bf9f47d8720b637
Brian Intile <brianintile@gmail.com>
2019-07-05 00:49:28 -0400

    Add punch gameplay

---
+++ Assets/Microgames/CloudPunch/Scripts/CloudPunchPiece.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudPunchPiece : MonoBehaviour
+{
+    [SerializeField]
+    private Animator rigAnimator;
+    [SerializeField]
+    private float animationDetectionCooldownTime = .15f;
+    [SerializeField]
+    private float heightPerPiece = 1.5f;
+    [SerializeField]
+    private bool isHead;
+    [SerializeField]
+    private float fallSpeed = 50f;
+
+    public static bool awaitingPunch;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collide(collision);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        collide(collision);
+    }
+
+    void collide(Collider2D collision)
+    {
+        if (collision.tag.Equals("MicrogameTag1")
+            && awaitingPunch
+            && !isHead)
+        {
+            // Ensure we're the closest piece vertically to the fist hitbox
+            for (int i = 0; i < transform.parent.childCount; i++)
+            {
+                var child = transform.parent.GetChild(i);
+                if (child != transform
+                    && !child.name.ToLower().Contains("head"))
+                {
+                    var distance = Mathf.Abs(transform.position.y - collision.transform.position.y);
+                    var otherPieceDistance = Mathf.Abs(child.transform.position.y - collision.transform.position.y);
+                    if (otherPieceDistance < distance)
+                        return;
+                }
+            }
+
+            rigAnimator.SetTrigger("Knock");
+            awaitingPunch = false;
+            GetComponentInChildren<Collider2D>().enabled = false;
+            transform.SetParent(null);
+            enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        var goalY = transform.GetSiblingIndex() * heightPerPiece;
+        if (transform.localPosition.y != goalY)
+        {
+            var newY = Mathf.MoveTowards(transform.localPosition.y, goalY, Time.deltaTime * fallSpeed);
+            transform.localPosition = new Vector3(transform.localPosition.x, newY, transform.localPosition.z);
+        }
+
+    }
+}
--- Assets/Microgames/CloudPunch/Scripts/CloudPunchUnzan.cs
+++ Assets/Microgames/CloudPunch/Scripts/CloudPunchUnzan.cs
@@ -19,26 +19,32 @@ public class CloudPunchUnzan : MonoBehaviour
 	void Start ()
     {
         initialX = transform.position.x;
-	}
+        updateMovement(true);
+    }
 	
 	void LateUpdate()
     {
         if (Time.time > lastPunchTime + punchMoveCooldownTime)
-            updateMovement();
+            updateMovement(false);
         if (Time.time > lastPunchTime + punchRePunchCooldownTime && Input.GetMouseButtonDown(0))
             punch();
 	}
 
-    void updateMovement()
+    void updateMovement(bool snap)
     {
+        CloudPunchPiece.awaitingPunch = false;
         var cursorPosition = CameraHelper.getCursorPosition();
         var goalPosition = new Vector3(initialX, cursorPosition.y, transform.position.z);
-        transform.position = Vector3.MoveTowards(transform.position, goalPosition, Time.deltaTime * speedToGoalPosition);
+        if (snap)
+            transform.position = goalPosition;
+        else
+            transform.position = Vector3.MoveTowards(transform.position, goalPosition, Time.deltaTime * speedToGoalPosition);
     }
 
     void punch()
     {
         rigAnimator.SetTrigger("Punch");
+        CloudPunchPiece.awaitingPunch = true;
         lastPunchTime = Time.time;
     }
 }
