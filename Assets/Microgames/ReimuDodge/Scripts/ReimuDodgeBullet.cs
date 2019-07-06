06005abeced0a0ec17ee0d8bd3d788e706492528
Alex <acf63@case.edu>
2019-07-06 15:59:35 -0400

    part 3

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ReimuDodgeBullet : MonoBehaviour
+{
+
+    // A Unity in-editor variable
+    [Header("The thing to fly towards")]
+    [SerializeField]
+    private GameObject target;
+
+    [Header("How fast the bullet goes")]
+    [SerializeField]
+    private float speed = 1f;
+
+    [Header("Firing delay in seconds")]
+    [SerializeField]
+    private float delay = 1f;
+
+    // Stores the direction of travel for the bullet
+    private Vector2 trajectory;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Invoke the setTrajectory method after the delay
+        Invoke("SetTrajectory", delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Only start moving after the trajectory has been set
+        if (trajectory != null)
+        {
+            // Move the bullet a certain distance based on trajectory speed and time
+            Vector2 newPosition = (Vector2)transform.position + (trajectory * speed * Time.deltaTime);
+            transform.position = newPosition;
+        }
+    }
+
+    void SetTrajectory()
+    {
+        // Calculate a trajectory towards the target
+        trajectory = (target.transform.position - transform.position).normalized;
+    }
+}
