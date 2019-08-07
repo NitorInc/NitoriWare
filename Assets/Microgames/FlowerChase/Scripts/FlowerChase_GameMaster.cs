dbadb160731af5c2ffceebfe253da258112f2844
Jennifer Raye <jtbont@live.com>
2019-08-07 17:04:31 -0500

    collision data, fail state
    cirno can now get hit by blocks, activating the fail state.

--- Assets/Microgames/FlowerChase/Scripts/FlowerChase_GameMaster.cs
+++ Assets/Microgames/FlowerChase/Scripts/FlowerChase_GameMaster.cs
@@ -9,31 +9,53 @@ public class FlowerChase_GameMaster : MonoBehaviour {
     public float CirnoMovementSpeed;
     public float ScrollSpeed;
     public float YuukaSpeed;
+    public int Status;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		if (Input.GetKey(KeyCode.LeftArrow))
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        switch (Status)
         {
-            if (CirnoTrans.position.x > -0.38f) CirnoTrans.position = new Vector3(CirnoTrans.position.x - CirnoMovementSpeed * Time.deltaTime, CirnoTrans.position.y, CirnoTrans.position.z);
-            
+
+            case 0: // Playing
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    if (CirnoTrans.position.x > -0.38f) CirnoTrans.position = new Vector3(CirnoTrans.position.x - CirnoMovementSpeed * Time.deltaTime, CirnoTrans.position.y, CirnoTrans.position.z);
+
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    if (CirnoTrans.position.x < 0.38f) CirnoTrans.position = new Vector3(CirnoTrans.position.x + CirnoMovementSpeed * Time.deltaTime, CirnoTrans.position.y, CirnoTrans.position.z);
+                }
+
+
+                if (CirnoTrans.position.x < -0.38f) CirnoTrans.position = new Vector3(-0.38f, CirnoTrans.position.y, CirnoTrans.position.z);
+                if (CirnoTrans.position.x > 0.38f) CirnoTrans.position = new Vector3(0.38f, CirnoTrans.position.y, CirnoTrans.position.z);
+
+
+                YuukaTrans.position = new Vector3(Mathf.Lerp(YuukaTrans.position.x, CirnoTrans.position.x, Time.deltaTime * YuukaSpeed), YuukaTrans.position.y, YuukaTrans.position.z - ScrollSpeed * Time.deltaTime);
+                CirnoTrans.position = new Vector3(CirnoTrans.position.x, CirnoTrans.position.y, CirnoTrans.position.z - ScrollSpeed * Time.deltaTime);
+                CameraTrans.position = new Vector3(0f, CameraTrans.position.y, CameraTrans.position.z);
+                break;
+
+            case 1: // Fail State
+                YuukaTrans.position = new Vector3(Mathf.Lerp(YuukaTrans.position.x, CirnoTrans.position.x, Time.deltaTime * YuukaSpeed), YuukaTrans.position.y, YuukaTrans.position.z);
+                break;
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        
+    }
+
+    public void HitBlock(){ // or probably a row of flowers
+        if (Status == 0)
         {
-            if (CirnoTrans.position.x < 0.38f) CirnoTrans.position = new Vector3(CirnoTrans.position.x + CirnoMovementSpeed*Time.deltaTime, CirnoTrans.position.y, CirnoTrans.position.z);
+            MicrogameController.instance.setVictory(victory: false, final: true);
+
+            Status = 1;
         }
-
-
-        if (CirnoTrans.position.x < -0.38f) CirnoTrans.position = new Vector3(-0.38f, CirnoTrans.position.y, CirnoTrans.position.z);
-        if (CirnoTrans.position.x > 0.38f) CirnoTrans.position = new Vector3(0.38f, CirnoTrans.position.y, CirnoTrans.position.z);
-
-
-        YuukaTrans.position = new Vector3(Mathf.Lerp(YuukaTrans.position.x, CirnoTrans.position.x, Time.deltaTime* YuukaSpeed), YuukaTrans.position.y, YuukaTrans.position.z - ScrollSpeed * Time.deltaTime);
-        CirnoTrans.position = new Vector3(CirnoTrans.position.x, CirnoTrans.position.y, CirnoTrans.position.z - ScrollSpeed*Time.deltaTime);
-        CameraTrans.position = new Vector3(0f, CameraTrans.position.y, CameraTrans.position.z); 
+        
     }
 }
