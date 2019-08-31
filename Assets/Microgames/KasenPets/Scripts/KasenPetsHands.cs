1af6fcd7fcb8b427d995745c165feb276130d06d
Sockerkaneliste <kaka5@live.se>
2019-08-31 14:12:20 +0200

    Implemented hands, complete with movement and hitboxes.

---
+++ Assets/Microgames/KasenPets/Scripts/KasenPetsHands.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KasenPetsHands : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void update () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		UpdatePosition ();
+	}
+
+	void UpdatePosition(){
+		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		cursorPosition.x = transform.position.x;
+		cursorPosition.z = transform.position.z;
+		transform.position = cursorPosition;
+	}
+}
