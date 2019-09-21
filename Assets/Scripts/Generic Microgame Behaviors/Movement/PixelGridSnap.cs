ff73504e8eade6decaa860b3644fdd5e88c4b286
Brian Intile <brianintile@gmail.com>
2019-09-21 12:45:14 -0400

    Add PixelGridSnap script

---
+++ Assets/Scripts/Generic Microgame Behaviors/Movement/PixelGridSnap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelGridSnap : MonoBehaviour
+{
+
+    [SerializeField]
+    private float pixelsPerUnit = 16f;
+    
+	
+	void LateUpdate ()
+    {
+        var unitsPerPixel = 1f / pixelsPerUnit;
+        var parentPos = transform.parent.position;
+
+        parentPos.x = roundNearest(parentPos.x, unitsPerPixel);
+        parentPos.y = roundNearest(parentPos.y, unitsPerPixel);
+
+        transform.position = parentPos;
+    }
+
+    float roundNearest(float x, float m)
+    {
+        var mod = MathHelper.trueMod(x, m);
+        var roundedDown = x - mod;
+        if (mod >= m / 2f)
+            return roundedDown + m;
+        else
+            return roundedDown;
+    }
+}
