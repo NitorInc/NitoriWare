08c377c96a151dcbcf6d723bcd31832469cc422c
IdlessChaye <1174932444@qq.com>
2019-10-15 11:05:14 +0800

    Test basic functionality

---
+++ Assets/Microgames/RumiaRescue/Scripts/RumiaRescueStateController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RumiaRescueStateController : MonoBehaviour {
+
+    private bool isRescued;
+    private Transform thisTransform;
+    public bool IsRescued {
+        get {
+            return isRescued;
+        }
+    }
+
+    void Awake() {
+        isRescued = false;
+    }
+
+    void Start() {
+        thisTransform = transform;
+    }
+
+    public bool CanRescueThisOne(Vector3 rescuerPosition,float rescueDistance) {
+        print(gameObject.name + " " + (rescuerPosition - thisTransform.position).magnitude);
+        if((rescuerPosition - thisTransform.position).sqrMagnitude <= rescueDistance * rescueDistance) {
+            isRescued = true;
+        } else {
+            isRescued = false;
+        }
+        return isRescued;
+    }
+
+	
+}
