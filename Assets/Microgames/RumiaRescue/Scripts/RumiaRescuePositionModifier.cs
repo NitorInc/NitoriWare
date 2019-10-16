8298acc40050feb7231dab782d303c0c15004e7e
IdlessChaye <1174932444@qq.com>
2019-10-16 16:23:34 +0800

    Fixed position bug and make sunshine better

---
+++ Assets/Microgames/RumiaRescue/Scripts/RumiaRescuePositionModifier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RumiaRescuePositionModifier : MonoBehaviour {
+
+    [SerializeField]
+    private float factorY2Z = 0.1f;
+
+    private Transform thisTransform;
+
+    void Start() {
+        thisTransform = transform;
+    }
+
+	void LateUpdate() {
+        Vector3 beforePosition = thisTransform.position;
+        beforePosition.z = factorY2Z * beforePosition.y;
+        thisTransform.position = beforePosition;
+    }
+}
