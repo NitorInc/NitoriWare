abd36da0105702ef5b63de10af659b71d2b96071
IdlessChaye <1174932444@qq.com>
2019-10-16 23:22:30 +0800

    Add tree as a obstacle

---
+++ Assets/Microgames/RumiaRescue/Scripts/RumiaRescueTreeController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RumiaRescueTreeController : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collider2D) {
+        print(collider2D.name);
+        MicrogameController.instance.setVictory(false);
+    }
+}
