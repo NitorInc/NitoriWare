5ab3e10f2cfcbf24985ec636bb906421db6b26b4
IdlessChaye <1174932444@qq.com>
2019-10-18 21:36:24 +0800

    Add Position Data ScriptableObject

---
+++ Assets/Microgames/RumiaRescue/Scripts/ScriptableObjectScripts/RumiaRescuePositionData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CreateAssetMenu(menuName = "Microgame Assets/RumiaRescue/Position Data")]
+public class RumiaRescuePositionData : ScriptableObject {
+    [SerializeField]
+    private Vector3 rumiaPosition;
+    public Vector3 RumiaPosition => rumiaPosition;
+
+
+    [SerializeField]
+    private Vector3[] needHelpPositionList;
+    public Vector3[] NeedHelpPositionList => needHelpPositionList;
+
+    [SerializeField]
+    private Vector3[] obstaclePositionList;
+    public Vector3[] ObstaclePositionList => obstaclePositionList;
+
+    public int GetNeedHelpListLength() => needHelpPositionList.Length;
+
+    public int GetObstacleListLength() => obstaclePositionList.Length;
+
+}
