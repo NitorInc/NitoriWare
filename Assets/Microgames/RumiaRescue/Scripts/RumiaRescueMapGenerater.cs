51dd834928b64078bb1440820de912d416c20a39
IdlessChaye <1174932444@qq.com>
2019-10-18 21:36:24 +0800

    Add Position Data ScriptableObject

---
+++ Assets/Microgames/RumiaRescue/Scripts/RumiaRescueMapGenerater.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class RumiaRescueMapGenerater : MonoBehaviour {
+    [SerializeField]
+    private GameObject rumia;
+    [SerializeField]
+    private GameObject[] needHelpList;
+    [SerializeField]
+    private GameObject[] obstacleList;
+
+    [SerializeField]
+    private int needHelpCount;
+    [SerializeField]
+    private int obstacleCount;
+
+    [SerializeField]
+    private const string positionDataRelativePath = "/Microgames/RumiaRescue/ScriptableObjects/";
+
+    private void Start() {
+        string fileName = "Traits";
+        string extension = ".asset";
+        print(Application.dataPath);    
+        var positionDataDirectories = Directory.GetDirectories(Application.dataPath + positionDataRelativePath);
+        print("!");
+        foreach(string s in positionDataDirectories)
+            print(s);
+        /*
+        MicrogameTraits traits = AssetDatabase.LoadAssetAtPath<MicrogameTraits>($"Assets{positionDataRelativePath}/{fileName}{extension}");
+        if (traits != null)
+            return traits;
+            */
+        
+    }
+}
