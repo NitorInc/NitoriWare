211104dbe21b2723de21493e9ddf2cfb226867f9
Enalye <38727813+Enalye@users.noreply.github.com>
2019-11-03 21:24:31 +0100

    Microgame/cheese find/basic functionality (#388)
    * Drawer logic
    
    * Started controller logic
    
    * Added camera movement
    
    * Basic items movement
    
    * Victory/Failure conditions
    
    The drawer and item are now chosen randomly
    Items are now hovering
    
    * Multiple objects to find
    
    * Microgame now uses prefabs
    
    Items/chest count and score are now configurable
    Items now move in circle
    Added victory screen
    Minor changes
    
    * Updated graphics
    
    Added music
    
    * Added new drawer assets
    
    Added background assets
    Changed music
    
    * Resized drawers, Background selection on startup
    
    * Changed prompt
    
    * Added difficulty settings

---
+++ Assets/Microgames/CheeseFind/Scripts/CheeseFindCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseFindCamera : MonoBehaviour {
+    [Header("Camera offset")]
+    [SerializeField]
+    private float offset = 1f;
+
+    private float _originY;
+    private float _destinationY;
+    private float _cameraFactor = 0f;
+    private bool _isMoving = false;
+
+    public void MoveCameraDown() {
+        _isMoving = true;
+    }
+
+	void Start () {
+        _destinationY = transform.position.y;
+        _originY = _destinationY + offset;
+        transform.position = new Vector3(transform.position.x, _originY, transform.position.z);
+	}
+	
+	void Update () {
+        if(!_isMoving)
+            return;
+
+        _cameraFactor +=  Time.deltaTime;
+        if(_cameraFactor > 1f) {
+            _cameraFactor = 1f;
+            _isMoving = false;
+        }
+
+		transform.position = new Vector3(
+            transform.position.x,
+            Mathf.Lerp(_originY, _destinationY, EaseInOutSine(_cameraFactor)),
+            transform.position.z);
+	}
+
+    float EaseInOutSine(float t) {
+        return (1f - Mathf.Cos(t * Mathf.PI)) / 2f;
+    }
+}
