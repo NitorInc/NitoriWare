ab6b8b7d720e45529577b39b86ed661af7fb8067
Brian Intile <brianintile@gmail.com>
2019-11-07 17:36:17 -0500

    Animate ring and improve animations

--- Assets/Microgames/BroomRace/Scripts/BroomRaceRing.cs
+++ Assets/Microgames/BroomRace/Scripts/BroomRaceRing.cs
@@ -7,10 +7,13 @@ public class BroomRaceRing : MonoBehaviour
 
     [SerializeField]
     private Animator rigAnimator;
+    [SerializeField]
+    private float ringActivateSpinSpeed = 2f;
 
     public void activate()
     {
         rigAnimator.SetTrigger("Activate");
+        rigAnimator.SetFloat("SpinSpeed", ringActivateSpinSpeed);
     }
 
 }
