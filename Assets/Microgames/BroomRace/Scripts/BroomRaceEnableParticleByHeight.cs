77757b5bcf1402e7aafd3da6fe790ed9d295d1c7
Brian Intile <brianintile@gmail.com>
2019-11-09 18:22:59 -0500

    More yeehaw adjustments

---
+++ Assets/Microgames/BroomRace/Scripts/BroomRaceEnableParticleByHeight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroomRaceEnableParticleByHeight : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject aboveParticle;
+    [SerializeField]
+    private GameObject belowParticle;
+    [SerializeField]
+    private float yThreshold;
+
+    private void Awake()
+    {
+        if (transform.position.y >= yThreshold)
+            aboveParticle.SetActive(true);
+        else
+            belowParticle.SetActive(true);
+    }
+
+}
