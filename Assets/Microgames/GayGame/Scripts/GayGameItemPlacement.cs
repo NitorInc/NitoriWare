5b832b44fd265b9506def1a9aa0c2171e5deac74
Brian Intile <brianintile@gmail.com>
2019-12-17 16:45:33 -0500

    GayGame: Randomize positions

--- Assets/Microgames/GayGame/Scripts/GayGameItemPlacement.cs
+++ Assets/Microgames/GayGame/Scripts/GayGameItemPlacement.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class GayGameItemPlacement : MonoBehaviour
 {
     [SerializeField]
-    private Vector2 letterSpawnRange;
+    private GameObject handObject;
     [SerializeField]
     private Vector2 handSpawnRange;
+    [SerializeField]
+    private GameObject letterObject;
+    [SerializeField]
+    private Vector2 letterSpawnRange;
 
     void Start ()
     {
-		
-	}
-	
-	void Update ()
-    {
-		
-	}
+        bool handOnLeft = MathHelper.randomBool();
+        handObject.transform.position = new Vector3(
+            MathHelper.randomRangeFromVector(handSpawnRange) * (handOnLeft ? -1f : 1f),
+            handObject.transform.position.y,
+            handObject.transform.position.z);
+        letterObject.transform.position = new Vector3(
+            MathHelper.randomRangeFromVector(letterSpawnRange) * (handOnLeft ? 1f : -1f),
+            letterObject.transform.position.y,
+            letterObject.transform.position.z);
+
+        //if (!handOnLeft)
+        //{
+        //    handObject.transform.localScale = new Vector3(
+        //        -handObject.transform.localScale.x,
+        //        handObject.transform.localScale.y,
+        //        handObject.transform.localScale.z);
+        //    letterObject.transform.localScale = new Vector3(
+        //        -letterObject.transform.localScale.x,
+        //        letterObject.transform.localScale.y,
+        //        letterObject.transform.localScale.z);
+        //}
+
+    }
 }
