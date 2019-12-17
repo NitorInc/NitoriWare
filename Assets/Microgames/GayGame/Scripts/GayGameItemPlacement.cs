53392511580d348978779fe3afd7b6bd9d296e6c
Brian Intile <brianintile@gmail.com>
2019-12-17 16:31:27 -0500

    GayGame: Create prefabs and adjustments

---
+++ Assets/Microgames/GayGame/Scripts/GayGameItemPlacement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GayGameItemPlacement : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 letterSpawnRange;
+    [SerializeField]
+    private Vector2 handSpawnRange;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
