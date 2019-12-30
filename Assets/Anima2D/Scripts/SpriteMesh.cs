9817643750bd7ed8a5bd5c5a60609aeffd3ee925
Brian Intile <brianintile@gmail.com>
2019-12-29 20:11:05 -0500

    BandConductor: Import microgame
    And add ambience from
    https://freesound.org/people/dethrok/sounds/272168/

---
+++ Assets/Anima2D/Scripts/SpriteMesh.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Anima2D 
+{
+	public class SpriteMesh : ScriptableObject
+	{
+		public const int api_version = 4;
+
+		[SerializeField][HideInInspector]
+		private int m_ApiVersion;
+		[SerializeField]
+		private Sprite m_Sprite;
+		[SerializeField]
+		private Mesh m_SharedMesh;
+
+		public Sprite sprite
+		{
+			get { return m_Sprite; }
+			private set { m_Sprite = value; }
+		}
+
+		public Mesh sharedMesh
+		{
+			get { return m_SharedMesh; }
+			private set { m_SharedMesh = value; }
+		}
+
+		//Needed for upgrade 4
+		[SerializeField, HideInInspector] Material[] m_SharedMaterials;
+		
+	}
+}
---
+++ Assets/Microgames/BandConductor/Scripts/BandConductorIKStayUpright.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BandConductorIKStayUpright : MonoBehaviour
+{
+    [SerializeField]
+    private float angle = 90f;
+
+	void Start ()
+    {
+		
+	}
+	
+	void LateUpdate ()
+    {
+        transform.eulerAngles = Vector3.forward * angle;
+	}
+}
