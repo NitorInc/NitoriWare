d5f86f058b7c139363a9c0a88e552fd2e5b2f69a
Brian Intile <brianintile@gmail.com>
2020-02-22 17:56:23 -0500

    Initial contents

---
+++ Assets/Microgames/Daidefender/Scripts/BulletMoveScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMoveScript : MonoBehaviour {
+
+    [Header("The thing to fly towards")]
+    [SerializeField]
+    private GameObject target;
+
+    [Header("BulletSpeed")]
+    [SerializeField]
+    private float speed;
+
+    [Header("Firing delay in seconds")]
+    [SerializeField]
+    private float delay;
+
+    private Vector2 trajectory;
+
+    // Use this for initialization
+    void Start () {
+        target = GameObject.Find("Dai");
+        Invoke("SetTrajectory", delay);
+       // var projectile = GameObject.FindGameObjectsWithTag("Bullet");
+        /* foreach (var bullet in projectile)
+         {
+             if (bullet.GetComponent<Collider> == GetComponent<collider>));
+                 Physics.IgnoreCollision(bullet.GetComponent<collider>, GetComponent<collider>);
+         } */
+        Physics2D.IgnoreCollision(this.gameObject.GetComponent<CircleCollider2D>(), this.gameObject.GetComponent<CircleCollider2D>());
+        //this works only for prefab, the bullets are probably identified as seperate gameobjects. Needs something to tie all bullets together.
+
+    }
+    //http://forum.brackeys.com/thread/ignoring-collision-between-colliders/
+
+    // Update is called once per frame
+    void Update () {
+
+        if (trajectory != null)
+        {
+            //Move the bullet a certain distance based on trajectory speed and time
+            Vector2 newPosistion = (Vector2)transform.position + (trajectory * speed * Time.deltaTime);
+            transform.position = newPosistion;
+        }
+        
+
+
+
+    }
+    void SetTrajectory()
+    {
+        // Calculate a trajectory towards the target
+        trajectory = (target.transform.position - transform.position).normalized;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit bullet detected");
+        // Destroy(collision.gameObject);
+        this.gameObject.SetActive(false);
+    }
+    }
+
+/*Previous setting
+ * 0.5 sec spacing
+ * speed 9
+ * distance 8
+ * 
+ * Current setting
+ * speed 10
+ * every bullet the repeat same spot to 2 sq
+ * delay -0.5
+ * 
+ * Game Last for 7 second
+ * animation should last no more than 1-2 seconds.*/
+
---
+++ Assets/Microgames/Daidefender/Scripts/BulletSpawn/BulletSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    //To be mount on main camera
+    public GameObject bulletPrefab;
+
+    [SerializeField]
+    private float respawnTime = 1.0f;
+
+    private Vector2 screenBounds;
+
+    private Vector2[] SpawnPos = new Vector2[0];
+
+    // Use this for initialization
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(bulletWave());
+        /*SpawnPos[0] = new Vector2(0,screenBounds.y * -2);
+        SpawnPos[1] = new Vector2(0,screenBounds.y * 2);
+        SpawnPos[2] = new Vector2(screenBounds.x * 2,0);
+        SpawnPos[3] = new Vector2(screenBounds.x * -2,0); */
+    }
+
+    
+
+   
+
+    private void spawnBullets()
+    {
+        GameObject a = Instantiate(bulletPrefab) as GameObject;
+        //set a randomizer 
+        //a.transform.position = new Vector2(screenBounds.x * -2, 0); //spawn on the left
+        // a.transform.position = new Vector2(screenBounds.x * 2, 0);//spawn from right
+        //a.transform.position = new Vector2(0, screenBounds.y * 2); //spawn from above
+        a.transform.position = new Vector2(0, screenBounds.y * -2);// spawn from below
+        /*int SpawnPoint = 0;
+        SpawnPoint = Random.Range(0, 3); 
+        a.transform.position = SpawnPos[1]; */
+    }
+    IEnumerator bulletWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnBullets();
+        }
+    }
+}
+
+//Hey how about having a line where you can type wasd and let the script process to set the bullet spawn. A sequence spawner.
