27e122c6eecd32382cdcdbbbcc5469b3c07ca77a
Brian Intile <brianintile@gmail.com>
2020-03-19 13:45:14 -0400

    DarkRoom: Change trapdoor feedback

--- Assets/Microgames/_Bosses/DarkRoom/Scripts/DarkRoom_TrapdoorBehavior.cs
+++ Assets/Microgames/_Bosses/DarkRoom/Scripts/DarkRoom_TrapdoorBehavior.cs
@@ -23,6 +23,8 @@ public class DarkRoom_TrapdoorBehavior : MonoBehaviour {
     private bool reverseLogic;
     [SerializeField]
     private bool instrumentsAlwaysOn = true;
+    [SerializeField]
+    private Animator rigAnimator;
 
     private GameObject myDoor;
     private GameObject myLamp;
@@ -56,12 +58,17 @@ public class DarkRoom_TrapdoorBehavior : MonoBehaviour {
             closeTimer = 0f;
         }
 
-        var c = lampOpen.color;
-        c.a = Mathf.InverseLerp(0f, closeDelay, closeTimer);
-        lampOpen.color = c;
-        c = lampClose.color;
-        c.a = Mathf.InverseLerp(closeDelay, 0f, closeTimer);
-        lampClose.color = c;
+
+        var t = Mathf.InverseLerp(0f, closeDelay, closeTimer);
+        rigAnimator.SetFloat("Flash", 1 - t);
+        
+        //var c = lampOpen.color;
+        //c.a = Mathf.InverseLerp(0f, closeDelay, closeTimer);
+        //lampOpen.color = c;
+        //c = lampClose.color;
+        //c.a = Mathf.InverseLerp(closeDelay, 0f, closeTimer);
+        //lampClose.color = c;
+
 
         // Handle door rotation
         HandleDoorRotation();
@@ -69,6 +76,13 @@ public class DarkRoom_TrapdoorBehavior : MonoBehaviour {
         sfxSource.panStereo = AudioHelper.getAudioPan(transform.position.x);
 
 	}
+
+    //private void LateUpdate()
+    //{
+    //    var c = lampClose.color;
+    //    c.a = 1f - lampOpen.color.a;
+    //    lampClose.color = c;
+    //}
 
     /* My methods */
 
@@ -90,12 +104,36 @@ public class DarkRoom_TrapdoorBehavior : MonoBehaviour {
             if (closeTimer == 0f)
             {
                 sfxSource.PlayOneShot(shutClip);
-                if (spotlightWarningClip != null)
-                    sfxSource.PlayOneShot(spotlightWarningClip);
+                rigAnimator.SetBool("Shining", true);
+                //if (spotlightWarningClip != null)
+                //    sfxSource.PlayOneShot(spotlightWarningClip);
             }
             closeTimer = Mathf.MoveTowards(closeTimer, closeDelay, Time.deltaTime * openingSpeedMult * 60f);
         }
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject other = otherCollider.gameObject;
+
+        // WITH: Light
+        if (other.name == "Light")
+        {
+            rigAnimator.SetTrigger("Shine");
+            rigAnimator.SetBool("Shining", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D otherCollider)
+    {
+        GameObject other = otherCollider.gameObject;
+
+        // WITH: Light
+        if (other.name == "Light")
+        {
+            rigAnimator.SetBool("Shining", false);
+        }
     }
 
     /* Getters and setters */
