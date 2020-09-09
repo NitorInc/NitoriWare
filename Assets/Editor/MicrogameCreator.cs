e17079c6f7fa3a38e5adf8f34df62ef013994bba
Brian Intile <brianintile@gmail.com>
2020-09-09 00:39:34 -0400

    Create Microgame Creator

---
+++ Assets/Editor/MicrogameCreator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+[CreateAssetMenu(menuName = "Microgame Creator")]
+public class MicrogameCreator : ScriptableSingleton<MicrogameCreator>
+{
+    [SerializeField]
+    private string templateSceneFilePath;
+    [SerializeField]
+    private string templateTraitsPath;
+
+    public enum MicrogameType
+    {
+        OneScenePerDifficulty,
+        SingleScene
+    }
+
+    public bool CreateMicrogame(string microgameId, MicrogameType type)
+    {
+        // Validate microgame ID
+        if (string.IsNullOrEmpty(microgameId))
+        {
+            Debug.LogError("Microgame ID cannot be blank");
+            return false;
+        }
+        if (Directory.Exists(Path.Combine(MicrogameCollection.FullMicrogameAssetPath, microgameId)))
+        {
+            Debug.LogError("Microgame folder for " + microgameId + " already exists");
+            return false;
+        }
+
+        AssetDatabase.CreateFolder("Assets/Microgames", microgameId);
+        AssetDatabase.CreateFolder($"Assets/Microgames/{microgameId}", "Scenes");
+
+        var traitsPath = $"Assets/Microgames/{microgameId}/Traits.asset";
+        string newScenePath;
+        switch (type)
+        {
+            case (MicrogameType.SingleScene):
+                // Copy single scene and create new traits
+                newScenePath = $"Assets/Microgames/{microgameId}/Scenes/{microgameId}.unity";
+                AssetDatabase.CopyAsset(templateSceneFilePath, newScenePath);
+                AssetDatabase.CreateAsset(CreateInstance<MicrogameTraitsSingleScene>(), traitsPath);
+                break;
+            default:
+                // Copy first difficulty scene and traits
+                newScenePath = $"Assets/Microgames/{microgameId}/Scenes/{microgameId}1.unity";
+                AssetDatabase.CopyAsset(templateSceneFilePath, newScenePath);
+                AssetDatabase.CopyAsset(templateTraitsPath, traitsPath);
+                break;
+        }
+
+        AssetDatabase.Refresh();
+        EditorSceneManager.OpenScene(newScenePath);
+        Selection.activeObject = AssetDatabase.LoadAssetAtPath<MicrogameTraits>(traitsPath);
+
+        Debug.Log($"Microgame {microgameId} created");
+
+        return true;
+    }
+}
