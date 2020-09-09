ef2ee99234c96ad6e639fb22fd7c37e270e1fe3f
Brian Intile <brianintile@gmail.com>
2020-09-08 23:47:48 -0400

    Create MicrogameTraitsSingleScene

---
+++ Assets/Scripts/Microgame/MicrogameTraitsSingleScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Microgame Traits/Traits (Single Scene)")]
+public class MicrogameTraitsSingleScene : MicrogameTraits
+{
+    public string commandKeySuffix;
+
+    [SerializeField]
+    private DifficultyCommand difficulty1;
+    [SerializeField]
+    private DifficultyCommand difficulty2;
+    [SerializeField]
+    private DifficultyCommand difficulty3;
+
+    [System.Serializable]
+    public class DifficultyCommand
+    {
+        public string commandKeySuffix;
+        public string defaultValue;
+    }
+
+
+    public override string GetLocalizedCommand(MicrogameSession session)
+    {
+        var difficulty = new DifficultyCommand[] { null, difficulty1, difficulty2, difficulty3 }[session.Difficulty];
+        return TextHelper.getLocalizedText("microgame." + session.MicrogameId + ".command" + difficulty.commandKeySuffix, difficulty.defaultValue);
+    }
+}
+
