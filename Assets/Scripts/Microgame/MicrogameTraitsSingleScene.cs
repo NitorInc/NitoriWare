5a81a0437a4b57952bdbe23fea2c854bddacb042
Brian Intile <brianintile@gmail.com>
2020-09-09 00:10:59 -0400

    Fix MicrogameTraitsSingleScene

--- Assets/Scripts/Microgame/MicrogameTraitsSingleScene.cs
+++ Assets/Scripts/Microgame/MicrogameTraitsSingleScene.cs
@@ -5,27 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Microgame Traits/Traits (Single Scene)")]
 public class MicrogameTraitsSingleScene : MicrogameTraits
 {
-    public string commandKeySuffix;
+    public override bool SceneDeterminesDifficulty => false;
 
-    [SerializeField]
-    private DifficultyCommand difficulty1;
-    [SerializeField]
-    private DifficultyCommand difficulty2;
-    [SerializeField]
-    private DifficultyCommand difficulty3;
-
-    [System.Serializable]
-    public class DifficultyCommand
-    {
-        public string commandKeySuffix;
-        public string defaultValue;
-    }
-
-
-    public override string GetLocalizedCommand(MicrogameSession session)
-    {
-        var difficulty = new DifficultyCommand[] { null, difficulty1, difficulty2, difficulty3 }[session.Difficulty];
-        return TextHelper.getLocalizedText("microgame." + session.MicrogameId + ".command" + difficulty.commandKeySuffix, difficulty.defaultValue);
-    }
+    public override string GetSceneName(MicrogameSession session) => session.MicrogameId;
 }
 
