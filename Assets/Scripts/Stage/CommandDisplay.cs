d7d13c1066beecccc1d2524dc44f3fe1f5f63cca
Brian Intile <brianintile@gmail.com>
2020-09-12 20:30:03 -0400

    Replace command with TMP text

--- Assets/Scripts/Stage/CommandDisplay.cs
+++ Assets/Scripts/Stage/CommandDisplay.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,7 @@ public class CommandDisplay : MonoBehaviour
 {
     
     [SerializeField]
-    private Text textComponent;
+    private TextMeshPro textComponent;
     [SerializeField]
     private Animator animator;
     
