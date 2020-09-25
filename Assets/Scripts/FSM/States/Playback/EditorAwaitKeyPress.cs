c0dda55f3c8596c41452374441dcb86aa9e67636
Brian Intile <brianintile@gmail.com>
2020-09-25 19:42:01 -0400

    Add key press await for editor mode

---
+++ Assets/Scripts/FSM/States/Playback/EditorAwaitKeyPress.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace StageFSM
+{
+    class EditorAwaitKeyPress : StageStateMachineBehaviour
+    {
+        [SerializeField]
+        private KeyCode advanceKey = KeyCode.Space;
+        [SerializeField]
+        private string advanceTrigger = "Advance";
+        [SerializeField]
+        private bool bypassIfNotStartScene = true;
+
+        bool Bypass => !Application.isEditor
+            || (bypassIfNotStartScene
+                && !GameController.instance.getStartScene().Equals(Animator.gameObject.scene.name));
+
+        protected override void OnStateEnterOfficial()
+        {
+            base.OnStateEnterOfficial();
+            if (Bypass)
+            {
+                Animator.SetTrigger(advanceTrigger);
+                Animator.Update(0f);
+            }
+        }
+
+        public override void OnStateUpdateOfficial()
+        {
+            base.OnStateUpdateOfficial();
+            if (!Bypass && Input.GetKeyDown(advanceKey))
+                Animator.SetTrigger(advanceTrigger);
+        }
+    }
+}
