0cfdb99f8e49d8c075c005a2688ffa6eb7009410
Brian Intile <brianintile@gmail.com>
2020-09-28 15:21:52 -0400

    Add StageDebugKeys

---
+++ Assets/Scripts/Debug/StageDebugKeys.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageDebugKeys : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(gameObject.scene.buildIndex);
+        if (Input.GetKeyDown(KeyCode.P))
+            PrefsHelper.setProgress(PrefsHelper.GameProgress.StoryComplete);
+        if (Input.GetKeyDown(KeyCode.S) && MicrogameController.instance != null)
+            MicrogameController.instance.setVictory(true);
+    }
+}
